258d44d6c98a12ab687d9074671a40f6fe61a0ea
Jason T. Greene <jason.greene@redhat.com>
2012-10-20 16:24:19 -0500

    Fix timestamp

--- core/src/main/java/io/undertow/server/handlers/file/FileHandler.java
+++ core/src/main/java/io/undertow/server/handlers/file/FileHandler.java
@@ -179,7 +179,7 @@ public class FileHandler implements HttpHandler {
             builder.append("<tr class='").append((++i & 1) == 1 ? "odd" : "even").append("'><td><a class='icon ");
             builder.append(entry.isFile() ? "file" : "dir");
             builder.append("' href='").append(entry.getName()).append("'>").append(entry.getName()).append("</a></td><td>");
-            builder.append(format.format(new Date(file.lastModified()))).append("</td><td>");
+            builder.append(format.format(new Date(entry.lastModified()))).append("</td><td>");
             if (entry.isFile()) {
                 formatSize(builder, entry.length());
             } else {
