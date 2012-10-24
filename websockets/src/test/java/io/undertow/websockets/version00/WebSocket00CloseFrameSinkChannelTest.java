553903bf894c414f5b0c12f092617278be4566d6
Norman Maurer <nmaurer@redhat.com>
2012-10-24 08:36:09 +0200

    Refactor tests to share code

--- websockets/src/test/java/io/undertow/websockets/version00/WebSocket00CloseFrameSinkChannelTest.java
+++ websockets/src/test/java/io/undertow/websockets/version00/WebSocket00CloseFrameSinkChannelTest.java
@@ -17,115 +17,83 @@
  */
 package io.undertow.websockets.version00;
 
-import static org.easymock.EasyMock.*;
-
 import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.channels.FileChannel;
-
-import io.undertow.websockets.utils.TestUtils;
 
 import org.junit.Test;
 import org.xnio.channels.StreamSinkChannel;
-import org.xnio.channels.StreamSourceChannel;
 
 /**
  *  
  * @author <a href="mailto:nmaurer@redhat.com">Norman Maurer</a>
  *
  */
-public class WebSocket00CloseFrameSinkChannelTest {
+public class WebSocket00CloseFrameSinkChannelTest extends AbstractWebSocketFrameSinkChannelTest {
 
+    @Override
+    protected WebSocket00FrameSinkChannel createChannel(StreamSinkChannel channel, WebSocket00Channel wsChannel,
+            int payloadLength) {
+        return new WebSocket00CloseFrameSinkChannel(channel, wsChannel);
+    }
+
+    @Override
     @Test(expected = IOException.class)
     public void testWriteWithBuffer() throws IOException {
-        WebSocket00Channel mockChannel = createMock(WebSocket00Channel.class);
-        replay(mockChannel);
-        
-        StreamSinkChannel mockStreamChannel = createMock(StreamSinkChannel.class);
-        expect(mockStreamChannel.isOpen()).andReturn(true);
-        replay(mockStreamChannel);
-        
-        try {
-            WebSocket00CloseFrameSinkChannel channel = new WebSocket00CloseFrameSinkChannel(mockStreamChannel, mockChannel);
-            channel.write(ByteBuffer.allocate(8));
-        } finally {
-            TestUtils.verifyAndReset(mockChannel, mockStreamChannel);
-        }
+        super.testWriteWithBuffer();
     }
-    
 
+    @Override
+    @Test(expected = IOException.class)
+    public void testWriteWithBufferWithCorruptedPayload() throws IOException {
+        super.testWriteWithBufferWithCorruptedPayload();
+    }
+
+    @Override
     @Test(expected = IOException.class)
     public void testWriteWithBuffers() throws IOException {
-        WebSocket00Channel mockChannel = createMock(WebSocket00Channel.class);
-        replay(mockChannel);
-        
-        StreamSinkChannel mockStreamChannel = createMock(StreamSinkChannel.class);
-        expect(mockStreamChannel.isOpen()).andReturn(true);
-        replay(mockStreamChannel);
-        
-        try {
-            WebSocket00CloseFrameSinkChannel channel = new WebSocket00CloseFrameSinkChannel(mockStreamChannel, mockChannel);
-            channel.write(new ByteBuffer[] {ByteBuffer.allocate(8), ByteBuffer.allocate(8)});
-        } finally {
-            TestUtils.verifyAndReset(mockChannel, mockStreamChannel);
-        }
-    }
-    
-    @Test(expected = IOException.class)
-    public void testWriteWithBuffersAndOffset() throws IOException {
-        WebSocket00Channel mockChannel = createMock(WebSocket00Channel.class);
-        replay(mockChannel);
-        
-        StreamSinkChannel mockStreamChannel = createMock(StreamSinkChannel.class);
-        expect(mockStreamChannel.isOpen()).andReturn(true);
-        replay(mockStreamChannel);
-        
-        try {
-            WebSocket00CloseFrameSinkChannel channel = new WebSocket00CloseFrameSinkChannel(mockStreamChannel, mockChannel);
-            channel.write(new ByteBuffer[] {ByteBuffer.allocate(8), ByteBuffer.allocate(8)}, 1, 4);
-        } finally {
-            TestUtils.verifyAndReset(mockChannel, mockStreamChannel);
-        }
+        super.testWriteWithBuffers();
     }
 
+    @Override
+    @Test(expected = IOException.class)
+    public void testWriteWithBuffersWithCorruptedPayload() throws IOException {
+        super.testWriteWithBuffersWithCorruptedPayload();
+    }
+
+    @Override
+    @Test(expected = IOException.class)
+    public void testWriteWithBuffersWithOffset() throws IOException {
+        super.testWriteWithBuffersWithOffset();
+    }
+
+    @Override
+    @Test(expected = IOException.class)
+    public void testWriteWithBuffersWithOffsetWithCorruptPayload() throws IOException {
+        super.testWriteWithBuffersWithOffsetWithCorruptPayload();
+    }
+
+    @Override
     @Test(expected = IOException.class)
     public void testTransferFrom() throws IOException {
-        WebSocket00Channel mockChannel = createMock(WebSocket00Channel.class);
-        replay(mockChannel);
-        
-        StreamSinkChannel mockStreamChannel = createMock(StreamSinkChannel.class);
-        expect(mockStreamChannel.isOpen()).andReturn(true);
-        replay(mockStreamChannel);
-        
-        FileChannel mockFileChannel = createMock(FileChannel.class);
-        replay(mockFileChannel);
-        
-        try {
-            WebSocket00CloseFrameSinkChannel channel = new WebSocket00CloseFrameSinkChannel(mockStreamChannel, mockChannel);
-            channel.transferFrom(mockFileChannel, 0, 10);
-        } finally {
-            TestUtils.verifyAndReset(mockChannel, mockStreamChannel, mockFileChannel);
-        }
+        super.testTransferFrom();
     }
 
+    @Override
+    @Test(expected = IOException.class)
+    public void testTransferFromWithCorruptedPayload() throws IOException {
+        super.testTransferFromWithCorruptedPayload();
+    }
 
+    @Override
     @Test(expected = IOException.class)
     public void testTransferFromSource() throws IOException {
-        WebSocket00Channel mockChannel = createMock(WebSocket00Channel.class);
-        replay(mockChannel);
-        
-        StreamSinkChannel mockStreamChannel = createMock(StreamSinkChannel.class);
-        expect(mockStreamChannel.isOpen()).andReturn(true);
-        replay(mockStreamChannel);
-        
-        StreamSourceChannel mockSourceChannel = createMock(StreamSourceChannel.class);
-        replay(mockSourceChannel);
-        
-        try {
-            WebSocket00CloseFrameSinkChannel channel = new WebSocket00CloseFrameSinkChannel(mockStreamChannel, mockChannel);
-            channel.transferFrom(mockSourceChannel, 1, ByteBuffer.allocate(8));
-        } finally {
-            TestUtils.verifyAndReset(mockChannel, mockStreamChannel, mockSourceChannel);
-        }
+        super.testTransferFromSource();
     }
+
+    @Override
+    @Test(expected = IOException.class)
+    public void testTransferFromSourceWithCorruptPayload() throws IOException {
+        super.testTransferFromSourceWithCorruptPayload();
+    }
+    
+    
 }
