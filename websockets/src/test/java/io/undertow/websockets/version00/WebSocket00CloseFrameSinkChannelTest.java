cd485c3cdcbcdcf519116da091e8a920f166e61a
Norman Maurer <nmaurer@redhat.com>
2012-10-25 13:21:34 +0200

    Add test cases to test that 0 is returned when try to write/transfer on a Sink that is not in use yet

--- websockets/src/test/java/io/undertow/websockets/version00/WebSocket00CloseFrameSinkChannelTest.java
+++ websockets/src/test/java/io/undertow/websockets/version00/WebSocket00CloseFrameSinkChannelTest.java
@@ -17,9 +17,24 @@
  */
 package io.undertow.websockets.version00;
 
+import static org.easymock.EasyMock.replay;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import io.undertow.websockets.utils.StreamSinkChannelAdapter;
+import io.undertow.websockets.utils.StreamSourceChannelAdapter;
+import io.undertow.websockets.utils.TestUtils;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.FileChannel;
 
 import org.junit.Test;
+import org.xnio.channels.ConnectedStreamChannel;
 import org.xnio.channels.StreamSinkChannel;
 
 /**
@@ -94,6 +109,127 @@ public class WebSocket00CloseFrameSinkChannelTest extends AbstractWebSocketFrame
     public void testTransferFromSourceWithCorruptPayload() throws IOException {
         super.testTransferFromSourceWithCorruptPayload();
     }
+
+    @Test
+    public void testWriteWithBufferNotInUse() throws IOException {
+        ConnectedStreamChannel mockChannel = createMockChannel();
+        replay(mockChannel);
+       
+        WebSocket00Channel wsChannel = createWSChannel(mockChannel, false);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+        try {
+            WebSocket00FrameSinkChannel channel = createChannel(new StreamSinkChannelAdapter(Channels.newChannel(out)), wsChannel, DATA.length);
+            ByteBuffer buf = ByteBuffer.wrap(DATA);
+            assertEquals(0, channel.write(buf));
+
+            channel.close();
+
+
+        } finally {
+            TestUtils.verifyAndReset(mockChannel);
+        }
+    }
     
+    @Test
+    public void testWriteWithBuffersNotInUse() throws IOException {
+        ConnectedStreamChannel mockChannel = createMockChannel();
+        replay(mockChannel);
+       
+        WebSocket00Channel wsChannel = createWSChannel(mockChannel, false);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+        try {
+            WebSocket00FrameSinkChannel channel = createChannel(new StreamSinkChannelAdapter(Channels.newChannel(out)), wsChannel, DATA.length);
+
+            ByteBuffer buf = ByteBuffer.wrap(DATA);
+            ByteBuffer buf1 = (ByteBuffer) buf.duplicate().limit(2);
+            ByteBuffer buf2 = (ByteBuffer) buf.duplicate().position(2).limit(buf.limit());
+
+            ByteBuffer[] bufs = new ByteBuffer[] {buf1, buf2};
+            assertEquals(0, channel.write(bufs));
+
+            channel.close();
+        } finally {
+            TestUtils.verifyAndReset(mockChannel);
+        }
+    }
     
+    @Test
+    public void testWriteWithBuffersWithOffsetNotInUse() throws IOException {
+        ConnectedStreamChannel mockChannel = createMockChannel();
+        replay(mockChannel);
+       
+        WebSocket00Channel wsChannel = createWSChannel(mockChannel, false);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+        try {
+            WebSocket00FrameSinkChannel channel = createChannel(new StreamSinkChannelAdapter(Channels.newChannel(out)), wsChannel, DATA.length);
+
+            ByteBuffer buf = ByteBuffer.wrap(DATA);
+            ByteBuffer buf1 = (ByteBuffer) buf.duplicate().limit(2);
+            ByteBuffer buf2 = (ByteBuffer) buf.duplicate().position(2).limit(buf.limit());
+
+            ByteBuffer[] bufs = new ByteBuffer[] {buf1, buf2};
+ 
+            assertEquals(0, channel.write(bufs, 0, 2));
+
+            channel.close();
+        } finally {
+            TestUtils.verifyAndReset(mockChannel);
+        }
+    }
+    
+    @Test
+    public void testTransferFromNotInUse() throws IOException {
+        ConnectedStreamChannel mockChannel = createMockChannel();
+        replay(mockChannel);
+        
+        File file = File.createTempFile("undertow-test", ".tmp");
+        file.deleteOnExit();
+        FileOutputStream fout = new FileOutputStream(file);
+        fout.write(DATA);
+        fout.close();
+        
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+        FileChannel fchannel = new FileInputStream(file).getChannel();
+        WebSocket00Channel wsChannel = createWSChannel(mockChannel, false);
+        try {
+            WebSocket00FrameSinkChannel channel = createChannel(new StreamSinkChannelAdapter(Channels.newChannel(out)), wsChannel, DATA.length);
+            assertEquals(0, channel.transferFrom(fchannel, 0, DATA.length));
+ 
+            channel.close();
+
+        } finally {
+            TestUtils.verifyAndReset(mockChannel);
+        }
+    }
+    
+    @Test
+    public void testTransferFromSourceNotInUse() throws IOException {
+        ConnectedStreamChannel mockChannel = createMockChannel();
+        replay(mockChannel);
+        
+        File file = File.createTempFile("undertow-test", ".tmp");
+        file.deleteOnExit();
+        FileOutputStream fout = new FileOutputStream(file);
+        fout.write(DATA);
+        fout.close();
+        
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+        StreamSourceChannelAdapter fchannel = new StreamSourceChannelAdapter(Channels.newChannel(new FileInputStream(file)));
+        WebSocket00Channel wsChannel = createWSChannel(mockChannel, false);
+        try {
+            WebSocket00FrameSinkChannel channel = createChannel(new StreamSinkChannelAdapter(Channels.newChannel(out)), wsChannel, DATA.length);
+            ByteBuffer buf = ByteBuffer.allocate(8);
+            assertEquals(0, channel.transferFrom(fchannel, DATA.length, (ByteBuffer) buf.clear()));
+
+            channel.close();
+
+        } finally {
+            TestUtils.verifyAndReset(mockChannel);
+        }
+    }
 }
