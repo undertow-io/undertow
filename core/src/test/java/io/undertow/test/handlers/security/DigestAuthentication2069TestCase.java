3ed0f5ab0bb5170bf0332e05c70ba123b9524688
Darran Lofthouse <darran.lofthouse@jboss.com>
2012-10-25 17:16:46 +0100

    Two additional tests to ensure invalid nonces are rejected.
    The first test rejects an arbitraty nonce from the client, in this case the nonce is completely invalid - this could happen if a server is restarted and a new nonce manager created.
    The second test verifies that a valid but previously used nonce is rejected - this covers the most likely replay attack scenario.

--- core/src/test/java/io/undertow/test/handlers/security/DigestAuthentication2069TestCase.java
+++ core/src/test/java/io/undertow/test/handlers/security/DigestAuthentication2069TestCase.java
@@ -64,6 +64,11 @@ public class DigestAuthentication2069TestCase extends UsernamePasswordAuthentica
                 callbackHandler, new SimpleNonceManager());
     }
 
+    /**
+     * Creates a response value from the supplied parameters.
+     *
+     * @return The generated Hex encoded MD5 digest based response.
+     */
     private String createResponse(final String userName, final String realm, final String password, final String method,
             final String uri, final String nonce) throws Exception {
         MessageDigest digest = MessageDigest.getInstance("MD5");
@@ -90,6 +95,9 @@ public class DigestAuthentication2069TestCase extends UsernamePasswordAuthentica
         return HexConverter.convertToHexString(digest.digest());
     }
 
+    /**
+     * Test for a successful authentication.
+     */
     @Test
     public void testDigestSuccess() throws Exception {
         setAuthenticationChain();
@@ -128,7 +136,13 @@ public class DigestAuthentication2069TestCase extends UsernamePasswordAuthentica
         assertEquals(1, values.length);
         assertEquals("ResponseHandler", values[0].getValue());
     }
-    
+
+    /**
+     * Test that a request is correctly rejected with a bad user name.
+     *
+     * In this case both the supplied username is wrong and also the generated response can not be valid as there is no
+     * corresponding user.
+     */
     @Test
     public void testBadUserName() throws Exception {
         setAuthenticationChain();
@@ -164,6 +178,9 @@ public class DigestAuthentication2069TestCase extends UsernamePasswordAuthentica
         assertEquals(401, result.getStatusLine().getStatusCode());
     }
 
+    /**
+     * Test that a request is correctly rejected if a bad password is used to generate the response value.
+     */
     @Test
     public void testBadPassword() throws Exception {
         setAuthenticationChain();
@@ -198,10 +215,155 @@ public class DigestAuthentication2069TestCase extends UsernamePasswordAuthentica
         result = client.execute(get);
         assertEquals(401, result.getStatusLine().getStatusCode());
     }
-    
 
-    // Test completely different nonce.
-    // Test nonce re-use.
+    /**
+     * Test that for a valid username and password if an invalid nonce is used the request should be rejected with the nonce
+     * marked as stale, using the replacement nonce should then work.
+     */
+    @Test
+    public void testDifferentNonce() throws Exception {
+        setAuthenticationChain();
+
+        DefaultHttpClient client = new DefaultHttpClient();
+        HttpGet get = new HttpGet(DefaultServer.getDefaultServerAddress());
+        HttpResponse result = client.execute(get);
+        assertEquals(401, result.getStatusLine().getStatusCode());
+        Header[] values = result.getHeaders(WWW_AUTHENTICATE.toString());
+        assertEquals(1, values.length);
+        String value = values[0].getValue();
+        assertTrue(value.startsWith(DIGEST.toString()));
+        Map<DigestWWWAuthenticateToken, String> parsedHeader = DigestWWWAuthenticateToken.parseHeader(value.substring(7));
+        assertEquals(REALM_NAME, parsedHeader.get(DigestWWWAuthenticateToken.REALM));
+        assertEquals(DigestAlgorithm.MD5.getToken(), parsedHeader.get(DigestWWWAuthenticateToken.ALGORITHM));
+
+        String nonce = "AU1aCIiy48ENMTM1MTE3OTUxMDU2OLrHnBlV2GBzzguCWOPET+0=";
+
+        String response = createResponse("userOne", REALM_NAME, "passwordOne", "GET", "/", nonce);
+
+        client = new DefaultHttpClient();
+        get = new HttpGet(DefaultServer.getDefaultServerAddress());
+        StringBuilder sb = new StringBuilder(DIGEST.toString());
+        sb.append(" ");
+        sb.append(DigestAuthorizationToken.USERNAME.getName()).append("=").append("\"userOne\"").append(",");
+        sb.append(DigestAuthorizationToken.REALM.getName()).append("=\"").append(REALM_NAME).append("\",");
+        sb.append(DigestAuthorizationToken.NONCE.getName()).append("=\"").append(nonce).append("\",");
+        sb.append(DigestAuthorizationToken.DIGEST_URI.getName()).append("=\"/\",");
+        sb.append(DigestAuthorizationToken.RESPONSE.getName()).append("=\"").append(response).append("\"");
+
+        get.addHeader(AUTHORIZATION.toString(), sb.toString());
+        result = client.execute(get);
+        assertEquals(401, result.getStatusLine().getStatusCode());
+        values = result.getHeaders(WWW_AUTHENTICATE.toString());
+        assertEquals(1, values.length);
+        value = values[0].getValue();
+        assertTrue(value.startsWith(DIGEST.toString()));
+        parsedHeader = DigestWWWAuthenticateToken.parseHeader(value.substring(7));
+        assertEquals(REALM_NAME, parsedHeader.get(DigestWWWAuthenticateToken.REALM));
+        assertEquals(DigestAlgorithm.MD5.getToken(), parsedHeader.get(DigestWWWAuthenticateToken.ALGORITHM));
+        assertEquals("true", parsedHeader.get(DigestWWWAuthenticateToken.STALE));
+
+        nonce = parsedHeader.get(DigestWWWAuthenticateToken.NONCE);
+        response = createResponse("userOne", REALM_NAME, "passwordOne", "GET", "/", nonce);
+
+        client = new DefaultHttpClient();
+        get = new HttpGet(DefaultServer.getDefaultServerAddress());
+        sb = new StringBuilder(DIGEST.toString());
+        sb.append(" ");
+        sb.append(DigestAuthorizationToken.USERNAME.getName()).append("=").append("\"userOne\"").append(",");
+        sb.append(DigestAuthorizationToken.REALM.getName()).append("=\"").append(REALM_NAME).append("\",");
+        sb.append(DigestAuthorizationToken.NONCE.getName()).append("=\"").append(nonce).append("\",");
+        sb.append(DigestAuthorizationToken.DIGEST_URI.getName()).append("=\"/\",");
+        sb.append(DigestAuthorizationToken.RESPONSE.getName()).append("=\"").append(response).append("\"");
+
+        get.addHeader(AUTHORIZATION.toString(), sb.toString());
+        result = client.execute(get);
+
+        assertEquals(200, result.getStatusLine().getStatusCode());
+
+        values = result.getHeaders("ProcessedBy");
+        assertEquals(1, values.length);
+        assertEquals("ResponseHandler", values[0].getValue());
+    }
+
+    /**
+     * Test that in RFC2069 mode nonce re-use is rejected.
+     */
+    @Test
+    public void testNonceReUse() throws Exception {
+        setAuthenticationChain();
+
+        DefaultHttpClient client = new DefaultHttpClient();
+        HttpGet get = new HttpGet(DefaultServer.getDefaultServerAddress());
+        HttpResponse result = client.execute(get);
+        assertEquals(401, result.getStatusLine().getStatusCode());
+        Header[] values = result.getHeaders(WWW_AUTHENTICATE.toString());
+        assertEquals(1, values.length);
+        String value = values[0].getValue();
+        assertTrue(value.startsWith(DIGEST.toString()));
+        Map<DigestWWWAuthenticateToken, String> parsedHeader = DigestWWWAuthenticateToken.parseHeader(value.substring(7));
+        assertEquals(REALM_NAME, parsedHeader.get(DigestWWWAuthenticateToken.REALM));
+        assertEquals(DigestAlgorithm.MD5.getToken(), parsedHeader.get(DigestWWWAuthenticateToken.ALGORITHM));
+
+        String nonce = parsedHeader.get(DigestWWWAuthenticateToken.NONCE);
+
+        String response = createResponse("userOne", REALM_NAME, "passwordOne", "GET", "/", nonce);
+
+        client = new DefaultHttpClient();
+        get = new HttpGet(DefaultServer.getDefaultServerAddress());
+        StringBuilder sb = new StringBuilder(DIGEST.toString());
+        sb.append(" ");
+        sb.append(DigestAuthorizationToken.USERNAME.getName()).append("=").append("\"userOne\"").append(",");
+        sb.append(DigestAuthorizationToken.REALM.getName()).append("=\"").append(REALM_NAME).append("\",");
+        sb.append(DigestAuthorizationToken.NONCE.getName()).append("=\"").append(nonce).append("\",");
+        sb.append(DigestAuthorizationToken.DIGEST_URI.getName()).append("=\"/\",");
+        sb.append(DigestAuthorizationToken.RESPONSE.getName()).append("=\"").append(response).append("\"");
+
+        get.addHeader(AUTHORIZATION.toString(), sb.toString());
+        result = client.execute(get);
+        assertEquals(200, result.getStatusLine().getStatusCode());
+        values = result.getHeaders("ProcessedBy");
+        assertEquals(1, values.length);
+        assertEquals("ResponseHandler", values[0].getValue());
+
+        client = new DefaultHttpClient();
+        get = new HttpGet(DefaultServer.getDefaultServerAddress());
+
+        get.addHeader(AUTHORIZATION.toString(), sb.toString());
+        result = client.execute(get);
+        assertEquals(401, result.getStatusLine().getStatusCode());
+
+        values = result.getHeaders(WWW_AUTHENTICATE.toString());
+        assertEquals(1, values.length);
+        value = values[0].getValue();
+        assertTrue(value.startsWith(DIGEST.toString()));
+        parsedHeader = DigestWWWAuthenticateToken.parseHeader(value.substring(7));
+        assertEquals(REALM_NAME, parsedHeader.get(DigestWWWAuthenticateToken.REALM));
+        assertEquals(DigestAlgorithm.MD5.getToken(), parsedHeader.get(DigestWWWAuthenticateToken.ALGORITHM));
+        assertEquals("true", parsedHeader.get(DigestWWWAuthenticateToken.STALE));
+
+        nonce = parsedHeader.get(DigestWWWAuthenticateToken.NONCE);
+        response = createResponse("userOne", REALM_NAME, "passwordOne", "GET", "/", nonce);
+
+        client = new DefaultHttpClient();
+        get = new HttpGet(DefaultServer.getDefaultServerAddress());
+        sb = new StringBuilder(DIGEST.toString());
+        sb.append(" ");
+        sb.append(DigestAuthorizationToken.USERNAME.getName()).append("=").append("\"userOne\"").append(",");
+        sb.append(DigestAuthorizationToken.REALM.getName()).append("=\"").append(REALM_NAME).append("\",");
+        sb.append(DigestAuthorizationToken.NONCE.getName()).append("=\"").append(nonce).append("\",");
+        sb.append(DigestAuthorizationToken.DIGEST_URI.getName()).append("=\"/\",");
+        sb.append(DigestAuthorizationToken.RESPONSE.getName()).append("=\"").append(response).append("\"");
+
+        get.addHeader(AUTHORIZATION.toString(), sb.toString());
+        result = client.execute(get);
+
+        assertEquals(200, result.getStatusLine().getStatusCode());
+
+        values = result.getHeaders("ProcessedBy");
+        assertEquals(1, values.length);
+        assertEquals("ResponseHandler", values[0].getValue());
+    }
+
     // Test choosing different algorithm.
     // Different URI - Test not matching the request as well.
     // Different Method
