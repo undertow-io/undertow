2f99a665e4fc0a50b66cd517586a5278420658e9
Norman Maurer <nmaurer@redhat.com>
2012-11-15 11:17:13 +0100

    Fix case for Connection and Upgrade header values

--- websockets/src/main/java/io/undertow/websockets/protocol/Handshake.java
+++ websockets/src/main/java/io/undertow/websockets/protocol/Handshake.java
@@ -88,8 +88,8 @@ public abstract class Handshake {
      */
     protected void performUpgrade(final ConcreteIoFuture<WebSocketChannel> ioFuture, final HttpServerExchange exchange, final byte[] data) {
         exchange.getResponseHeaders().put(Headers.CONTENT_LENGTH, "" + data.length);
-        exchange.getResponseHeaders().put(Headers.UPGRADE, "websocket");
-        exchange.getResponseHeaders().put(Headers.CONNECTION, "upgrade");
+        exchange.getResponseHeaders().put(Headers.UPGRADE, "WebSocket");
+        exchange.getResponseHeaders().put(Headers.CONNECTION, "Upgrade");
 
         exchange.upgradeChannel();
         final StreamSinkChannel channel = exchange.getResponseChannelFactory().create();
