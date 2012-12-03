cb8a7f65dc03d7ba391157a0b1f97a690080b8e3
Norman Maurer <nmaurer@redhat.com>
2012-12-03 07:40:54 +0100

    Fix for latest api changes

--- websockets/src/test/java/io/undertow/websockets/protocol/server/AutobahnWebSocketServer.java
+++ websockets/src/test/java/io/undertow/websockets/protocol/server/AutobahnWebSocketServer.java
@@ -76,7 +76,7 @@ public class AutobahnWebSocketServer {
                     .set(Options.TCP_NODELAY, true)
                     .set(Options.REUSE_ADDRESSES, true)
                     .getMap();
-            openListener = new HttpOpenListener(new ByteBufferSlicePool(BufferAllocator.DIRECT_BYTE_BUFFER_ALLOCATOR, 8192, 8192 * 8192));
+            openListener = new HttpOpenListener(new ByteBufferSlicePool(BufferAllocator.DIRECT_BYTE_BUFFER_ALLOCATOR, 8192, 8192 * 8192), 8192);
             ChannelListener acceptListener = ChannelListeners.openListenerAdapter(openListener);
             server = worker.createStreamServer(new InetSocketAddress(port), acceptListener, serverOptions);
 
