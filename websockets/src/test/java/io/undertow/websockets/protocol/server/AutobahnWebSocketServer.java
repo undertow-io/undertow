ca62bec4c47c40fbb21fefdccfb683955e9ab556
Norman Maurer <nmaurer@redhat.com>
2012-12-05 07:19:14 +0100

    Add documentation howto run autobahntestsuite against undertow

--- websockets/src/test/java/io/undertow/websockets/protocol/server/AutobahnWebSocketServer.java
+++ websockets/src/test/java/io/undertow/websockets/protocol/server/AutobahnWebSocketServer.java
@@ -43,6 +43,43 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 
 /**
+ * This class is intended for use with testing against the Python
+ * <a href="http://www.tavendo.de/autobahn/testsuite.html">AutoBahn test suite</a>.
+ *
+ * Autobahn installation documentation can be found <a href="http://autobahn.ws/testsuite/installation">here</a>.
+ *
+ * <h3>How to run the tests on Linux/OSX.</h3>
+ *
+ * <p>01. Install AutoBahn: <tt>sudo easy_install autobahntestsuite</tt>.  Test using <tt>wstest --help</tt>.
+ *
+ * <p>02. Create a directory for test configuration and results: <tt>mkdir ~/autobahn</tt> <tt>cd ~/autobahn</tt>.
+ *
+ * <p>03. Create <tt>fuzzing_client_spec.json</tt> in the above directory
+ * {@code
+ * {
+ *    "options": {"failByDrop": false},
+ *    "outdir": "./reports/servers",
+ *
+ *    "servers": [
+ *                 {"agent": "Netty4",
+ *                  "url": "ws://localhost:9000",
+ *                  "options": {"version": 18}}
+ *               ],
+ *
+ *    "cases": ["*"],
+ *    "exclude-cases": ["9.*"],
+ *    "exclude-agent-cases": {}
+ * }
+ * }
+ * Note that we disabled the <strong>9.*</strong> tests for now as these fail.
+ *
+ * <p>04. Run the <tt>AutobahnServer</tt> located in this package. If you are in Eclipse IDE, right click on
+ * <tt>AutobahnServer.java</tt> and select Run As > Java Application.
+ *
+ * <p>05. Run the Autobahn test <tt>wstest -m fuzzingclient -s fuzzingclient.json</tt>.
+ *
+ * <p>06. See the results in <tt>./reports/servers/index.html</tt>
+ *
  * @author <a href="mailto:nmaurer@redhat.com">Norman Maurer</a>
  */
 public class AutobahnWebSocketServer {
