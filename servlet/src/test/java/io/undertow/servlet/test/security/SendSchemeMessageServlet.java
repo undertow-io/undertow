d7a5e71c03d224ace4517bb59e4196ff6508430f
Darran Lofthouse <darran.lofthouse@jboss.com>
2013-01-24 11:10:37 +0000

    Addition of a test case to the servlet module to test transport guarantee handling and some minor modifications required for the test to pass.

---
+++ servlet/src/test/java/io/undertow/servlet/test/security/SendSchemeMessageServlet.java
@@ -0,0 +1,46 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2013 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.undertow.servlet.test.security;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.Charset;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Simple test servlet to return the transport for the request back to the calling client.
+ *
+ * @author <a href="mailto:darran.lofthouse@jboss.com">Darran Lofthouse</a>
+ */
+public class SendSchemeMessageServlet extends HttpServlet {
+
+    private static final long serialVersionUID = -4804724108087346230L;
+
+    private static Charset UTF_8 = Charset.forName("UTF-8");
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        OutputStream stream = resp.getOutputStream();
+        stream.write(req.getScheme().getBytes(UTF_8));
+    }
+
+}
