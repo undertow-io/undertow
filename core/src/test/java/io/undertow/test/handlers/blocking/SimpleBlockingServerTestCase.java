022b8a08b0e5623ceb3d75662fcbea8528ae9eb6
Stuart Douglas <stuart.w.douglas@gmail.com>
2013-02-04 11:10:17 +1100

    Use correct ChannelInputStream

--- core/src/test/java/io/undertow/test/handlers/blocking/SimpleBlockingServerTestCase.java
+++ core/src/test/java/io/undertow/test/handlers/blocking/SimpleBlockingServerTestCase.java
@@ -39,8 +39,8 @@ import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.xnio.streams.ChannelInputStream;
 import org.xnio.streams.ChannelOutputStream;
-import sun.nio.ch.ChannelInputStream;
 
 /**
  * @author Stuart Douglas
