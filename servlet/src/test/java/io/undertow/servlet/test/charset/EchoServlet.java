7c874805f96d3cc0204dbde75d3e023a6e89ee4e
Matej Lazar <matejonnet@gmail.com>
2013-02-25 16:50:35 +0100

    Multipart form data encoding test.

---
+++ servlet/src/test/java/io/undertow/servlet/test/charset/EchoServlet.java
@@ -0,0 +1,27 @@
+package io.undertow.servlet.test.charset;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Matej Lazar
+ */
+public class EchoServlet extends HttpServlet {
+
+    @Override
+    protected void service(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        String charset = req.getParameter("charset");
+        resp.setCharacterEncoding(charset);
+        String message = req.getParameter("message");
+        System.out.println("Received message: " + message);
+        writer.write(message);
+        writer.close();
+    }
+
+}
