fd9f3180b09e07231bcc8ddaccca9cd19d43c16e
Stuart Douglas <stuart.w.douglas@gmail.com>
2013-02-26 16:02:53 +1100

    Simplify the security example

--- examples/src/main/java/io/undertow/examples/security/basic/BasicAuthServer.java
+++ examples/src/main/java/io/undertow/examples/security/basic/BasicAuthServer.java
@@ -1,104 +1,32 @@
 package io.undertow.examples.security.basic;
 
-import java.security.Principal;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 import io.undertow.Undertow;
 import io.undertow.io.IoCallback;
-import io.undertow.security.idm.Account;
-import io.undertow.security.idm.Credential;
 import io.undertow.security.idm.IdentityManager;
-import io.undertow.security.idm.PasswordCredential;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.util.Headers;
 
 /**
+ * Example of HTTP Basic auth
+ *
+ *
  * @author Stuart Douglas
  */
 public class BasicAuthServer {
 
-    protected static final IdentityManager identityManager;
+    public static void main(final String[] args) {
 
-    static {
-        final Map<String, char[]> users = new HashMap<String, char[]>(2);
+
+        final Map<String, char[]> users = new HashMap<>(2);
         users.put("userOne", "passwordOne".toCharArray());
         users.put("userTwo", "passwordTwo".toCharArray());
 
-        identityManager = new IdentityManager() {
+        final IdentityManager identityManager = new MapIdentityManager(users);
 
-            @Override
-            public Account verify(Account account) {
-                // An existing account so for testing assume still valid.
-                return account;
-            }
-
-            @Override
-            public Account verify(String id, Credential credential) {
-                Account account = getAccount(id);
-                if (account != null && verifyCredential(account, credential)) {
-                    return account;
-                }
-
-                return null;
-            }
-
-            @Override
-            public Account verify(Credential credential) {
-                // TODO Auto-generated method stub
-                return null;
-            }
-
-            private boolean verifyCredential(Account account, Credential credential) {
-                if (credential instanceof PasswordCredential) {
-                    char[] password = ((PasswordCredential) credential).getPassword();
-                    char[] expectedPassword = users.get(account.getPrincipal().getName());
-
-                    return Arrays.equals(password, expectedPassword);
-                }
-                return false;
-            }
-
-            @Override
-            public char[] getPassword(final Account account) {
-                return users.get(account.getPrincipal().getName());
-            }
-
-            @Override
-            public Account getAccount(final String id) {
-                if (users.containsKey(id)) {
-                    return new Account() {
-
-                        private Principal principal = new Principal() {
-
-                            @Override
-                            public String getName() {
-                                return id;
-                            }
-                        };
-
-                        @Override
-                        public Principal getPrincipal() {
-                            return principal;
-                        }
-
-                        @Override
-                        public boolean isUserInGroup(String group) {
-                            return false;
-                        }
-
-                    };
-                }
-                return null;
-            }
-
-        };
-    }
-
-
-    public static void main(final String[] args) {
         Undertow server = Undertow.builder()
                 .addListener(8080, "localhost")
                 .setDefaultHandler(new HttpHandler() {
---
+++ examples/src/main/java/io/undertow/examples/security/basic/MapIdentityManager.java
@@ -0,0 +1,94 @@
+package io.undertow.examples.security.basic;
+
+import java.security.Principal;
+import java.util.Arrays;
+import java.util.Map;
+
+import io.undertow.security.idm.Account;
+import io.undertow.security.idm.Credential;
+import io.undertow.security.idm.IdentityManager;
+import io.undertow.security.idm.PasswordCredential;
+
+/**
+ * A simple {@link IdentityManager} implementation, that just takes a map of users to their
+ * password.
+ *
+ * This is in now way suitable for real world production use.
+ *
+ *
+* @author Stuart Douglas
+*/
+class MapIdentityManager implements IdentityManager {
+
+    private final Map<String, char[]> users;
+
+    public MapIdentityManager(final Map<String, char[]> users) {
+        this.users = users;
+    }
+
+    @Override
+    public Account verify(Account account) {
+        // An existing account so for testing assume still valid.
+        return account;
+    }
+
+    @Override
+    public Account verify(String id, Credential credential) {
+        Account account = getAccount(id);
+        if (account != null && verifyCredential(account, credential)) {
+            return account;
+        }
+
+        return null;
+    }
+
+    @Override
+    public Account verify(Credential credential) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    private boolean verifyCredential(Account account, Credential credential) {
+        if (credential instanceof PasswordCredential) {
+            char[] password = ((PasswordCredential) credential).getPassword();
+            char[] expectedPassword = users.get(account.getPrincipal().getName());
+
+            return Arrays.equals(password, expectedPassword);
+        }
+        return false;
+    }
+
+    @Override
+    public char[] getPassword(final Account account) {
+        return users.get(account.getPrincipal().getName());
+    }
+
+    @Override
+    public Account getAccount(final String id) {
+        if (users.containsKey(id)) {
+            return new Account() {
+
+                private Principal principal = new Principal() {
+
+                    @Override
+                    public String getName() {
+                        return id;
+                    }
+                };
+
+                @Override
+                public Principal getPrincipal() {
+                    return principal;
+                }
+
+                @Override
+                public boolean isUserInGroup(String group) {
+                    return false;
+                }
+
+            };
+        }
+        return null;
+    }
+
+}
