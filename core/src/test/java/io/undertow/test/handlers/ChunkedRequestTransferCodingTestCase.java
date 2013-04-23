5d52164af51000417c4df6f84063431f3252890c
Stuart Douglas <stuart.w.douglas@gmail.com>
2013-04-23 13:34:46 +1000

    Fix test

--- core/src/test/java/io/undertow/test/handlers/ChunkedRequestTransferCodingTestCase.java
+++ core/src/test/java/io/undertow/test/handlers/ChunkedRequestTransferCodingTestCase.java
@@ -125,7 +125,7 @@ public class ChunkedRequestTransferCodingTestCase {
                     public void writeTo(OutputStream outstream) throws IOException {
                         int l = 0;
                         int i = 0;
-                        while (i < message.length()) {
+                        while (i <= message.length()) {
                             i += random.nextInt(1000);
                             i = Math.min(i, message.length());
                             outstream.write(message.getBytes(), l, i - l);
