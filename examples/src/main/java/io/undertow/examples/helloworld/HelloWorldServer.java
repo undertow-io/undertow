6e4c5462fd3a609772b0e8be0845c5e0928a0158
Stuart Douglas <stuart.w.douglas@gmail.com>
2013-05-17 14:54:48 +1000

    Unused imports

--- examples/src/main/java/io/undertow/examples/helloworld/HelloWorldServer.java
+++ examples/src/main/java/io/undertow/examples/helloworld/HelloWorldServer.java
@@ -2,10 +2,8 @@ package io.undertow.examples.helloworld;
 
 import io.undertow.Undertow;
 import io.undertow.examples.UndertowExample;
-import io.undertow.io.IoCallback;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
-import io.undertow.util.Headers;
 
 /**
  * @author Stuart Douglas
