82f0ec8758b8c5904a0a4ee9cee03a2322442fe5
Stuart Douglas <stuart.w.douglas@gmail.com>
2013-05-20 08:45:55 +1000

    Add handlers that allow or disallow requests based on the HTTP method

---
+++ core/src/main/java/io/undertow/server/handlers/AllowedMethodsHandler.java
@@ -0,0 +1,43 @@
+package io.undertow.server.handlers;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.util.HttpString;
+import io.undertow.util.StatusCodes;
+
+/**
+ * Handler that whitelists certain HTTP methods. Only requests with a method in
+ * the allowed methods set will be allowed to continue.
+ *
+ * @author Stuart Douglas
+ */
+public class AllowedMethodsHandler implements HttpHandler {
+
+    private final Set<HttpString> allowedMethods;
+    private final HttpHandler next;
+
+    public AllowedMethodsHandler(final HttpHandler next, final Set<HttpString> allowedMethods) {
+        this.allowedMethods = new HashSet<>(allowedMethods);
+        this.next = next;
+    }
+
+    public AllowedMethodsHandler(final HttpHandler next, final HttpString... allowedMethods) {
+        this.allowedMethods = new HashSet<>(Arrays.asList(allowedMethods));
+        this.next = next;
+    }
+
+    @Override
+    public void handleRequest(final HttpServerExchange exchange) throws Exception {
+        if (allowedMethods.contains(exchange.getRequestMethod())) {
+            next.handleRequest(exchange);
+        } else {
+            exchange.setResponseCode(StatusCodes.METHOD_NOT_ALLOWED);
+            exchange.endExchange();
+        }
+    }
+
+}
---
+++ core/src/main/java/io/undertow/server/handlers/DisallowedMethodsHandler.java
@@ -0,0 +1,43 @@
+package io.undertow.server.handlers;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.util.HttpString;
+import io.undertow.util.StatusCodes;
+
+/**
+ * Handler that blacklists certain HTTP methods.
+ *
+ * @author Stuart Douglas
+ */
+public class DisallowedMethodsHandler implements HttpHandler {
+
+    private final Set<HttpString> disallowedMethods;
+    private final HttpHandler next;
+
+    public DisallowedMethodsHandler(final HttpHandler next, final Set<HttpString> disallowedMethods) {
+        this.disallowedMethods = new HashSet<>(disallowedMethods);
+        this.next = next;
+    }
+
+
+    public DisallowedMethodsHandler(final HttpHandler next, final HttpString... disallowedMethods) {
+        this.disallowedMethods = new HashSet<>(Arrays.asList(disallowedMethods));
+        this.next = next;
+    }
+
+    @Override
+    public void handleRequest(final HttpServerExchange exchange) throws Exception {
+        if (disallowedMethods.contains(exchange.getRequestMethod())) {
+            exchange.setResponseCode(StatusCodes.METHOD_NOT_ALLOWED);
+            exchange.endExchange();
+        } else {
+            next.handleRequest(exchange);
+        }
+    }
+
+}
