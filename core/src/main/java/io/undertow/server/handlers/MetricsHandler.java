531aef15c6c60381574f3d04f6237fa8e1bcf8ac
Stuart Douglas <stuart.w.douglas@gmail.com>
2013-07-08 08:42:49 +1000

    Add metrics handler

---
+++ core/src/main/java/io/undertow/server/handlers/MetricsHandler.java
@@ -0,0 +1,122 @@
+package io.undertow.server.handlers;
+
+import io.undertow.server.ExchangeCompletionListener;
+import io.undertow.server.HandlerWrapper;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+
+import java.util.Date;
+import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
+import java.util.concurrent.atomic.AtomicLongFieldUpdater;
+
+/**
+ * Handler that records some metrics
+ *
+ * @author Stuart Douglas
+ */
+public class MetricsHandler implements HttpHandler {
+
+    public static final HandlerWrapper WRAPPER = new HandlerWrapper() {
+        @Override
+        public HttpHandler wrap(HttpHandler handler) {
+            return new MetricsHandler(handler);
+        }
+    };
+
+    private volatile MetricResult totalResult = new MetricResult(new Date());
+    private final HttpHandler next;
+
+    public MetricsHandler(HttpHandler next) {
+        this.next = next;
+    }
+
+    @Override
+    public void handleRequest(HttpServerExchange exchange) throws Exception {
+        final long start = System.currentTimeMillis();
+        exchange.addExchangeCompleteListener(new ExchangeCompletionListener() {
+            @Override
+            public void exchangeEvent(HttpServerExchange exchange, NextListener nextListener) {
+                long time = System.currentTimeMillis() - start;
+                totalResult.update((int)time);
+                nextListener.proceed();
+            }
+        });
+    }
+
+    public void reset() {
+        this.totalResult = new MetricResult(new Date());
+    }
+
+    public MetricResult getMetrics() {
+        return new MetricResult(this.totalResult);
+    }
+
+    public static class MetricResult {
+
+        private static final AtomicLongFieldUpdater<MetricResult> totalRequestTimeUpdater = AtomicLongFieldUpdater.newUpdater(MetricResult.class, "totalRequestTime");
+        private static final AtomicIntegerFieldUpdater<MetricResult> maxRequestTimeUpdater = AtomicIntegerFieldUpdater.newUpdater(MetricResult.class, "maxRequestTime");
+        private static final AtomicIntegerFieldUpdater<MetricResult> minRequestTimeUpdater = AtomicIntegerFieldUpdater.newUpdater(MetricResult.class, "minRequestTime");
+        private static final AtomicLongFieldUpdater<MetricResult> invocationsUpdater = AtomicLongFieldUpdater.newUpdater(MetricResult.class, "totalRequests");
+
+        private final Date metricsStartDate;
+
+        private volatile long totalRequestTime;
+        private volatile int maxRequestTime;
+        private volatile int minRequestTime = -1;
+        private volatile long totalRequests;
+
+        public MetricResult(Date metricsStartDate) {
+            this.metricsStartDate = metricsStartDate;
+        }
+
+        public MetricResult(MetricResult copy) {
+            this.metricsStartDate = copy.metricsStartDate;
+            this.totalRequestTime = copy.totalRequestTime;
+            this.maxRequestTime = copy.maxRequestTime;
+            this.minRequestTime = copy.minRequestTime;
+            this.totalRequests = copy.totalRequests;
+        }
+
+        void update(final int requestTime) {
+            totalRequestTimeUpdater.addAndGet(this, requestTime);
+            int maxRequestTime;
+            do {
+                maxRequestTime = this.maxRequestTime;
+                if (requestTime < maxRequestTime) {
+                    break;
+                }
+            } while (!maxRequestTimeUpdater.compareAndSet(this, maxRequestTime, requestTime));
+
+            int minRequestTime;
+            do {
+                minRequestTime = this.minRequestTime;
+                if (requestTime > minRequestTime && minRequestTime != -1) {
+                    break;
+                }
+            } while (!minRequestTimeUpdater.compareAndSet(this, minRequestTime, requestTime));
+            invocationsUpdater.incrementAndGet(this);
+
+
+        }
+
+        public Date getMetricsStartDate() {
+            return metricsStartDate;
+        }
+
+        public long getTotalRequestTime() {
+            return totalRequestTime;
+        }
+
+        public int getMaxRequestTime() {
+            return maxRequestTime;
+        }
+
+        public int getMinRequestTime() {
+            return minRequestTime;
+        }
+
+        public long getTotalRequests() {
+            return totalRequests;
+        }
+    }
+}
---
+++ core/src/test/java/io/undertow/server/handlers/MetricsHandlerTestCase.java
@@ -0,0 +1,63 @@
+package io.undertow.server.handlers;
+
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.testutils.DefaultServer;
+import io.undertow.testutils.HttpClientUtils;
+import io.undertow.testutils.TestHttpClient;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.IOException;
+
+/**
+ * @author Stuart Douglas
+ */
+@RunWith(DefaultServer.class)
+public class MetricsHandlerTestCase {
+
+    private static MetricsHandler metricsHandler;
+
+    @BeforeClass
+    public static void setup() {
+        DefaultServer.setRootHandler(metricsHandler = new MetricsHandler(new HttpHandler() {
+            @Override
+            public void handleRequest(HttpServerExchange exchange) throws Exception {
+                Thread.sleep(10);
+            }
+        }));
+    }
+
+    @Test
+    public void testMetrics() throws IOException, InterruptedException {
+        HttpGet get = new HttpGet(DefaultServer.getDefaultServerURL() + "/path");
+        TestHttpClient client = new TestHttpClient();
+        try {
+            HttpResponse result = client.execute(get);
+            Assert.assertEquals(200, result.getStatusLine().getStatusCode());
+            HttpClientUtils.readResponse(result);
+
+            MetricsHandler.MetricResult metrics = metricsHandler.getMetrics();
+            Assert.assertEquals(1, metrics.getTotalRequests());
+            Assert.assertTrue(metrics.getMaxRequestTime() > 0);
+            Assert.assertEquals(metrics.getMinRequestTime(), metrics.getMaxRequestTime());
+            Assert.assertEquals(metrics.getMaxRequestTime(), metrics.getTotalRequestTime());
+
+            result = client.execute(get);
+            Assert.assertEquals(200, result.getStatusLine().getStatusCode());
+            HttpClientUtils.readResponse(result);
+
+            metrics = metricsHandler.getMetrics();
+            Assert.assertEquals(2, metrics.getTotalRequests());
+
+        } finally {
+
+            client.getConnectionManager().shutdown();
+        }
+    }
+
+}
