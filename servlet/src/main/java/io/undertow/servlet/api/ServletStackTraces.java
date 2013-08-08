72162d49f08e0e0b2de129f10999788429194e61
Stuart Douglas <stuart.w.douglas@gmail.com>
2013-08-08 10:48:36 +0200

    Add string representation to enum

--- servlet/src/main/java/io/undertow/servlet/api/ServletStackTraces.java
+++ servlet/src/main/java/io/undertow/servlet/api/ServletStackTraces.java
@@ -5,7 +5,18 @@ package io.undertow.servlet.api;
  */
 public enum ServletStackTraces {
 
-    NONE,
-    LOCAL_ONLY,
-    ALL
+    NONE("none"),
+    LOCAL_ONLY("local-only"),
+    ALL("all");
+
+    private final String value;
+
+    private ServletStackTraces(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
 }
