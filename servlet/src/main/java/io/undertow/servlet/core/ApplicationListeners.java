1d9b3c47f3372537c617e676029f69bf8dcd913e
Stuart Douglas <stuart.w.douglas@gmail.com>
2013-09-05 10:29:31 +0200

    Change to using arrays instead of lists for performance

--- servlet/src/main/java/io/undertow/servlet/core/ApplicationListeners.java
+++ servlet/src/main/java/io/undertow/servlet/core/ApplicationListeners.java
@@ -18,9 +18,7 @@
 
 package io.undertow.servlet.core;
 
-import java.util.List;
-import java.util.ListIterator;
-import java.util.concurrent.CopyOnWriteArrayList;
+import io.undertow.servlet.UndertowServletLogger;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextAttributeEvent;
@@ -39,8 +37,8 @@ import javax.servlet.http.HttpSessionBindingEvent;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionIdListener;
 import javax.servlet.http.HttpSessionListener;
-
-import io.undertow.servlet.UndertowServletLogger;
+import java.util.ArrayList;
+import java.util.List;
 
 import static io.undertow.servlet.core.ApplicationListeners.ListenerState.DECLARED_LISTENER;
 import static io.undertow.servlet.core.ApplicationListeners.ListenerState.PROGRAMATIC_LISTENER;
@@ -50,11 +48,15 @@ import static io.undertow.servlet.core.ApplicationListeners.ListenerState.PROGRA
  * <p/>
  * This class does not perform any context setup, the context must be setup
  * before invoking this class.
+ * <p/>
+ * Note that arrays are used instead of lists for performance reasons.
  *
  * @author Stuart Douglas
  */
 public class ApplicationListeners implements Lifecycle {
 
+
+    private static final ManagedListener[] EMPTY = {};
 
     private static final Class[] LISTENER_CLASSES = {ServletContextListener.class,
             ServletContextAttributeListener.class,
@@ -71,27 +73,26 @@ public class ApplicationListeners implements Lifecycle {
         }
     };
 
-    private final ServletContext servletContext;
-    private final List<ManagedListener> allListeners;
-    private final List<ManagedListener> servletContextListeners;
-    private final List<ManagedListener> servletContextAttributeListeners;
-    private final List<ManagedListener> servletRequestListeners;
-    private final List<ManagedListener> servletRequestAttributeListeners;
-    private final List<ManagedListener> httpSessionListeners;
-    private final List<ManagedListener> httpSessionAttributeListeners;
-    private final List<ManagedListener> httpSessionIdListeners;
+    private ServletContext servletContext;
+    private final List<ManagedListener> allListeners = new ArrayList<ManagedListener>();
+    private ManagedListener[] servletContextListeners;
+    private ManagedListener[] servletContextAttributeListeners;
+    private ManagedListener[] servletRequestListeners;
+    private ManagedListener[] servletRequestAttributeListeners;
+    private ManagedListener[] httpSessionListeners;
+    private ManagedListener[] httpSessionAttributeListeners;
+    private ManagedListener[] httpSessionIdListeners;
     private volatile boolean started = false;
 
     public ApplicationListeners(final List<ManagedListener> allListeners, final ServletContext servletContext) {
         this.servletContext = servletContext;
-        servletContextListeners = new CopyOnWriteArrayList<ManagedListener>();
-        servletContextAttributeListeners = new CopyOnWriteArrayList<ManagedListener>();
-        servletRequestListeners = new CopyOnWriteArrayList<ManagedListener>();
-        servletRequestAttributeListeners = new CopyOnWriteArrayList<ManagedListener>();
-        httpSessionListeners = new CopyOnWriteArrayList<ManagedListener>();
-        httpSessionAttributeListeners = new CopyOnWriteArrayList<ManagedListener>();
-        httpSessionIdListeners = new CopyOnWriteArrayList<ManagedListener>();
-        this.allListeners = new CopyOnWriteArrayList<ManagedListener>();
+        servletContextListeners = EMPTY;
+        servletContextAttributeListeners = EMPTY;
+        servletRequestListeners = EMPTY;
+        servletRequestAttributeListeners = EMPTY;
+        httpSessionListeners = EMPTY;
+        httpSessionAttributeListeners = EMPTY;
+        httpSessionIdListeners = EMPTY;
         for (final ManagedListener listener : allListeners) {
             addListener(listener);
         }
@@ -99,25 +100,47 @@ public class ApplicationListeners implements Lifecycle {
 
     public void addListener(final ManagedListener listener) {
         if (ServletContextListener.class.isAssignableFrom(listener.getListenerInfo().getListenerClass())) {
-            servletContextListeners.add(listener);
+            ManagedListener[] old = servletContextListeners;
+            servletContextListeners = new ManagedListener[old.length + 1];
+            System.arraycopy(old, 0, servletContextListeners, 0, old.length);
+            servletContextListeners[old.length] = listener;
         }
         if (ServletContextAttributeListener.class.isAssignableFrom(listener.getListenerInfo().getListenerClass())) {
-            servletContextAttributeListeners.add(listener);
+
+            ManagedListener[] old = servletContextAttributeListeners;
+            servletContextAttributeListeners = new ManagedListener[old.length + 1];
+            System.arraycopy(old, 0, servletContextAttributeListeners, 0, old.length);
+            servletContextAttributeListeners[old.length] = listener;
         }
         if (ServletRequestListener.class.isAssignableFrom(listener.getListenerInfo().getListenerClass())) {
-            servletRequestListeners.add(listener);
+            ManagedListener[] old = servletRequestListeners;
+            servletRequestListeners = new ManagedListener[old.length + 1];
+            System.arraycopy(old, 0, servletRequestListeners, 0, old.length);
+            servletRequestListeners[old.length] = listener;
         }
         if (ServletRequestAttributeListener.class.isAssignableFrom(listener.getListenerInfo().getListenerClass())) {
-            servletRequestAttributeListeners.add(listener);
+            ManagedListener[] old = servletRequestAttributeListeners;
+            servletRequestAttributeListeners = new ManagedListener[old.length + 1];
+            System.arraycopy(old, 0, servletRequestAttributeListeners, 0, old.length);
+            servletRequestAttributeListeners[old.length] = listener;
         }
         if (HttpSessionListener.class.isAssignableFrom(listener.getListenerInfo().getListenerClass())) {
-            httpSessionListeners.add(listener);
+            ManagedListener[] old = httpSessionListeners;
+            httpSessionListeners = new ManagedListener[old.length + 1];
+            System.arraycopy(old, 0, httpSessionListeners, 0, old.length);
+            httpSessionListeners[old.length] = listener;
         }
         if (HttpSessionAttributeListener.class.isAssignableFrom(listener.getListenerInfo().getListenerClass())) {
-            httpSessionAttributeListeners.add(listener);
+            ManagedListener[] old = httpSessionAttributeListeners;
+            httpSessionAttributeListeners = new ManagedListener[old.length + 1];
+            System.arraycopy(old, 0, httpSessionAttributeListeners, 0, old.length);
+            httpSessionAttributeListeners[old.length] = listener;
         }
         if (HttpSessionIdListener.class.isAssignableFrom(listener.getListenerInfo().getListenerClass())) {
-            httpSessionIdListeners.add(listener);
+            ManagedListener[] old = httpSessionIdListeners;
+            httpSessionIdListeners = new ManagedListener[old.length + 1];
+            System.arraycopy(old, 0, httpSessionIdListeners, 0, old.length);
+            httpSessionIdListeners[old.length] = listener;
         }
         this.allListeners.add(listener);
     }
@@ -143,8 +166,8 @@ public class ApplicationListeners implements Lifecycle {
     public void contextInitialized() {
         //new listeners can be added here, so we don't use an iterator
         final ServletContextEvent event = new ServletContextEvent(servletContext);
-        for (int i = 0; i < servletContextListeners.size(); ++i) {
-            ManagedListener listener = servletContextListeners.get(i);
+        for (int i = 0; i < servletContextListeners.length; ++i) {
+            ManagedListener listener = servletContextListeners[i];
             IN_PROGRAMATIC_SC_LISTENER_INVOCATION.set(listener.isProgramatic() ? PROGRAMATIC_LISTENER : DECLARED_LISTENER);
             try {
                 this.<ServletContextListener>get(listener).contextInitialized(event);
@@ -156,8 +179,8 @@ public class ApplicationListeners implements Lifecycle {
 
     public void contextDestroyed() {
         final ServletContextEvent event = new ServletContextEvent(servletContext);
-        for (ListIterator<ManagedListener> iterator = getReturningListIterator(servletContextListeners); iterator.hasPrevious(); ) {
-            ManagedListener listener = iterator.previous();
+        for (int i = servletContextListeners.length - 1; i >= 0; --i) {
+            ManagedListener listener = servletContextListeners[i];
             try {
                 this.<ServletContextListener>get(listener).contextDestroyed(event);
             } catch (Exception e) {
@@ -168,36 +191,36 @@ public class ApplicationListeners implements Lifecycle {
 
     public void servletContextAttributeAdded(final String name, final Object value) {
         final ServletContextAttributeEvent sre = new ServletContextAttributeEvent(servletContext, name, value);
-        for (final ManagedListener listener : servletContextAttributeListeners) {
-            this.<ServletContextAttributeListener>get(listener).attributeAdded(sre);
+        for (int i = 0; i < servletContextAttributeListeners.length; ++i) {
+            this.<ServletContextAttributeListener>get(servletContextAttributeListeners[i]).attributeAdded(sre);
         }
     }
 
     public void servletContextAttributeRemoved(final String name, final Object value) {
         final ServletContextAttributeEvent sre = new ServletContextAttributeEvent(servletContext, name, value);
-        for (final ManagedListener listener : servletContextAttributeListeners) {
-            this.<ServletContextAttributeListener>get(listener).attributeRemoved(sre);
+        for (int i = 0; i < servletContextAttributeListeners.length; ++i) {
+            this.<ServletContextAttributeListener>get(servletContextAttributeListeners[i]).attributeRemoved(sre);
         }
     }
 
     public void servletContextAttributeReplaced(final String name, final Object value) {
         final ServletContextAttributeEvent sre = new ServletContextAttributeEvent(servletContext, name, value);
-        for (final ManagedListener listener : servletContextAttributeListeners) {
-            this.<ServletContextAttributeListener>get(listener).attributeReplaced(sre);
+        for (int i = 0; i < servletContextAttributeListeners.length; ++i) {
+            this.<ServletContextAttributeListener>get(servletContextAttributeListeners[i]).attributeReplaced(sre);
         }
     }
 
     public void requestInitialized(final ServletRequest request) {
         final ServletRequestEvent sre = new ServletRequestEvent(servletContext, request);
-        for (final ManagedListener listener : servletRequestListeners) {
-            this.<ServletRequestListener>get(listener).requestInitialized(sre);
+        for (int i = 0; i < servletRequestListeners.length; ++i) {
+            this.<ServletRequestListener>get(servletRequestListeners[i]).requestInitialized(sre);
         }
     }
 
     public void requestDestroyed(final ServletRequest request) {
         final ServletRequestEvent sre = new ServletRequestEvent(servletContext, request);
-        for (ListIterator<ManagedListener> iterator = getReturningListIterator(servletRequestListeners); iterator.hasPrevious(); ) {
-            ManagedListener listener = iterator.previous();
+        for (int i = servletRequestListeners.length - 1; i >= 0; --i) {
+            ManagedListener listener = servletRequestListeners[i];
             try {
                 this.<ServletRequestListener>get(listener).requestDestroyed(sre);
             } catch (Exception e) {
@@ -208,74 +231,70 @@ public class ApplicationListeners implements Lifecycle {
 
     public void servletRequestAttributeAdded(final HttpServletRequest request, final String name, final Object value) {
         final ServletRequestAttributeEvent sre = new ServletRequestAttributeEvent(servletContext, request, name, value);
-        for (final ManagedListener listener : servletRequestAttributeListeners) {
-            this.<ServletRequestAttributeListener>get(listener).attributeAdded(sre);
+        for (int i = 0; i < servletRequestAttributeListeners.length; ++i) {
+            this.<ServletRequestAttributeListener>get(servletRequestAttributeListeners[i]).attributeAdded(sre);
         }
     }
 
     public void servletRequestAttributeRemoved(final HttpServletRequest request, final String name, final Object value) {
         final ServletRequestAttributeEvent sre = new ServletRequestAttributeEvent(servletContext, request, name, value);
-        for (final ManagedListener listener : servletRequestAttributeListeners) {
-            this.<ServletRequestAttributeListener>get(listener).attributeRemoved(sre);
+        for (int i = 0; i < servletRequestAttributeListeners.length; ++i) {
+            this.<ServletRequestAttributeListener>get(servletRequestAttributeListeners[i]).attributeRemoved(sre);
         }
     }
 
     public void servletRequestAttributeReplaced(final HttpServletRequest request, final String name, final Object value) {
         final ServletRequestAttributeEvent sre = new ServletRequestAttributeEvent(servletContext, request, name, value);
-        for (final ManagedListener listener : servletRequestAttributeListeners) {
-            this.<ServletRequestAttributeListener>get(listener).attributeReplaced(sre);
+        for (int i = 0; i < servletRequestAttributeListeners.length; ++i) {
+            this.<ServletRequestAttributeListener>get(servletRequestAttributeListeners[i]).attributeReplaced(sre);
         }
     }
 
     public void sessionCreated(final HttpSession session) {
         final HttpSessionEvent sre = new HttpSessionEvent(session);
-        for (final ManagedListener listener : httpSessionListeners) {
-            this.<HttpSessionListener>get(listener).sessionCreated(sre);
+        for (int i = 0; i < httpSessionListeners.length; ++i) {
+            this.<HttpSessionListener>get(httpSessionListeners[i]).sessionCreated(sre);
         }
     }
 
     public void sessionDestroyed(final HttpSession session) {
         final HttpSessionEvent sre = new HttpSessionEvent(session);
-        for (ListIterator<ManagedListener> iterator = getReturningListIterator(httpSessionListeners); iterator.hasPrevious(); ) {
-            ManagedListener listener = iterator.previous();
+        for (int i = httpSessionListeners.length - 1; i >= 0; --i) {
+            ManagedListener listener = httpSessionListeners[i];
             this.<HttpSessionListener>get(listener).sessionDestroyed(sre);
         }
     }
 
     public void httpSessionAttributeAdded(final HttpSession session, final String name, final Object value) {
         final HttpSessionBindingEvent sre = new HttpSessionBindingEvent(session, name, value);
-        for (final ManagedListener listener : httpSessionAttributeListeners) {
-            this.<HttpSessionAttributeListener>get(listener).attributeAdded(sre);
+        for (int i = 0; i < httpSessionAttributeListeners.length; ++i) {
+            this.<HttpSessionAttributeListener>get(httpSessionAttributeListeners[i]).attributeAdded(sre);
         }
     }
 
     public void httpSessionAttributeRemoved(final HttpSession session, final String name, final Object value) {
         final HttpSessionBindingEvent sre = new HttpSessionBindingEvent(session, name, value);
-        for (final ManagedListener listener : httpSessionAttributeListeners) {
-            this.<HttpSessionAttributeListener>get(listener).attributeRemoved(sre);
+        for (int i = 0; i < httpSessionAttributeListeners.length; ++i) {
+            this.<HttpSessionAttributeListener>get(httpSessionAttributeListeners[i]).attributeRemoved(sre);
         }
     }
 
     public void httpSessionAttributeReplaced(final HttpSession session, final String name, final Object value) {
         final HttpSessionBindingEvent sre = new HttpSessionBindingEvent(session, name, value);
-        for (final ManagedListener listener : httpSessionAttributeListeners) {
-            this.<HttpSessionAttributeListener>get(listener).attributeReplaced(sre);
+        for (int i = 0; i < httpSessionAttributeListeners.length; ++i) {
+            this.<HttpSessionAttributeListener>get(httpSessionAttributeListeners[i]).attributeReplaced(sre);
         }
     }
 
     public void httpSessionIdChanged(final HttpSession session, final String oldSessionId) {
         final HttpSessionEvent sre = new HttpSessionEvent(session);
-        for (final ManagedListener listener : httpSessionIdListeners) {
-            this.<HttpSessionIdListener>get(listener).sessionIdChanged(sre, oldSessionId);
+        for (int i = 0; i < httpSessionIdListeners.length; ++i) {
+            this.<HttpSessionIdListener>get(httpSessionIdListeners[i]).sessionIdChanged(sre, oldSessionId);
         }
     }
 
     private <T> T get(final ManagedListener listener) {
         return (T) listener.instance();
-    }
-
-    private <T> ListIterator<T> getReturningListIterator(List<T> list) {
-        return list.listIterator(list.size());
     }
 
     /**
