0c159ca36700bee14a3f675f3435f95180609edf
Darran Lofthouse <darran.lofthouse@jboss.com>
2013-11-20 17:59:39 +0000

    [UNDERTOW-143] Test for SPNEGO authentication in issolation.
    To be continued...

---
+++ core/src/test/java/io/undertow/server/security/SpnegoAuthenticationTestCase.java
@@ -0,0 +1,47 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2013 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.undertow.server.security;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * A test case to test the SPNEGO authentication mechanism.
+ *
+ * @author <a href="mailto:darran.lofthouse@jboss.com">Darran Lofthouse</a>
+ */
+public class SpnegoAuthenticationTestCase {
+
+    @BeforeClass
+    public static void startServers() {
+
+    }
+
+    @AfterClass
+    public static void stopServers() {
+
+    }
+
+
+    @Test
+    public void test() {
+
+    }
+
+}
