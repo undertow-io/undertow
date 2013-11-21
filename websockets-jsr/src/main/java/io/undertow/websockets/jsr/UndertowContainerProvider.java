1dc678275bb794fec7e0d2680ac5bc3b2633d850
Stuart Douglas <stuart.w.douglas@gmail.com>
2013-11-21 10:22:29 +0000

    UNDERTOW-123 Add default web socket container usable in a SE environment

--- websockets-jsr/src/main/java/io/undertow/websockets/jsr/UndertowContainerProvider.java
+++ websockets-jsr/src/main/java/io/undertow/websockets/jsr/UndertowContainerProvider.java
@@ -1,7 +1,22 @@
-package io.undertow.websockets.jsr;import javax.websocket.ContainerProvider;
+package io.undertow.websockets.jsr;
+
+import io.undertow.servlet.api.ThreadSetupAction;
+import io.undertow.servlet.core.CompositeThreadSetupAction;
+import io.undertow.servlet.util.DefaultClassIntrospector;
+import org.xnio.ByteBufferSlicePool;
+import org.xnio.OptionMap;
+import org.xnio.Options;
+import org.xnio.Pool;
+import org.xnio.Xnio;
+import org.xnio.XnioWorker;
+
+import javax.websocket.ContainerProvider;
 import javax.websocket.WebSocketContainer;
+import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.security.AccessController;
 import java.security.PrivilegedAction;
+import java.util.Collections;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -13,6 +28,8 @@ public class UndertowContainerProvider extends ContainerProvider {
     private static final RuntimePermission PERMISSION = new RuntimePermission("io.undertow.websockets.jsr.MODIFY_WEBSOCKET_CONTAINER");
 
     private static final Map<ClassLoader, WebSocketContainer> webSocketContainers = new ConcurrentHashMap<ClassLoader, WebSocketContainer>();
+
+    private static volatile WebSocketContainer defaultContainer;
 
     @Override
     protected WebSocketContainer getContainer() {
@@ -27,7 +44,32 @@ public class UndertowContainerProvider extends ContainerProvider {
                 }
             });
         }
-        return webSocketContainers.get(tccl);
+        WebSocketContainer webSocketContainer = webSocketContainers.get(tccl);
+        if(webSocketContainer == null) {
+            return getDefaultContainer();
+        }
+        return webSocketContainer;
+    }
+
+    private WebSocketContainer getDefaultContainer() {
+        if(defaultContainer != null) {
+            return defaultContainer;
+        }
+        synchronized (UndertowContainerProvider.class) {
+            if(defaultContainer == null) {
+                try {
+                    //this is not great, as we have no way to control the lifecycle
+                    //but there is not much we can do
+                    //todo: what options should we use here?
+                    XnioWorker worker = Xnio.getInstance().createWorker(OptionMap.create(Options.THREAD_DAEMON, true));
+                    Pool<ByteBuffer> buffers = new ByteBufferSlicePool(1024, 10240);
+                    defaultContainer = new ServerWebSocketContainer(DefaultClassIntrospector.INSTANCE, worker, buffers, new CompositeThreadSetupAction(Collections.<ThreadSetupAction>emptyList()));
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return defaultContainer;
+        }
     }
 
     public static void addContainer(final ClassLoader classLoader, final WebSocketContainer webSocketContainer) {
