b62822238ca3307133ebdf6c360f9d2f583584eb
Stuart Douglas <stuart.w.douglas@gmail.com>
2013-12-10 08:38:09 +0100

    UNDERTOW-160 Mark strings as being able to be encoded automatically

--- websockets-jsr/src/main/java/io/undertow/websockets/jsr/EncodingFactory.java
+++ websockets-jsr/src/main/java/io/undertow/websockets/jsr/EncodingFactory.java
@@ -277,6 +277,7 @@ public class EncodingFactory {
                 clazz == Integer.class ||
                 clazz == Long.class ||
                 clazz == Float.class ||
-                clazz == Double.class; //we don't care about void
+                clazz == Double.class ||
+                clazz == String.class; //we don't care about void
     }
 }
