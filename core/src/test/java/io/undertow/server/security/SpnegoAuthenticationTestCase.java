05a62375ec903f1ac661f720cbb4cc58359b6d86
Darran Lofthouse <darran.lofthouse@jboss.com>
2013-12-12 18:28:51 +0000

    [UNDERTOW-151] No longer needs to be ignored for AJP testing.

--- core/src/test/java/io/undertow/server/security/SpnegoAuthenticationTestCase.java
+++ core/src/test/java/io/undertow/server/security/SpnegoAuthenticationTestCase.java
@@ -29,7 +29,6 @@ import io.undertow.security.api.AuthenticationMechanism;
 import io.undertow.security.api.GSSAPIServerSubjectFactory;
 import io.undertow.security.api.SecurityNotification.EventType;
 import io.undertow.security.impl.GSSAPIAuthenticationMechanism;
-import io.undertow.testutils.AjpIgnore;
 import io.undertow.testutils.DefaultServer;
 import io.undertow.testutils.HttpClientUtils;
 import io.undertow.testutils.TestHttpClient;
@@ -60,7 +59,6 @@ import org.junit.runner.RunWith;
  * @author <a href="mailto:darran.lofthouse@jboss.com">Darran Lofthouse</a>
  */
 @RunWith(DefaultServer.class)
-@AjpIgnore
 public class SpnegoAuthenticationTestCase extends AuthenticationTestBase {
 
     private static Oid SPNEGO;
