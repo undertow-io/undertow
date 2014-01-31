f5bea5c8c5dcabacb2f847ce5d0e8cbc4978d3f6
Stuart Douglas <stuart.w.douglas@gmail.com>
2014-01-31 09:32:17 +0100

    Remove duplicate logic in SessionCookieConfigImpl

--- servlet/src/main/java/io/undertow/servlet/spec/SessionCookieConfigImpl.java
+++ servlet/src/main/java/io/undertow/servlet/spec/SessionCookieConfigImpl.java
@@ -18,35 +18,24 @@
 
 package io.undertow.servlet.spec;
 
-import java.util.Map;
-
-import javax.servlet.SessionCookieConfig;
-
 import io.undertow.server.HttpServerExchange;
-import io.undertow.server.handlers.Cookie;
-import io.undertow.server.handlers.CookieImpl;
 import io.undertow.server.session.SessionConfig;
 import io.undertow.servlet.UndertowServletMessages;
+
+import javax.servlet.SessionCookieConfig;
 
 /**
  * @author Stuart Douglas
  */
 public class SessionCookieConfigImpl implements SessionCookieConfig, SessionConfig {
 
-    public static final String DEFAULT_SESSION_ID = "JSESSIONID";
-
     private final ServletContextImpl servletContext;
-    private String name = DEFAULT_SESSION_ID;
-    private String path = "/";
-    private String domain;
-    private boolean secure;
-    private boolean httpOnly;
-    private int maxAge;
-    private String comment;
+    private final io.undertow.server.session.SessionCookieConfig delegate;
     private SessionConfig fallback;
 
     public SessionCookieConfigImpl(final ServletContextImpl servletContext) {
         this.servletContext = servletContext;
+        this.delegate = new io.undertow.server.session.SessionCookieConfig();
     }
 
     @Override
@@ -56,37 +45,19 @@ public class SessionCookieConfigImpl implements SessionCookieConfig, SessionConf
 
     @Override
     public void setSessionId(final HttpServerExchange exchange, final String sessionId) {
-        Cookie cookie = new CookieImpl(name, sessionId)
-                .setPath(path)
-                .setDomain(domain)
-                .setSecure(secure)
-                .setHttpOnly(httpOnly)
-                .setComment(comment);
-        if (maxAge > 0) {
-            cookie.setMaxAge(maxAge);
-        }
-        exchange.setResponseCookie(cookie);
+        delegate.setSessionId(exchange, sessionId);
     }
 
     @Override
     public void clearSession(final HttpServerExchange exchange, final String sessionId) {
-        Cookie cookie = new CookieImpl(name, sessionId)
-                .setPath(path)
-                .setDomain(domain)
-                .setSecure(secure)
-                .setHttpOnly(httpOnly)
-                .setMaxAge(0);
-        exchange.setResponseCookie(cookie);
+        delegate.clearSession(exchange, sessionId);
     }
 
     @Override
     public String findSessionId(final HttpServerExchange exchange) {
-        Map<String, Cookie> cookies = exchange.getRequestCookies();
-        if (cookies != null) {
-            Cookie sessionId = cookies.get(name);
-            if (sessionId != null) {
-                return sessionId.getValue();
-            }
+        String existing = delegate.findSessionId(exchange);
+        if(existing != null) {
+            return existing;
         }
         if(fallback != null) {
             return fallback.findSessionId(exchange);
@@ -96,12 +67,9 @@ public class SessionCookieConfigImpl implements SessionCookieConfig, SessionConf
 
     @Override
     public SessionCookieSource sessionCookieSource(HttpServerExchange exchange) {
-        Map<String, Cookie> cookies = exchange.getRequestCookies();
-        if (cookies != null) {
-            Cookie sessionId = cookies.get(name);
-            if (sessionId != null) {
-                return SessionCookieSource.COOKIE;
-            }
+        String existing = delegate.findSessionId(exchange);
+        if (existing != null) {
+            return SessionCookieSource.COOKIE;
         }
         if(fallback != null) {
             String id =  fallback.findSessionId(exchange);
@@ -111,80 +79,80 @@ public class SessionCookieConfigImpl implements SessionCookieConfig, SessionConf
     }
 
     public String getName() {
-        return name;
+        return delegate.getCookieName();
     }
 
     public void setName(final String name) {
         if(servletContext.isInitialized()) {
             throw UndertowServletMessages.MESSAGES.servletContextAlreadyInitialized();
         }
-        this.name = name;
+        delegate.setCookieName(name);
     }
 
     public String getDomain() {
-        return domain;
+        return delegate.getDomain();
     }
 
     public void setDomain(final String domain) {
         if(servletContext.isInitialized()) {
             throw UndertowServletMessages.MESSAGES.servletContextAlreadyInitialized();
         }
-        this.domain = domain;
+        delegate.setDomain(domain);
     }
 
     public String getPath() {
-        return path;
+        return delegate.getPath();
     }
 
     public void setPath(final String path) {
         if(servletContext.isInitialized()) {
             throw UndertowServletMessages.MESSAGES.servletContextAlreadyInitialized();
         }
-        this.path = path;
+        delegate.setPath(path);
     }
 
     public String getComment() {
-        return comment;
+        return delegate.getComment();
     }
 
     public void setComment(final String comment) {
         if(servletContext.isInitialized()) {
             throw UndertowServletMessages.MESSAGES.servletContextAlreadyInitialized();
         }
-        this.comment = comment;
+        delegate.setComment(comment);
     }
 
     public boolean isHttpOnly() {
-        return httpOnly;
+        return delegate.isHttpOnly();
     }
 
     public void setHttpOnly(final boolean httpOnly) {
         if(servletContext.isInitialized()) {
             throw UndertowServletMessages.MESSAGES.servletContextAlreadyInitialized();
         }
-        this.httpOnly = httpOnly;
+        delegate.setHttpOnly(httpOnly);
     }
 
     public boolean isSecure() {
-        return secure;
+        return delegate.isSecure();
     }
 
     public void setSecure(final boolean secure) {
         if(servletContext.isInitialized()) {
             throw UndertowServletMessages.MESSAGES.servletContextAlreadyInitialized();
         }
-        this.secure = secure;
+        delegate.setSecure(secure);
     }
 
     public int getMaxAge() {
-        return maxAge;
+        return delegate.getMaxAge();
     }
 
     public void setMaxAge(final int maxAge) {
         if(servletContext.isInitialized()) {
             throw UndertowServletMessages.MESSAGES.servletContextAlreadyInitialized();
         }
-        this.maxAge = maxAge;
+        this.delegate.setMaxAge(maxAge);
     }
 
     public SessionConfig getFallback() {
