4a8d58773c135f61823c8c942e65a5295b5a9873
Stuart Douglas <stuart.w.douglas@gmail.com>
2014-01-31 16:29:30 +0200

    Don't allow session cookie fallback to be used with SSL

--- servlet/src/main/java/io/undertow/servlet/spec/ServletContextImpl.java
+++ servlet/src/main/java/io/undertow/servlet/spec/ServletContextImpl.java
@@ -128,7 +128,7 @@ public class ServletContextImpl implements ServletContext {
         SessionConfig sessionConfig = sessionCookieConfig;
         if (trackingMethods != null && !trackingMethods.isEmpty()) {
             if (sessionTrackingModes.contains(SessionTrackingMode.SSL)) {
-                sessionConfig = new SslSessionConfig(sessionCookieConfig);
+                sessionConfig = new SslSessionConfig();
             } else {
                 if (sessionTrackingModes.contains(SessionTrackingMode.COOKIE) && sessionTrackingModes.contains(SessionTrackingMode.URL)) {
                     sessionCookieConfig.setFallback(new PathParameterSessionConfig(sessionCookieConfig.getName().toLowerCase(Locale.ENGLISH)));
