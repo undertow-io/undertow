60b2cb01723a45e668385bea746794df508cd19c
Stuart Douglas <stuart.w.douglas@gmail.com>
2014-02-07 13:47:33 +0200

    Add servlet version of single single on mechanism

---
+++ servlet/src/main/java/io/undertow/servlet/handlers/security/ServletSingleSignOnAuthenticationMechainism.java
@@ -0,0 +1,38 @@
+package io.undertow.servlet.handlers.security;
+
+import io.undertow.security.impl.SingleSignOnAuthenticationMechanism;
+import io.undertow.security.impl.SingleSignOnManager;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.server.session.Session;
+import io.undertow.servlet.handlers.ServletRequestContext;
+import io.undertow.servlet.spec.HttpSessionImpl;
+
+import java.security.AccessController;
+import java.security.PrivilegedAction;
+
+/**
+ * Servlet version of the single sign on authentication mechanism.
+ *
+ * @author Stuart Douglas
+ */
+public class ServletSingleSignOnAuthenticationMechainism extends SingleSignOnAuthenticationMechanism {
+    public ServletSingleSignOnAuthenticationMechainism(SingleSignOnManager storage) {
+        super(storage);
+    }
+
+    @Override
+    protected Session getSession(HttpServerExchange exchange) {
+        ServletRequestContext servletRequestContext = exchange.getAttachment(ServletRequestContext.ATTACHMENT_KEY);
+        final HttpSessionImpl session = servletRequestContext.getCurrentServletContext().getSession(exchange, true);
+        if(System.getSecurityManager() == null) {
+            return session.getSession();
+        } else {
+            return AccessController.doPrivileged(new PrivilegedAction<Session>() {
+                @Override
+                public Session run() {
+                    return session.getSession();
+                }
+            });
+        }
+    }
+}
