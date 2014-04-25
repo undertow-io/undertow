a7918bbca27017e127835c504a97e2e01c0f42ef
Derrick Childers <derrick.childers@gmail.com>
2014-04-25 14:17:50 -0400

    Fix path specific cookies being overridden
    Path specific JSESSIONID cookie gets overridden by parent path cookie.
    If a more specific cookie already exists, then don’t override it.

--- core/src/main/java/io/undertow/util/Cookies.java
+++ core/src/main/java/io/undertow/util/Cookies.java
@@ -275,11 +275,17 @@ public class Cookies {
     private static int createCookie(final String name, final String value, int maxCookies, int cookieCount,
             final Map<String, String> cookies, final Map<String, String> additional) {
         if (name.charAt(0) == '$') {
+            if(additional.containsKey(name)) {
+                return cookieCount;
+            }
             additional.put(name, value);
             return cookieCount;
         } else {
             if (cookieCount == maxCookies) {
                 throw UndertowMessages.MESSAGES.tooManyCookies(maxCookies);
+            }
+            if(cookies.containsKey(name)) {
+                return cookieCount;
             }
             cookies.put(name, value);
             return ++cookieCount;
