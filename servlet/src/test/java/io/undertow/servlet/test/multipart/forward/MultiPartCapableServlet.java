b74030ad38e8e3edae0946b6c8f843fb2ae87fa3
Christian Kaltepoth <christian@kaltepoth.de>
2014-05-26 15:09:22 +0200

    UNDERTOW-251 Tests for reproducing this issue

---
+++ servlet/src/test/java/io/undertow/servlet/test/multipart/forward/MultiPartCapableServlet.java
@@ -0,0 +1,50 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2014 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package io.undertow.servlet.test.multipart.forward;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Map.Entry;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Christian Kaltepoth
+ */
+public class MultiPartCapableServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doPost(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
+
+        PrintWriter writer = resp.getWriter();
+
+        writer.println("Params:");
+
+        for (Entry<String, String[]> entry : req.getParameterMap().entrySet()) {
+            writer.println(entry.getKey() + ": " + entry.getValue()[0]);
+        }
+
+    }
+
+}
