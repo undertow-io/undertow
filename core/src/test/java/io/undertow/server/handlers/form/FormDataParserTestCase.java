9cf80d056b204451b0b573f00ac490385228d938
Stuart Douglas <stuart.w.douglas@gmail.com>
2014-06-22 10:52:59 -0400

    Fix test

--- core/src/test/java/io/undertow/server/handlers/form/FormDataParserTestCase.java
+++ core/src/test/java/io/undertow/server/handlers/form/FormDataParserTestCase.java
@@ -115,9 +115,9 @@ public class FormDataParserTestCase {
     @Test
     public void testFormDataParsing() throws Exception {
         runTest(new BasicNameValuePair("name", "A Value"));
-        runTest(new BasicNameValuePair("name", "A Value"), new BasicNameValuePair("Single value", null));
+        runTest(new BasicNameValuePair("name", "A Value"), new BasicNameValuePair("Single-value", null));
         runTest(new BasicNameValuePair("name", "A Value"), new BasicNameValuePair("A/name/with_special*chars", "A $ value&& with=SomeCharacters"));
-        runTest(new BasicNameValuePair("name", "A Value"), new BasicNameValuePair("Single value", null) , new BasicNameValuePair("A/name/with_special*chars", "A $ value&& with=SomeCharacters"));
+        runTest(new BasicNameValuePair("name", "A Value"), new BasicNameValuePair("Single-value", null) , new BasicNameValuePair("A/name/with_special*chars", "A $ value&& with=SomeCharacters"));
 
     }
 
