b0baa2148b7333e30dde68247d24433938af95a6
Stuart Douglas <stuart.w.douglas@gmail.com>
2014-07-18 13:57:32 +1000

    Remove temporary logging

--- core/src/main/java/io/undertow/server/handlers/proxy/mod_cluster/MCMPHandler.java
+++ core/src/main/java/io/undertow/server/handlers/proxy/mod_cluster/MCMPHandler.java
@@ -131,8 +131,6 @@ class MCMPHandler implements HttpHandler {
         final InetSocketAddress addr = exchange.getDestinationAddress();
         //we use getHostString to avoid a reverse lookup
         if (addr.getPort() != config.getManagementPort() || (!addr.getHostString().equals(config.getManagementHost()) &&  !addr.getHostString().equals(config.getManagementHostIp()))) {
-            //temporary log statement to see what is going on with CI
-            UndertowLogger.REQUEST_LOGGER.error("Failed to match request on port " + addr.getPort() + " with " + config.getManagementPort() + " and host " + addr.getHostString() + " with " + config.getManagementHost() + " or " + config.getManagementHostIp());
             next.handleRequest(exchange);
             return;
         }
