3719fb04ff7584f4a72d57a4b1a90ca252b82272
Stuart Douglas <stuart.w.douglas@gmail.com>
2014-07-18 17:58:23 +1000

    Increase buffer leak timeout

--- core/src/test/java/io/undertow/testutils/DefaultServer.java
+++ core/src/test/java/io/undertow/testutils/DefaultServer.java
@@ -229,7 +229,7 @@ public class DefaultServer extends BlockJUnit4ClassRunner {
 
                 if(!DebuggingSlicePool.BUFFERS.isEmpty()) {
                     try {
-                        Thread.sleep(200);
+                        Thread.sleep(2000);
                     } catch (InterruptedException e) {
                         throw new RuntimeException(e);
                     }
