aa59c7ca9ee6d2067d201d5e0830c7be59f7c003
Emanuel Muckenhuber <emuckenh@redhat.com>
2014-07-25 15:19:57 +0200

    align SpdyClientExchange#setResponseListener() with other exchanges

--- core/src/main/java/io/undertow/client/spdy/SpdyClientExchange.java
+++ core/src/main/java/io/undertow/client/spdy/SpdyClientExchange.java
@@ -46,6 +46,7 @@ public class SpdyClientExchange extends AbstractAttachable implements ClientExch
     private final ClientConnection clientConnection;
     private final SpdyStreamSinkChannel request;
     private final ClientRequest clientRequest;
+    private IOException failedReason;
 
     public SpdyClientExchange(ClientConnection clientConnection, SpdyStreamSinkChannel request, ClientRequest clientRequest) {
         this.clientConnection = clientConnection;
@@ -53,10 +54,16 @@ public class SpdyClientExchange extends AbstractAttachable implements ClientExch
         this.clientRequest = clientRequest;
     }
 
-
     @Override
     public void setResponseListener(ClientCallback<ClientExchange> responseListener) {
         this.responseListener = responseListener;
+        if (responseListener != null) {
+            if (failedReason != null) {
+                responseListener.failed(failedReason);
+            } else if (clientResponse != null) {
+                responseListener.completed(this);
+            }
+        }
     }
 
     @Override
@@ -98,6 +105,7 @@ public class SpdyClientExchange extends AbstractAttachable implements ClientExch
     }
 
     void failed(final IOException e) {
+        this.failedReason = e;
         if(responseListener != null) {
             responseListener.failed(e);
         }
