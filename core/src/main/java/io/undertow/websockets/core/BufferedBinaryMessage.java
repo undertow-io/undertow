2008a3dafa808d57c76d4beff40caaf254443e16
Stuart Douglas <stuart.w.douglas@gmail.com>
2014-10-24 08:52:43 +1100

    Make WebsocketChannel store the close code that was used to initiate the close

--- core/src/main/java/io/undertow/websockets/core/BufferedBinaryMessage.java
+++ core/src/main/java/io/undertow/websockets/core/BufferedBinaryMessage.java
@@ -163,7 +163,7 @@ public class BufferedBinaryMessage {
     private void checkMaxSize(StreamSourceFrameChannel channel, int res) throws IOException {
         currentSize += res;
         if (maxMessageSize > 0 && currentSize > maxMessageSize) {
-            WebSockets.sendClose(new CloseMessage(CloseMessage.MSG_TOO_BIG, WebSocketMessages.MESSAGES.messageToBig(maxMessageSize)).toByteBuffer(), channel.getWebSocketChannel(), null);
+            WebSockets.sendClose(new CloseMessage(CloseMessage.MSG_TOO_BIG, WebSocketMessages.MESSAGES.messageToBig(maxMessageSize)), channel.getWebSocketChannel(), null);
             throw new IOException(WebSocketMessages.MESSAGES.messageToBig(maxMessageSize));
         }
     }
