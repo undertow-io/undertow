1a8d40fc4be40752fcf9076eb723fe8759fa6779
Stuart Douglas <stuart.w.douglas@gmail.com>
2014-10-24 11:29:49 +1100

    More close handling fixes

--- websockets-jsr/src/main/java/io/undertow/websockets/jsr/FrameHandler.java
+++ websockets-jsr/src/main/java/io/undertow/websockets/jsr/FrameHandler.java
@@ -86,9 +86,9 @@ class FrameHandler extends AbstractReceiveListener {
                     if (singleBuffer.remaining() > 1) {
                         final CloseReason.CloseCode code = CloseReason.CloseCodes.getCloseCode(singleBuffer.getShort());
                         final String reasonPhrase = singleBuffer.remaining() > 1 ? new UTF8Output(singleBuffer).extract() : null;
-                        session.close(new CloseReason(code, reasonPhrase));
+                        session.closeInternal(new CloseReason(code, reasonPhrase));
                     } else {
-                        session.close();
+                        session.closeInternal(new CloseReason(CloseReason.CloseCodes.NO_STATUS_CODE, null));
                     }
                 } catch (IOException e) {
                     invokeOnError(e);
