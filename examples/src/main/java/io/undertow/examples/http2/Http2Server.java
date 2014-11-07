0572cd864b4d1c395540d4a176a05d20b715de6a
Stuart Douglas <stuart.w.douglas@gmail.com>
2014-11-07 13:43:44 +1100

    Allow the example directory to be changed

--- examples/src/main/java/io/undertow/examples/http2/Http2Server.java
+++ examples/src/main/java/io/undertow/examples/http2/Http2Server.java
@@ -56,7 +56,7 @@ public class Http2Server {
                 .setServerOption(UndertowOptions.ENABLE_HTTP2, true)
                 .setServerOption(UndertowOptions.ENABLE_SPDY, true)
                 .addHttpsListener(8443, "localhost", createSSLContext(loadKeyStore("server.keystore"), loadKeyStore("server.truststore")))
-                .setHandler(resource(new FileResourceManager(new File(System.getProperty("user.home")), 100))
+                .setHandler(resource(new FileResourceManager(new File(System.getProperty("example.directory", System.getProperty("user.home"))), 100))
                         .setDirectoryListingEnabled(true))
                 .build();
         server.start();
