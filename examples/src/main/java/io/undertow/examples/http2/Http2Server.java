898544542165fa492357415b533917751b1f9526
Jason T. Greene <jason.greene@redhat.com>
2014-11-10 22:39:05 -0600

    Add a getTransportProtocol(), and use it

--- examples/src/main/java/io/undertow/examples/http2/Http2Server.java
+++ examples/src/main/java/io/undertow/examples/http2/Http2Server.java
@@ -18,6 +18,21 @@
 
 package io.undertow.examples.http2;
 
+import static io.undertow.Handlers.predicate;
+import static io.undertow.Handlers.resource;
+import static io.undertow.predicate.Predicates.secure;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.security.KeyStore;
+
+import javax.net.ssl.KeyManager;
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
+
 import io.undertow.Undertow;
 import io.undertow.UndertowOptions;
 import io.undertow.examples.UndertowExample;
@@ -25,22 +40,9 @@ import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.server.handlers.resource.FileResourceManager;
 import io.undertow.util.Headers;
+import io.undertow.util.HttpString;
 import io.undertow.util.StatusCodes;
 import org.xnio.IoUtils;
-
-import javax.net.ssl.KeyManager;
-import javax.net.ssl.KeyManagerFactory;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.TrustManagerFactory;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.security.KeyStore;
-
-import static io.undertow.Handlers.predicate;
-import static io.undertow.Handlers.resource;
-import static io.undertow.predicate.Predicates.secure;
 
 /**
  * @author Stuart Douglas
@@ -65,16 +67,38 @@ public class Http2Server {
                 .setServerOption(UndertowOptions.ENABLE_SPDY, true)
                 .addHttpListener(8080, bindAddress)
                 .addHttpsListener(8443, bindAddress, createSSLContext(loadKeyStore("server.keystore"), loadKeyStore("server.truststore")))
-                .setHandler(predicate(secure(), resource(new FileResourceManager(new File(System.getProperty("example.directory", System.getProperty("user.home"))), 100))
+                .setHandler(transport(predicate(secure(), resource(new FileResourceManager(new File(System.getProperty("example.directory", System.getProperty("user.home"))), 100))
                         .setDirectoryListingEnabled(true), new HttpHandler() {
                     @Override
                     public void handleRequest(HttpServerExchange exchange) throws Exception {
                         exchange.getResponseHeaders().add(Headers.LOCATION, "https://" + exchange.getHostName() + ":" + (exchange.getHostPort() + 363) + exchange.getRelativePath());
                         exchange.setResponseCode(StatusCodes.TEMPORARY_REDIRECT);
                     }
-                })).build();
+                }))).build();
         server.start();
     }
+
+    private static HttpHandler transport(HttpHandler next) {
+        return new AddTransportProtocolHandler(next);
+    }
+
+    private static class AddTransportProtocolHandler implements HttpHandler {
+        private static final HttpString TRANSPORT = new HttpString("X-Undertow-Transport");
+
+        private final HttpHandler next;
+
+        private AddTransportProtocolHandler(HttpHandler next) {
+            this.next = next;
+        }
+
+        @Override
+        public void handleRequest(HttpServerExchange exchange) throws Exception {
+            exchange.getResponseHeaders().put(TRANSPORT, exchange.getConnection().getTransportProtocol());
+            next.handleRequest(exchange);
+
+        }
+    }
+
 
     private static KeyStore loadKeyStore(String name) throws Exception {
         String storeLoc = System.getProperty(name);
