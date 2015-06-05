99cc252f3961d95c0f262c63200b940c89372ebe
Adam Forgacs <adamforgacs256@gmail.com>
2015-06-05 17:52:25 +0200

    Keep the servlet context attribute as a File.

--- servlet/src/main/java/io/undertow/servlet/core/DeploymentManagerImpl.java
+++ servlet/src/main/java/io/undertow/servlet/core/DeploymentManagerImpl.java
@@ -425,8 +425,8 @@ public class DeploymentManagerImpl implements DeploymentManager {
     }
 
     private void initializeTempDir(final ServletContextImpl servletContext, final DeploymentInfo deploymentInfo) {
-        if (deploymentInfo.getTempDirPath() != null) {
-            servletContext.setAttribute(ServletContext.TEMPDIR, deploymentInfo.getTempDirPath());
+        if (deploymentInfo.getTempDir() != null) {
+            servletContext.setAttribute(ServletContext.TEMPDIR, deploymentInfo.getTempDir());
         } else {
             servletContext.setAttribute(ServletContext.TEMPDIR, new File(SecurityActions.getSystemProperty("java.io.tmpdir")));
         }
