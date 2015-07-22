160922dd99e054e35d6d3641685430cb263f0cb5
Stuart Douglas <stuart.w.douglas@gmail.com>
2015-07-23 07:39:43 +1000

    UNDERTOW-500 Fix issue with range requests

--- core/src/test/java/io/undertow/server/handlers/RangeRequestTestCase.java
+++ core/src/test/java/io/undertow/server/handlers/RangeRequestTestCase.java
@@ -18,10 +18,14 @@
 
 package io.undertow.server.handlers;
 
+import io.undertow.Handlers;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
+import io.undertow.server.handlers.resource.PathResourceManager;
+import io.undertow.server.handlers.resource.ResourceHandler;
 import io.undertow.testutils.DefaultServer;
 import io.undertow.testutils.TestHttpClient;
+import io.undertow.util.Headers;
 import io.undertow.util.StatusCodes;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -32,6 +36,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * @author Stuart Douglas
@@ -41,53 +48,79 @@ public class RangeRequestTestCase {
 
 
     @BeforeClass
-    public static void setup() {
-        DefaultServer.setRootHandler(new ByteRangeHandler(new HttpHandler() {
+    public static void setup() throws URISyntaxException {
+        Path rootPath = Paths.get(RangeRequestTestCase.class.getResource("range.txt").toURI()).getParent();
+        PathHandler path = Handlers.path();
+        path.addPrefixPath("/path", new ByteRangeHandler(new HttpHandler() {
             @Override
             public void handleRequest(HttpServerExchange exchange) throws Exception {
                 exchange.getResponseSender().send("0123456789");
             }
         }, true));
+        path.addPrefixPath("/resource",  new ResourceHandler(new PathResourceManager(rootPath, 10485760))
+                        .setDirectoryListingEnabled(true));
+        DefaultServer.setRootHandler(path);
     }
 
     @Test
-    public void testRangeRequests() throws IOException, InterruptedException {
+    public void testGenericRangeHandler() throws IOException, InterruptedException {
+        runTest("/path");
+    }
+    @Test
+    public void testResourceHandler() throws IOException, InterruptedException {
+        runTest("/resource/range.txt");
+    }
+
+    public void runTest(String path) throws IOException, InterruptedException {
         TestHttpClient client = new TestHttpClient();
         try {
-            HttpGet get = new HttpGet(DefaultServer.getDefaultServerURL() + "/path");
+            HttpGet get = new HttpGet(DefaultServer.getDefaultServerURL() + path);
             get.addHeader("range", "bytes=2-3");
             HttpResponse result = client.execute(get);
             Assert.assertEquals(StatusCodes.PARTIAL_CONTENT, result.getStatusLine().getStatusCode());
             String response = EntityUtils.toString(result.getEntity());
             Assert.assertEquals("23", response);
+            Assert.assertEquals( "2-3/10", result.getFirstHeader(Headers.CONTENT_RANGE_STRING).getValue());
 
-            get = new HttpGet(DefaultServer.getDefaultServerURL() + "/path");
+            get = new HttpGet(DefaultServer.getDefaultServerURL() + path);
             get.addHeader("range", "bytes=0-0");
             result = client.execute(get);
             Assert.assertEquals(StatusCodes.PARTIAL_CONTENT, result.getStatusLine().getStatusCode());
             response = EntityUtils.toString(result.getEntity());
             Assert.assertEquals("0", response);
+            Assert.assertEquals( "0-0/10", result.getFirstHeader(Headers.CONTENT_RANGE_STRING).getValue());
 
-            get = new HttpGet(DefaultServer.getDefaultServerURL() + "/path");
+            get = new HttpGet(DefaultServer.getDefaultServerURL() + path);
             get.addHeader("range", "bytes=1-");
             result = client.execute(get);
             Assert.assertEquals(StatusCodes.PARTIAL_CONTENT, result.getStatusLine().getStatusCode());
             response = EntityUtils.toString(result.getEntity());
             Assert.assertEquals("123456789", response);
+            Assert.assertEquals( "1-9/10", result.getFirstHeader(Headers.CONTENT_RANGE_STRING).getValue());
 
-            get = new HttpGet(DefaultServer.getDefaultServerURL() + "/path");
+            get = new HttpGet(DefaultServer.getDefaultServerURL() + path);
+            get.addHeader("range", "bytes=0-");
+            result = client.execute(get);
+            Assert.assertEquals(StatusCodes.PARTIAL_CONTENT, result.getStatusLine().getStatusCode());
+            response = EntityUtils.toString(result.getEntity());
+            Assert.assertEquals("0123456789", response);
+            Assert.assertEquals("0-9/10", result.getFirstHeader(Headers.CONTENT_RANGE_STRING).getValue());
+
+            get = new HttpGet(DefaultServer.getDefaultServerURL() + path);
             get.addHeader("range", "bytes=9-");
             result = client.execute(get);
             Assert.assertEquals(StatusCodes.PARTIAL_CONTENT, result.getStatusLine().getStatusCode());
             response = EntityUtils.toString(result.getEntity());
             Assert.assertEquals("9", response);
+            Assert.assertEquals("9-9/10", result.getFirstHeader(Headers.CONTENT_RANGE_STRING).getValue());
 
-            get = new HttpGet(DefaultServer.getDefaultServerURL() + "/path");
+            get = new HttpGet(DefaultServer.getDefaultServerURL() + path);
             get.addHeader("range", "bytes=-1");
             result = client.execute(get);
             Assert.assertEquals(StatusCodes.PARTIAL_CONTENT, result.getStatusLine().getStatusCode());
             response = EntityUtils.toString(result.getEntity());
             Assert.assertEquals("9", response);
+            Assert.assertEquals("9-9/10", result.getFirstHeader(Headers.CONTENT_RANGE_STRING).getValue());
 
         } finally {
             client.getConnectionManager().shutdown();
