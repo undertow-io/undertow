0c8f6e4f9b4bdbd5cce8ce0c914c126d2607880b
Stuart Douglas <stuart.w.douglas@gmail.com>
2016-02-29 11:04:21 +1100

    UNDERTOW-647 Bug in HTTP/1 transferFrom implementation

--- core/src/main/java/io/undertow/server/protocol/http/HttpResponseConduit.java
+++ core/src/main/java/io/undertow/server/protocol/http/HttpResponseConduit.java
@@ -59,6 +59,7 @@ final class HttpResponseConduit extends AbstractStreamSinkConduit<StreamSinkCond
     private int valueIdx;
     private int charIndex;
     private PooledByteBuffer pooledBuffer;
+    private PooledByteBuffer pooledFileTransferBuffer;
     private HttpServerExchange exchange;
 
     private ByteBuffer[] writevBuffer;
@@ -649,17 +650,41 @@ final class HttpResponseConduit extends AbstractStreamSinkConduit<StreamSinkCond
     public long transferFrom(final FileChannel src, final long position, final long count) throws IOException {
         try {
             if (state != 0) {
-                final PooledByteBuffer pooled = exchange.getConnection().getByteBufferPool().allocate();
-                ByteBuffer buffer = pooled.getBuffer();
-                try {
-                    int res = src.read(buffer);
-                    if (res <= 0) {
-                        return res;
+                if(pooledFileTransferBuffer != null) {
+                    try {
+                        return write(pooledFileTransferBuffer.getBuffer());
+                    } catch (IOException|RuntimeException e) {
+                        if(pooledFileTransferBuffer != null) {
+                            pooledFileTransferBuffer.close();
+                            pooledFileTransferBuffer = null;
+                        }
+                        throw e;
+                    } finally {
+                        if(pooledFileTransferBuffer != null) {
+                            if (!pooledFileTransferBuffer.getBuffer().hasRemaining()) {
+                                pooledFileTransferBuffer.close();
+                                pooledFileTransferBuffer = null;
+                            }
+                        }
                     }
-                    buffer.flip();
-                    return write(buffer);
-                } finally {
-                    pooled.close();
+                } else {
+                    final PooledByteBuffer pooled = exchange.getConnection().getByteBufferPool().allocate();
+
+                    ByteBuffer buffer = pooled.getBuffer();
+                    try {
+                        int res = src.read(buffer);
+                        buffer.flip();
+                        if (res <= 0) {
+                            return res;
+                        }
+                        return write(buffer);
+                    } finally {
+                        if(buffer.hasRemaining()) {
+                            pooledFileTransferBuffer = pooled;
+                        } else {
+                            pooled.close();
+                        }
+                    }
                 }
             } else {
                 return next.transferFrom(src, position, count);
@@ -758,6 +783,10 @@ final class HttpResponseConduit extends AbstractStreamSinkConduit<StreamSinkCond
             if (pooledBuffer != null) {
                 bufferDone();
             }
+            if(pooledFileTransferBuffer != null) {
+                pooledFileTransferBuffer.close();
+                pooledFileTransferBuffer = null;
+            }
         }
     }
 
@@ -769,6 +798,10 @@ final class HttpResponseConduit extends AbstractStreamSinkConduit<StreamSinkCond
         done = true;
         if(pooledBuffer != null) {
             bufferDone();
+        }
+        if(pooledFileTransferBuffer != null) {
+            pooledFileTransferBuffer.close();
+            pooledFileTransferBuffer = null;
         }
     }
 }
