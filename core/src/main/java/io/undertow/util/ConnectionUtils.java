f6f425baec32abd625a4f4da0044d10ed7b37741
Stuart Douglas <stuart.w.douglas@gmail.com>
2016-03-09 14:12:25 +1100

    More changes to SSL clean close

--- core/src/main/java/io/undertow/util/ConnectionUtils.java
+++ core/src/main/java/io/undertow/util/ConnectionUtils.java
@@ -19,23 +19,32 @@
 package io.undertow.util;
 
 import io.undertow.UndertowLogger;
+import org.xnio.ChannelExceptionHandler;
+import org.xnio.ChannelListener;
+import org.xnio.ChannelListeners;
 import org.xnio.IoUtils;
 import org.xnio.StreamConnection;
+import org.xnio.conduits.ConduitStreamSinkChannel;
+import org.xnio.conduits.ConduitStreamSourceChannel;
 
 import java.io.Closeable;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author Stuart Douglas
  */
 public class ConnectionUtils {
 
+    private static final long MAX_DRAIN_TIME = Long.getLong("io.undertow.max-drain-time", 10000);
+
     private ConnectionUtils() {
 
     }
 
     /**
-     * Cleanly close a connection, by shutting down and flushing writes.
+     * Cleanly close a connection, by shutting down and flushing writes and then draining reads.
      * <p/>
      * If this fails the connection is forcibly closed.
      *
@@ -45,9 +54,25 @@ public class ConnectionUtils {
     public static void cleanClose(final StreamConnection connection, final Closeable... additional) {
         try {
             connection.getSinkChannel().shutdownWrites();
-            connection.getSinkChannel().flush();
-            IoUtils.safeClose(connection);
-            IoUtils.safeClose(additional);
+            if (!connection.getSinkChannel().flush()) {
+                connection.getSinkChannel().setWriteListener(ChannelListeners.flushingChannelListener(new ChannelListener<ConduitStreamSinkChannel>() {
+                    @Override
+                    public void handleEvent(ConduitStreamSinkChannel channel) {
+                        doDrain(connection, additional);
+                    }
+                }, new ChannelExceptionHandler<ConduitStreamSinkChannel>() {
+                    @Override
+                    public void handleException(ConduitStreamSinkChannel channel, IOException exception) {
+                        UndertowLogger.REQUEST_IO_LOGGER.ioException(exception);
+                        IoUtils.safeClose(connection);
+                        IoUtils.safeClose(additional);
+                    }
+                }));
+                connection.getSinkChannel().resumeWrites();
+            } else {
+                doDrain(connection, additional);
+            }
+
         } catch (Exception e) {
             if (e instanceof IOException) {
                 UndertowLogger.REQUEST_IO_LOGGER.ioException((IOException) e);
@@ -58,4 +83,61 @@ public class ConnectionUtils {
             IoUtils.safeClose(additional);
         }
     }
+
+    private static void doDrain(final StreamConnection connection, final Closeable... additional) {
+        if (!connection.getSourceChannel().isOpen()) {
+            IoUtils.safeClose(connection);
+            IoUtils.safeClose(additional);
+            return;
+        }
+        final ByteBuffer b = ByteBuffer.allocate(1);
+        try {
+            int res = connection.getSourceChannel().read(b);
+            if (res == 0) {
+                connection.getSourceChannel().setReadListener(new ChannelListener<ConduitStreamSourceChannel>() {
+                    @Override
+                    public void handleEvent(ConduitStreamSourceChannel channel) {
+                        try {
+                            int res = channel.read(b);
+                            if (res == 0) {
+                                return;
+                            } else {
+                                IoUtils.safeClose(connection);
+                                IoUtils.safeClose(additional);
+                            }
+                        } catch (Exception e) {
+                            if (e instanceof IOException) {
+                                UndertowLogger.REQUEST_IO_LOGGER.ioException((IOException) e);
+                            } else {
+                                UndertowLogger.REQUEST_IO_LOGGER.ioException(new IOException(e));
+                            }
+                            IoUtils.safeClose(connection);
+                            IoUtils.safeClose(additional);
+                        }
+                    }
+                });
+                connection.getSourceChannel().resumeReads();
+                connection.getIoThread().executeAfter(new Runnable() {
+                    @Override
+                    public void run() {
+                        IoUtils.safeClose(connection);
+                        IoUtils.safeClose(additional);
+                    }
+                }, MAX_DRAIN_TIME, TimeUnit.MILLISECONDS);
+            } else {
+                IoUtils.safeClose(connection);
+                IoUtils.safeClose(additional);
+            }
+        } catch (Exception e) {
+            if (e instanceof IOException) {
+                UndertowLogger.REQUEST_IO_LOGGER.ioException((IOException) e);
+            } else {
+                UndertowLogger.REQUEST_IO_LOGGER.ioException(new IOException(e));
+            }
+            IoUtils.safeClose(connection);
+            IoUtils.safeClose(additional);
+        }
+    }
+
+
 }
