204aabf9a39e6b18513fba21e47997da4326d077
Ingo Weiss <ingo@redhat.com>
2016-05-23 15:03:39 +0100

    [UNDERTOW-711] add trace logging to the SSO code in wildfly/undertow

--- core/src/main/java/io/undertow/security/impl/InMemorySingleSignOnManager.java
+++ core/src/main/java/io/undertow/security/impl/InMemorySingleSignOnManager.java
@@ -28,12 +28,15 @@ import io.undertow.server.session.SecureRandomSessionIdGenerator;
 import io.undertow.server.session.Session;
 import io.undertow.server.session.SessionManager;
 import io.undertow.util.CopyOnWriteMap;
+import org.jboss.logging.Logger;
 
 /**
  * @author Stuart Douglas
  * @author Paul Ferraro
  */
 public class InMemorySingleSignOnManager implements SingleSignOnManager {
+
+    private static final Logger log = Logger.getLogger(InMemorySingleSignOnManager.class);
 
     private static final SecureRandomSessionIdGenerator SECURE_RANDOM_SESSION_ID_GENERATOR = new SecureRandomSessionIdGenerator();
 
@@ -49,11 +52,17 @@ public class InMemorySingleSignOnManager implements SingleSignOnManager {
         String id = SECURE_RANDOM_SESSION_ID_GENERATOR.createSessionId();
         SingleSignOn entry = new SimpleSingleSignOnEntry(id, account, mechanism);
         this.ssoEntries.put(id, entry);
+        if(log.isTraceEnabled()) {
+            log.tracef("Creating SSO ID %s for Principal %s and Roles %s.", id, account.getPrincipal().getName(), account.getRoles().toString());
+        }
         return entry;
     }
 
     @Override
     public void removeSingleSignOn(SingleSignOn sso) {
+        if(log.isTraceEnabled()) {
+            log.tracef("Removing SSO ID %s.", sso.getId());
+        }
         this.ssoEntries.remove(sso.getId());
     }
 
