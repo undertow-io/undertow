542024ade4cf123689a4401fb13ea02dfefded73
Stuart Douglas <stuart.w.douglas@gmail.com>
2016-07-06 09:08:39 +1000

    Limit the number of websocket redirects

--- core/src/main/java/io/undertow/UndertowMessages.java
+++ core/src/main/java/io/undertow/UndertowMessages.java
@@ -32,7 +32,6 @@ import org.jboss.logging.annotations.MessageBundle;
 
 import javax.net.ssl.SSLHandshakeException;
 import javax.net.ssl.SSLPeerUnverifiedException;
-import javax.net.ssl.SSLProtocolException;
 
 /**
  * @author Stuart Douglas
@@ -455,27 +454,15 @@ public interface UndertowMessages {
     @Message(id = 141, value = "Initial SSL/TLS handshake record is invalid")
     SSLHandshakeException invalidHandshakeRecord();
 
-    @Message(id = 4010, value = "Initial SSL/TLS handshake spans multiple records")
+    @Message(id = 142, value = "Initial SSL/TLS handshake spans multiple records")
     SSLHandshakeException multiRecordSSLHandshake();
 
-    @Message(id = 142, value = "Expected \"client hello\" record")
+    @Message(id = 143, value = "Expected \"client hello\" record")
     SSLHandshakeException expectedClientHello();
 
-    @Message(id = 143, value = "Unsupported SSL/TLS record")
-    SSLHandshakeException unsupportedSslRecord();
-
-    @Message(id = 144, value = "Invalid SNI extension")
-    SSLProtocolException invalidSniExt();
-
-    @Message(id = 145, value = "Not enough data in record to fill declared item size")
-    SSLProtocolException notEnoughData();
-
-    @Message(id = 146, value = "Empty host name in SNI record data")
-    SSLProtocolException emptyHostNameSni();
-
-    @Message(id = 147, value = "Duplicated SNI server name of type %d")
-    SSLProtocolException duplicatedSniServerName(int type);
-
-    @Message(id = 148, value = "Expected server hello")
+    @Message(id = 144, value = "Expected server hello")
     SSLHandshakeException expectedServerHello();
+
+    @Message(id = 145, value = "Too many redirects")
+    IOException tooManyRedirects(@Cause IOException exception);
 }
