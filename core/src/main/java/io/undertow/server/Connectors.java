c9dff63fe5798a16a9aed7c4add7d80dc72f0e82
Stuart Douglas <stuart.w.douglas@gmail.com>
2016-08-30 13:56:08 +1000

    Copy paste error in UNDERTOW-802 fix

--- core/src/main/java/io/undertow/server/Connectors.java
+++ core/src/main/java/io/undertow/server/Connectors.java
@@ -194,7 +194,7 @@ public class Connectors {
             header.append("; Expires=");
             header.append(DateUtils.toDateString(cookie.getExpires()));
         }
-        if (cookie.getMaxAge() != null) {
+        if (cookie.getComment() != null) {
             if (cookie.getComment() != null && !cookie.getComment().isEmpty()) {
                 header.append("; Comment=");
                 header.append(cookie.getComment());
