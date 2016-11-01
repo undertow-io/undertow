df6586baf4b40af42761cc0c9da311a7eb84c6cf
Stuart Douglas <stuart.w.douglas@gmail.com>
2016-11-02 10:33:38 +1100

    UNDERTOW-885 PathResourceManager cannot resolve non-normal paths that occur after a symlink segment

--- core/src/test/java/io/undertow/server/handlers/file/PathResourceManagerTestCase.java
+++ core/src/test/java/io/undertow/server/handlers/file/PathResourceManagerTestCase.java
@@ -1,18 +1,19 @@
 package io.undertow.server.handlers.file;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import io.undertow.server.handlers.resource.PathResourceManager;
 import org.junit.Assert;
+import org.junit.Assume;
 import org.junit.Test;
+import io.undertow.server.handlers.resource.PathResourceManager;
 
 /**
  * @author Tomaz Cerar (c) 2016 Red Hat Inc.
  */
 
 public class PathResourceManagerTestCase {
-
 
 
     @Test
@@ -33,5 +34,36 @@ public class PathResourceManagerTestCase {
         final PathResourceManager resourceManager = new PathResourceManager(rootPath, 1024 * 1024);
         Assert.assertNotNull(resourceManager.getResource("a.txt"));
         Assert.assertNull(resourceManager.getResource("../page.html"));
+    }
+
+
+    @Test
+    public void testBaseDirInSymlink() throws Exception {
+        Assume.assumeFalse(System.getProperty("os.name").toLowerCase().contains("windows"));
+
+        Path filePath = Paths.get(getClass().getResource("page.html").toURI());
+        Path rootPath = filePath.getParent();
+
+        Path newDir = rootPath.resolve("newDir");
+        Path innerPage = newDir.resolve("page.html");
+        Path newSymlink = rootPath.resolve("newSymlink");
+        try {
+            Files.createDirectories(newDir);
+            Files.copy(filePath, innerPage);
+            Files.createSymbolicLink(newSymlink, newDir);
+
+            Assert.assertTrue("Ensure that newSymlink is still a symlink as expected", Files.isSymbolicLink(newSymlink));
+            final PathResourceManager resourceManager = new PathResourceManager(newSymlink, 1024 * 1024);
+            Assert.assertNotNull(resourceManager.getResource("page.html"));
+            Assert.assertNull(resourceManager.getResource("Page.html"));
+            Assert.assertNotNull(resourceManager.getResource("./page.html"));
+
+        } finally {
+            Files.deleteIfExists(newSymlink);
+            Files.deleteIfExists(innerPage);
+            Files.deleteIfExists(newDir);
+            Files.deleteIfExists(newDir);
+        }
+
     }
 }
