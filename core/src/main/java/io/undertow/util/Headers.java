undertow-io/undertow

commit 8ef565d9d41505d4ce0b7896326bec7b43701fb4

UNDERTOW-993 Name of Multipart Parts is parsed incorrectly when filename property comes first

core/src/main/java/io/undertow/util/Headers.java
@@ -283,10 +283,16 @@ public final class Headers {
      * @param key    The key that identifies the token to extract
      * @return The token, or null if it was not found
      */
+    @Deprecated
     public static String extractTokenFromHeader(final String header, final String key) {
-        int pos = header.indexOf(key + '=');
+        int pos = header.indexOf(' ' + key + '=');
         if (pos == -1) {
-            return null;
+            if(!header.startsWith(key + '=')) {
+                return null;
+            }
+            pos = 0;
+        } else {
+            pos++;
         }
         int end;
         int start = pos + key.length() + 1;
@@ -305,6 +311,7 @@ public final class Headers {
      * content-disposition=form-data; name="my field"
      * and the key is name then "my field" will be returned without the quotes.
      *
+     *
      * @param header The header
      * @param key    The key that identifies the token to extract
      * @return The token, or null if it was not found
@@ -313,6 +320,7 @@ public final class Headers {
 
         int keypos = 0;
         int pos = -1;
+        boolean whiteSpace = true;
         boolean inQuotes = false;
         for (int i = 0; i < header.length() - 1; ++i) { //-1 because we need room for the = at the end
             //TODO: a more efficient matching algorithm
@@ -322,13 +330,20 @@ public final class Headers {
                     inQuotes = false;
                 }
             } else {
-                if (key.charAt(keypos) == c) {
+                if (key.charAt(keypos) == c && (whiteSpace || keypos > 0)) {
                     keypos++;
+                    whiteSpace = false;
                 } else if (c == '"') {
                     keypos = 0;
                     inQuotes = true;
+                    whiteSpace = false;
                 } else {
                     keypos = 0;
+                    if(c == ' ') {
+                        whiteSpace = true;
+                    } else {
+                        whiteSpace = false;
+                    }
                 }
                 if (keypos == key.length()) {
                     if (header.charAt(i + 1) == '=') {

core/src/test/java/io/undertow/util/ContentTypeParsingTestCase.java
@@ -34,6 +34,7 @@ public class ContentTypeParsingTestCase {
         Assert.assertEquals("UTF-8", Headers.extractQuotedValueFromHeader("text/html; charset=UTF-8", "charset"));
         Assert.assertEquals("UTF-8", Headers.extractQuotedValueFromHeader("text/html; charset=\"UTF-8\"; foo=bar", "charset"));
         Assert.assertEquals("UTF-8", Headers.extractQuotedValueFromHeader("text/html; charset=UTF-8 foo=bar", "charset"));
+        Assert.assertEquals("UTF-8", Headers.extractQuotedValueFromHeader("text/html; badcharset=bad charset=UTF-8 foo=bar", "charset"));
     }
 
 }

commit 2b71e928bb467e4cf073af5b73f731648ae5244d

UNDERTOW-1008 Fix charset parsing regression
Fix content type parsing regression introduced by
8ef565d9d41505d4ce0b7896326bec7b43701fb4

content type parsing fails when no whitespace is present following
the delimiter semicolon.

core/src/main/java/io/undertow/util/Headers.java
@@ -339,11 +339,7 @@ public final class Headers {
                     whiteSpace = false;
                 } else {
                     keypos = 0;
-                    if(c == ' ') {
-                        whiteSpace = true;
-                    } else {
-                        whiteSpace = false;
-                    }
+                    whiteSpace = c == ' ' || c == ';' || c == '\t';
                 }
                 if (keypos == key.length()) {
                     if (header.charAt(i + 1) == '=') {

core/src/test/java/io/undertow/util/ContentTypeParsingTestCase.java
@@ -38,6 +38,8 @@ public class ContentTypeParsingTestCase {
         Assert.assertEquals("UTF-8", Headers.extractQuotedValueFromHeader("text/html; charset=\"UTF-8\"; foo=bar", "charset"));
         Assert.assertEquals("UTF-8", Headers.extractQuotedValueFromHeader("text/html; charset=UTF-8 foo=bar", "charset"));
         Assert.assertEquals("UTF-8", Headers.extractQuotedValueFromHeader("text/html; badcharset=bad charset=UTF-8 foo=bar", "charset"));
+        Assert.assertEquals("UTF-8", Headers.extractQuotedValueFromHeader("text/html;charset=UTF-8", "charset"));
+        Assert.assertEquals("UTF-8", Headers.extractQuotedValueFromHeader("text/html;\tcharset=UTF-8", "charset"));
     }
 
 }