02affa436e8b2741706cd2624304bb44f4c6e11d
Stuart Douglas <stuart.w.douglas@gmail.com>
2017-03-07 11:29:34 +1100

    UNDERTOW-1009 SSLHeaderHandler should not require base64 SSL_SESSION_ID

--- core/src/main/java/io/undertow/server/BasicSSLSessionInfo.java
+++ core/src/main/java/io/undertow/server/BasicSSLSessionInfo.java
@@ -133,7 +133,8 @@ public class BasicSSLSessionInfo implements SSLSessionInfo {
             }
             return sessionIdData;
         } catch (IOException e) {
-            throw new RuntimeException(e); //won't happen
+            //can happen if the session id is invalid
+            return null;
         }
     }
 }
