eb93698307fc6b19d2d426053c534da83a6d5fec
Steve Hu <stevehu@gmail.com>
2017-03-21 21:13:46 -0400

    add Http2ClientTestCase.java

---
+++ core/src/test/java/io/undertow/client/http/Http2ClientTestCase.java
@@ -0,0 +1,346 @@
+package io.undertow.client.http;
+
+import io.undertow.Undertow;
+import io.undertow.UndertowOptions;
+import io.undertow.client.ClientCallback;
+import io.undertow.client.ClientConnection;
+import io.undertow.client.ClientExchange;
+import io.undertow.client.ClientRequest;
+import io.undertow.client.ClientResponse;
+import io.undertow.client.UndertowHttp2Client;
+import io.undertow.io.Sender;
+import io.undertow.protocols.ssl.UndertowXnioSsl;
+import io.undertow.server.DefaultByteBufferPool;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.testutils.DebuggingSlicePool;
+import io.undertow.testutils.DefaultServer;
+import io.undertow.util.AttachmentKey;
+import io.undertow.util.Headers;
+import io.undertow.util.Methods;
+import io.undertow.util.StatusCodes;
+import io.undertow.util.StringReadChannelListener;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.xnio.ChannelListeners;
+import org.xnio.IoUtils;
+import org.xnio.OptionMap;
+import org.xnio.Options;
+import org.xnio.Xnio;
+import org.xnio.XnioWorker;
+import org.xnio.channels.StreamSinkChannel;
+import org.xnio.ssl.XnioSsl;
+
+import javax.net.ssl.KeyManager;
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.KeyStore;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @author Steve Hu
+ */
+public class Http2ClientTestCase {
+    static Undertow server;
+
+    private static final String message = "Hello World!";
+    private static XnioWorker worker;
+
+    private static final OptionMap DEFAULT_OPTIONS;
+    private static final HttpHandler SIMPLE_MESSAGE_HANDLER;
+    private static final char[] STORE_PASSWORD = "password".toCharArray();
+    private static final DebuggingSlicePool pool = new DebuggingSlicePool(new DefaultByteBufferPool(true, 8192 * 3, 1000, 10, 100));
+
+
+    private static final AttachmentKey<String> RESPONSE_BODY = AttachmentKey.create(String.class);
+
+    static {
+
+        final OptionMap.Builder builder = OptionMap.builder()
+                .set(Options.WORKER_IO_THREADS, 8)
+                .set(Options.TCP_NODELAY, true)
+                .set(Options.KEEP_ALIVE, true)
+                .set(Options.WORKER_NAME, "Client");
+
+        DEFAULT_OPTIONS = builder.getMap();
+
+        SIMPLE_MESSAGE_HANDLER = new HttpHandler() {
+            @Override
+            public void handleRequest(HttpServerExchange exchange) throws Exception {
+                sendMessage(exchange);
+            }
+        };
+    }
+
+    static void sendMessage(final HttpServerExchange exchange) {
+        exchange.setStatusCode(StatusCodes.OK);
+        exchange.getResponseHeaders().put(Headers.CONTENT_LENGTH, message.length() + "");
+        final Sender sender = exchange.getResponseSender();
+        sender.send(message);
+    }
+
+    @BeforeClass
+    public static void setup() throws Exception {
+        SSLContext sslContext = createSSLContext(loadKeyStore("server.keystore"), loadKeyStore("server.truststore"));
+        server = Undertow.builder()
+                .addHttpListener(7777, "localhost")
+                .addHttpsListener(7778, "localhost", sslContext)
+                .setServerOption(UndertowOptions.ENABLE_HTTP2, true)
+                .setHandler(SIMPLE_MESSAGE_HANDLER)
+                .build();
+        server.start();
+
+        // Create xnio worker
+        final Xnio xnio = Xnio.getInstance();
+        final XnioWorker xnioWorker = xnio.createWorker(null, DEFAULT_OPTIONS);
+        worker = xnioWorker;
+
+    }
+
+    @AfterClass
+    public static void stop() {
+        server.stop();
+        worker.shutdown();
+    }
+
+    static UndertowHttp2Client createClient() {
+        return createClient(OptionMap.EMPTY);
+    }
+
+    static UndertowHttp2Client createClient(final OptionMap options) {
+        return UndertowHttp2Client.getInstance();
+    }
+
+    /*
+    @Test
+    public void testSimpleHttp() throws Exception {
+        final UndertowHttp2Client client = createClient();
+
+        final List<ClientResponse> responses = new CopyOnWriteArrayList<>();
+        final CountDownLatch latch = new CountDownLatch(10);
+        final ClientConnection connection = client.connect(new URI("http://localhost:7777"), worker, DefaultServer.getBufferPool(), OptionMap.EMPTY).get();
+        try {
+            connection.getIoThread().execute(new Runnable() {
+                @Override
+                public void run() {
+                    for (int i = 0; i < 10; i++) {
+                        final ClientRequest request = new ClientRequest().setMethod(Methods.GET).setPath("/");
+                        request.getRequestHeaders().put(Headers.HOST, DefaultServer.getHostAddress());
+                        connection.sendRequest(request, createClientCallback(responses, latch));
+                    }
+                }
+
+            });
+
+            latch.await(10, TimeUnit.SECONDS);
+
+            Assert.assertEquals(10, responses.size());
+            for (final ClientResponse response : responses) {
+                System.out.println(response.getProtocol());
+                Assert.assertEquals(message, response.getAttachment(RESPONSE_BODY));
+            }
+        } finally {
+            IoUtils.safeClose(connection);
+        }
+    }
+    */
+
+    @Test
+    public void testVoid() throws Exception {
+        //Thread.sleep(100000);
+    }
+
+    /*
+    @Test
+    public void testSimpleHttps() throws Exception {
+        final UndertowHttp2Client client = createClient();
+        final List<ClientResponse> responses = new CopyOnWriteArrayList<>();
+        final CountDownLatch latch = new CountDownLatch(10);
+        SSLContext clientSslContext = createSSLContext(loadKeyStore("client.keystore"), loadKeyStore("client.truststore"));
+        XnioSsl ssl = new UndertowXnioSsl(Xnio.getInstance(), OptionMap.EMPTY, clientSslContext);
+        final ClientConnection connection = client.connect(new URI("https://localhost:7778"), worker, ssl, pool, OptionMap.EMPTY).get();
+        try {
+            connection.getIoThread().execute(new Runnable() {
+                @Override
+                public void run() {
+                    for (int i = 0; i < 10; i++) {
+                        final ClientRequest request = new ClientRequest().setMethod(Methods.GET).setPath("/");
+                        request.getRequestHeaders().put(Headers.HOST, DefaultServer.getHostAddress());
+                        connection.sendRequest(request, createClientCallback(responses, latch));
+                    }
+                }
+            });
+
+            latch.await(10, TimeUnit.SECONDS);
+
+            Assert.assertEquals(10, responses.size());
+            for (final ClientResponse response : responses) {
+                System.out.println(response.getProtocol());
+                Assert.assertEquals(message, response.getAttachment(RESPONSE_BODY));
+            }
+        } finally {
+            connection.getIoThread().execute(new Runnable() {
+                @Override
+                public void run() {
+                    IoUtils.safeClose(connection);
+                }
+            });
+        }
+    }
+    */
+
+    /*
+    @Test
+    public void testConnectionClose() throws Exception {
+        final UndertowHttp2Client client = createClient();
+
+        final CountDownLatch latch = new CountDownLatch(1);
+        final ClientConnection connection = client.connect(new URI("http://localhost:7777"), worker, DefaultServer.getBufferPool(), OptionMap.EMPTY).get();
+        try {
+            ClientRequest request = new ClientRequest().setPath("/1324").setMethod(Methods.GET);
+            request.getRequestHeaders().put(Headers.HOST, DefaultServer.getHostAddress());
+            final List<ClientResponse> responses = new CopyOnWriteArrayList<>();
+            request.getRequestHeaders().add(Headers.CONNECTION, Headers.CLOSE.toString());
+            connection.sendRequest(request, createClientCallback(responses, latch));
+            latch.await();
+            final ClientResponse response = responses.iterator().next();
+            System.out.println(response.getProtocol());
+            Assert.assertEquals(message, response.getAttachment(RESPONSE_BODY));
+            Assert.assertEquals(false, connection.isOpen());
+        } finally {
+            IoUtils.safeClose(connection);
+        }
+    }
+    */
+
+    @Test
+    public void testHttpsConnectionClose() throws Exception {
+        final UndertowHttp2Client client = createClient();
+
+        final CountDownLatch latch = new CountDownLatch(1);
+        SSLContext clientSslContext = createSSLContext(loadKeyStore("client.keystore"), loadKeyStore("client.truststore"));
+        XnioSsl ssl = new UndertowXnioSsl(Xnio.getInstance(), OptionMap.EMPTY, clientSslContext);
+        final ClientConnection connection = client.connect(new URI("https://localhost:7778"), worker, ssl, pool, OptionMap.EMPTY).get();
+        try {
+            ClientRequest request = new ClientRequest().setPath("/1234").setMethod(Methods.GET);
+            request.getRequestHeaders().put(Headers.HOST, DefaultServer.getHostAddress());
+            final List<ClientResponse> responses = new CopyOnWriteArrayList<>();
+            request.getRequestHeaders().add(Headers.CONNECTION, Headers.CLOSE.toString());
+            connection.sendRequest(request, createClientCallback(responses, latch));
+            latch.await();
+            final ClientResponse response = responses.iterator().next();
+            System.out.println(response.getProtocol());
+            Assert.assertEquals(message, response.getAttachment(RESPONSE_BODY));
+            Assert.assertEquals(true, connection.isOpen());
+        } finally {
+            IoUtils.safeClose(connection);
+        }
+    }
+
+    private ClientCallback<ClientExchange> createClientCallback(final List<ClientResponse> responses, final CountDownLatch latch) {
+        return new ClientCallback<ClientExchange>() {
+            @Override
+            public void completed(ClientExchange result) {
+                result.setResponseListener(new ClientCallback<ClientExchange>() {
+                    @Override
+                    public void completed(final ClientExchange result) {
+                        responses.add(result.getResponse());
+                        new StringReadChannelListener(result.getConnection().getBufferPool()) {
+
+                            @Override
+                            protected void stringDone(String string) {
+                                result.getResponse().putAttachment(RESPONSE_BODY, string);
+                                latch.countDown();
+                            }
+
+                            @Override
+                            protected void error(IOException e) {
+                                e.printStackTrace();
+
+                                latch.countDown();
+                            }
+                        }.setup(result.getResponseChannel());
+                    }
+
+                    @Override
+                    public void failed(IOException e) {
+                        e.printStackTrace();
+
+                        latch.countDown();
+                    }
+                });
+                try {
+                    result.getRequestChannel().shutdownWrites();
+                    if(!result.getRequestChannel().flush()) {
+                        result.getRequestChannel().getWriteSetter().set(ChannelListeners.<StreamSinkChannel>flushingChannelListener(null, null));
+                        result.getRequestChannel().resumeWrites();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    latch.countDown();
+                }
+            }
+
+            @Override
+            public void failed(IOException e) {
+                e.printStackTrace();
+                latch.countDown();
+            }
+        };
+    }
+
+    private static KeyStore loadKeyStore(String name) throws Exception {
+        String storeLoc = System.getProperty(name);
+        final InputStream stream;
+        if(storeLoc == null) {
+            stream = Http2ClientTestCase.class.getClassLoader().getResourceAsStream(name);
+        } else {
+            stream = Files.newInputStream(Paths.get(storeLoc));
+        }
+
+        if(stream == null) {
+            throw new RuntimeException("Could not load keystore");
+        }
+        try(InputStream is = stream) {
+            KeyStore loadedKeystore = KeyStore.getInstance("JKS");
+            loadedKeystore.load(is, password(name));
+            return loadedKeystore;
+        }
+    }
+
+    static char[] password(String name) {
+        String pw = System.getProperty(name + ".password");
+        return pw != null ? pw.toCharArray() : STORE_PASSWORD;
+    }
+
+
+    private static SSLContext createSSLContext(final KeyStore keyStore, final KeyStore trustStore) throws Exception {
+        KeyManager[] keyManagers;
+        KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+        keyManagerFactory.init(keyStore, password("key"));
+        keyManagers = keyManagerFactory.getKeyManagers();
+
+        TrustManager[] trustManagers;
+        TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+        trustManagerFactory.init(trustStore);
+        trustManagers = trustManagerFactory.getTrustManagers();
+
+        SSLContext sslContext;
+        sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(keyManagers, trustManagers, null);
+
+        return sslContext;
+    }
+
+}
