b762cf96a9dfbb7d1ddbcd46dbac99008f395d23
Stuart Douglas <stuart.w.douglas@gmail.com>
2017-06-27 08:36:18 +1000

    Fix test failure on linux

--- servlet/src/test/java/io/undertow/servlet/test/defaultservlet/DefaultServletCachingTestCase.java
+++ servlet/src/test/java/io/undertow/servlet/test/defaultservlet/DefaultServletCachingTestCase.java
@@ -86,7 +86,7 @@ public class DefaultServletCachingTestCase {
                 .setClassLoader(ServletPathMappingTestCase.class.getClassLoader())
                 .setContextPath("/servletContext")
                 .setDeploymentName("servletContext.war")
-                .setResourceManager(new CachingResourceManager(100, 10000, dataCache, new PathResourceManager(tmpDir, 10485760), METADATA_MAX_AGE));
+                .setResourceManager(new CachingResourceManager(100, 10000, dataCache, new PathResourceManager(tmpDir, 10485760, false, false, false), METADATA_MAX_AGE));
 
         builder.addServlet(new ServletInfo("DefaultTestServlet", PathTestServlet.class)
                 .addMapping("/path/default"))
