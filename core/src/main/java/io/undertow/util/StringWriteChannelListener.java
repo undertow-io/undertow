eb4d55febfad02b3c769c983cf585d25396721a2
Carter Kozak <ckozak@palantir.com>
2017-08-21 14:20:23 -0400

    Expose broken client compression
    Possibly a bug with the xnio DeflatingStreamSinkConduit

--- core/src/main/java/io/undertow/util/StringWriteChannelListener.java
+++ core/src/main/java/io/undertow/util/StringWriteChannelListener.java
@@ -18,15 +18,8 @@
 
 package io.undertow.util;
 
-import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
-
-import io.undertow.UndertowLogger;
-import org.xnio.ChannelListener;
-import org.xnio.ChannelListeners;
-import org.xnio.IoUtils;
-import org.xnio.channels.StreamSinkChannel;
 
 /**
  * A simple write listener that can be used to write out the contents of a String. When the string is written
@@ -37,78 +30,13 @@ import org.xnio.channels.StreamSinkChannel;
  *
  * @author Stuart Douglas
  */
-public class StringWriteChannelListener implements ChannelListener<StreamSinkChannel> {
-
-    private final ByteBuffer buffer;
+public class StringWriteChannelListener extends ByteBufferWriteChannelListener {
 
     public StringWriteChannelListener( final String string) {
         this(string, Charset.defaultCharset());
     }
 
     public StringWriteChannelListener( final String string, Charset charset) {
-        buffer = ByteBuffer.wrap(string.getBytes(charset));
-    }
-
-    public void setup(final StreamSinkChannel channel) {
-        try {
-            int c;
-            do {
-                c = channel.write(buffer);
-            } while (buffer.hasRemaining() && c > 0);
-            if (buffer.hasRemaining()) {
-                channel.getWriteSetter().set(this);
-                channel.resumeWrites();
-            } else {
-                writeDone(channel);
-            }
-        } catch (IOException e) {
-            handleError(channel, e);
-        }
-    }
-
-    protected void handleError(StreamSinkChannel channel, IOException e) {
-        UndertowLogger.REQUEST_IO_LOGGER.ioException(e);
-        IoUtils.safeClose(channel);
-    }
-
-    @Override
-    public void handleEvent(final StreamSinkChannel channel) {
-        try {
-            int c;
-            do {
-                c = channel.write(buffer);
-            } while (buffer.hasRemaining() && c > 0);
-            if (buffer.hasRemaining()) {
-                channel.resumeWrites();
-                return;
-            } else {
-                writeDone(channel);
-            }
-        } catch (IOException e) {
-            handleError(channel, e);
-        }
-    }
-
-    public boolean hasRemaining() {
-        return buffer.hasRemaining();
-    }
-
-    protected void writeDone(final StreamSinkChannel channel) {
-        try {
-            channel.shutdownWrites();
-
-            if (!channel.flush()) {
-                channel.getWriteSetter().set(ChannelListeners.flushingChannelListener(new ChannelListener<StreamSinkChannel>() {
-                    @Override
-                    public void handleEvent(StreamSinkChannel o) {
-                        IoUtils.safeClose(channel);
-                    }
-                }, ChannelListeners.closingChannelExceptionHandler()));
-                channel.resumeWrites();
-
-            }
-        } catch (IOException e) {
-            handleError(channel, e);
-        }
+        super(ByteBuffer.wrap(string.getBytes(charset)));
     }
 }
