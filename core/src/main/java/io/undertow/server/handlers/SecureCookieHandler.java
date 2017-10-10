806dac3f591489600c06cfccdd45a205a8c74d37
Stuart Douglas <stuart.w.douglas@gmail.com>
2017-10-10 13:59:27 +0200

    UNDERTOW-1182 Add SecureCookieHandler that will place the secure flag on all cookies sent over a secure channel

---
+++ core/src/main/java/io/undertow/server/handlers/SecureCookieHandler.java
@@ -0,0 +1,88 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2014 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package io.undertow.server.handlers;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
+
+import io.undertow.server.HandlerWrapper;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.server.ResponseCommitListener;
+import io.undertow.server.handlers.builder.HandlerBuilder;
+
+public class SecureCookieHandler implements HttpHandler {
+
+    public static final HandlerWrapper WRAPPER = new HandlerWrapper() {
+        @Override
+        public HttpHandler wrap(HttpHandler handler) {
+            return new SecureCookieHandler(handler);
+        }
+    };
+
+    private final HttpHandler next;
+
+    public SecureCookieHandler(HttpHandler next) {
+        this.next = next;
+    }
+
+    @Override
+    public void handleRequest(HttpServerExchange exchange) throws Exception {
+        if(exchange.getRequestScheme().equals("https")) {
+            exchange.addResponseCommitListener(new ResponseCommitListener() {
+                @Override
+                public void beforeCommit(HttpServerExchange exchange) {
+                    for(Map.Entry<String, Cookie> cookie : exchange.getResponseCookies().entrySet()) {
+                        cookie.getValue().setSecure(true);
+                    }
+                }
+            });
+        }
+        next.handleRequest(exchange);
+    }
+
+    public static class Builder implements HandlerBuilder {
+
+        @Override
+        public String name() {
+            return "secure-cookie";
+        }
+
+        @Override
+        public Map<String, Class<?>> parameters() {
+            return Collections.emptyMap();
+        }
+
+        @Override
+        public Set<String> requiredParameters() {
+            return Collections.emptySet();
+        }
+
+        @Override
+        public String defaultParameter() {
+            return null;
+        }
+
+        @Override
+        public HandlerWrapper build(Map<String, Object> config) {
+            return WRAPPER;
+        }
+    }
+}
---
+++ core/src/test/java/io/undertow/server/handlers/SecureCookieHandlerTestCase.java
@@ -0,0 +1,77 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2014 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package io.undertow.server.handlers;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+import org.apache.http.Header;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.testutils.DefaultServer;
+import io.undertow.testutils.TestHttpClient;
+import io.undertow.util.FileUtils;
+import io.undertow.util.StatusCodes;
+
+/**
+ * @author Stuart Douglas
+ */
+@RunWith(DefaultServer.class)
+public class SecureCookieHandlerTestCase {
+
+
+    @Test
+    public void testSecureCookieHandler() throws IOException, GeneralSecurityException {
+
+        DefaultServer.setRootHandler(new SecureCookieHandler(new HttpHandler() {
+            @Override
+            public void handleRequest(final HttpServerExchange exchange) throws Exception {
+                exchange.getResponseCookies().put("foo", new CookieImpl("foo", "bar"));
+            }
+        }));
+
+        DefaultServer.startSSLServer();
+        TestHttpClient client = new TestHttpClient();
+        client.setSSLContext(DefaultServer.getClientSSLContext());
+        try {
+            HttpGet get = new HttpGet(DefaultServer.getDefaultServerSSLAddress());
+            HttpResponse result = client.execute(get);
+            Assert.assertEquals(StatusCodes.OK, result.getStatusLine().getStatusCode());
+            Header header = result.getFirstHeader("set-cookie");
+            Assert.assertEquals("foo=bar; secure", header.getValue());
+            FileUtils.readFile(result.getEntity().getContent());
+
+            get = new HttpGet(DefaultServer.getDefaultServerURL());
+            result = client.execute(get);
+            Assert.assertEquals(StatusCodes.OK, result.getStatusLine().getStatusCode());
+            header = result.getFirstHeader("set-cookie");
+            Assert.assertEquals("foo=bar", header.getValue());
+        } finally {
+            client.getConnectionManager().shutdown();
+            DefaultServer.stopSSLServer();
+        }
+    }
+
+}
