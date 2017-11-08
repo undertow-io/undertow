8623e0ee58f7f7bd124e58d8c3d4ca392aea3cd9
Stuart Douglas <stuart.w.douglas@gmail.com>
2017-11-09 09:51:57 +1100

    Ignore test when running under proxy

--- servlet/src/test/java/io/undertow/servlet/test/multipart/MultiPartTestCase.java
+++ servlet/src/test/java/io/undertow/servlet/test/multipart/MultiPartTestCase.java
@@ -32,6 +32,7 @@ import io.undertow.servlet.api.LoggingExceptionHandler;
 import io.undertow.servlet.test.util.DeploymentUtils;
 import io.undertow.testutils.DefaultServer;
 import io.undertow.testutils.HttpClientUtils;
+import io.undertow.testutils.ProxyIgnore;
 import io.undertow.testutils.TestHttpClient;
 import io.undertow.util.StatusCodes;
 import org.apache.http.HttpResponse;
@@ -53,6 +54,7 @@ import static io.undertow.servlet.Servlets.servlet;
  * @author Stuart Douglas
  */
 @RunWith(DefaultServer.class)
+@ProxyIgnore
 public class MultiPartTestCase {
 
 
