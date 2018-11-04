957eca02419178df3ab70ae6639d57e7c6c66e3b
ORD Tester <ordtesters@gmail.com>
2018-11-04 13:00:32 -0600

    [UNDERTOW-1435] Fixing flaky test AnnotatedEndpointTest.testCloseReason.

--- websockets-jsr/src/test/java/io/undertow/websockets/jsr/test/annotated/AnnotatedEndpointTest.java
+++ websockets-jsr/src/test/java/io/undertow/websockets/jsr/test/annotated/AnnotatedEndpointTest.java
@@ -181,6 +181,7 @@ public class AnnotatedEndpointTest {
 
     @Test
     public void testCloseReason() throws Exception {
+        AnnotatedClientEndpoint.reset();
         MessageEndpoint.reset();
 
         Session session = deployment.connectToServer(AnnotatedClientEndpoint.class, new URI("ws://" + DefaultServer.getHostAddress("default") + ":" + DefaultServer.getHostPort("default") + "/ws/chat/Bob"));
