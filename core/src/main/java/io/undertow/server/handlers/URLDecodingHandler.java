50064f30ba3142346683f8dcb9356bf03a0ac191
Carter Kozak <ckozak@apache.org>
2019-01-08 12:21:20 -0500

    Refactor URLDecodingHandler into smaller digestable methods

--- core/src/main/java/io/undertow/server/handlers/URLDecodingHandler.java
+++ core/src/main/java/io/undertow/server/handlers/URLDecodingHandler.java
@@ -58,35 +58,46 @@ public class URLDecodingHandler implements HttpHandler {
         boolean decodeDone = exchange.getConnection().getUndertowOptions().get(UndertowOptions.DECODE_URL, true);
         if (!decodeDone) {
             final StringBuilder sb = new StringBuilder();
-            final boolean decodeSlash = exchange.getConnection().getUndertowOptions().get(UndertowOptions.ALLOW_ENCODED_SLASH, false);
-            exchange.setRequestPath(URLUtils.decode(exchange.getRequestPath(), charset, decodeSlash, false, sb));
-            exchange.setRelativePath(URLUtils.decode(exchange.getRelativePath(), charset, decodeSlash, false, sb));
-            exchange.setResolvedPath(URLUtils.decode(exchange.getResolvedPath(), charset, decodeSlash, false, sb));
-            if (!exchange.getQueryString().isEmpty()) {
-                final TreeMap<String, Deque<String>> newParams = new TreeMap<>();
-                for (Map.Entry<String, Deque<String>> param : exchange.getQueryParameters().entrySet()) {
-                    final Deque<String> newVales = new ArrayDeque<>(param.getValue().size());
-                    for (String val : param.getValue()) {
-                        newVales.add(URLUtils.decode(val, charset, true, true, sb));
-                    }
-                    newParams.put(URLUtils.decode(param.getKey(), charset, true, true, sb), newVales);
-                }
-                exchange.getQueryParameters().clear();
-                exchange.getQueryParameters().putAll(newParams);
-            }
-            PathTemplateMatch pathTemplateMatch = exchange.getAttachment(PathTemplateMatch.ATTACHMENT_KEY);
-            if (pathTemplateMatch != null) {
-                Map<String, String> parameters = pathTemplateMatch.getParameters();
-                if (parameters != null) {
-                    for (Map.Entry<String, String> entry : parameters.entrySet()) {
-                        entry.setValue(URLUtils.decode(entry.getValue(), charset, true, false, sb));
-                    }
-                }
-            }
+            decodePath(exchange, charset, sb);
+            decodeQueryString(exchange, charset, sb);
+            decodePathTemplateMatch(exchange, charset, sb);
         }
         next.handleRequest(exchange);
     }
 
+    private static void decodePath(HttpServerExchange exchange, String charset, StringBuilder sb) {
+        final boolean decodeSlash = exchange.getConnection().getUndertowOptions().get(UndertowOptions.ALLOW_ENCODED_SLASH, false);
+        exchange.setRequestPath(URLUtils.decode(exchange.getRequestPath(), charset, decodeSlash, false, sb));
+        exchange.setRelativePath(URLUtils.decode(exchange.getRelativePath(), charset, decodeSlash, false, sb));
+        exchange.setResolvedPath(URLUtils.decode(exchange.getResolvedPath(), charset, decodeSlash, false, sb));
+    }
+
+    private static void decodeQueryString(HttpServerExchange exchange, String charset, StringBuilder sb) {
+        if (!exchange.getQueryString().isEmpty()) {
+            final TreeMap<String, Deque<String>> newParams = new TreeMap<>();
+            for (Map.Entry<String, Deque<String>> param : exchange.getQueryParameters().entrySet()) {
+                final Deque<String> newVales = new ArrayDeque<>(param.getValue().size());
+                for (String val : param.getValue()) {
+                    newVales.add(URLUtils.decode(val, charset, true, true, sb));
+                }
+                newParams.put(URLUtils.decode(param.getKey(), charset, true, true, sb), newVales);
+            }
+            exchange.getQueryParameters().clear();
+            exchange.getQueryParameters().putAll(newParams);
+        }
+    }
+
+    private static void decodePathTemplateMatch(HttpServerExchange exchange, String charset, StringBuilder sb) {
+        PathTemplateMatch pathTemplateMatch = exchange.getAttachment(PathTemplateMatch.ATTACHMENT_KEY);
+        if (pathTemplateMatch != null) {
+            Map<String, String> parameters = pathTemplateMatch.getParameters();
+            if (parameters != null) {
+                for (Map.Entry<String, String> entry : parameters.entrySet()) {
+                    entry.setValue(URLUtils.decode(entry.getValue(), charset, true, false, sb));
+                }
+            }
+        }
+    }
 
     public static class Builder implements HandlerBuilder {
 
