e6a17da545663f75933648cef4043d0eb016f9c1
Mark Elliot <mark.elliot@gmail.com>
2019-01-23 00:51:38 +0000

    Fix a minor typo in JavaDoc of ExchangeCompletionListener

--- core/src/main/java/io/undertow/server/ExchangeCompletionListener.java
+++ core/src/main/java/io/undertow/server/ExchangeCompletionListener.java
@@ -22,7 +22,7 @@ package io.undertow.server;
  * Listener interface for events that are run at the completion of a request/response
  * cycle (i.e. when the request has been completely read, and the response has been fully written).
  *
- * At this point it is to late to modify the exchange further.
+ * At this point it is too late to modify the exchange further.
  *
  * Completion listeners are invoked in reverse order,
  *
