f6c6966bd194489f9adf1fbb2385e08ad607e7fc
jjYBdx4IL <jjYBdx4IL@github.com>
2019-02-19 21:40:31 +0100

    fix license headers

--- core/src/main/java/io/undertow/attribute/RemoteObfuscatedIPAttribute.java
+++ core/src/main/java/io/undertow/attribute/RemoteObfuscatedIPAttribute.java
@@ -1,6 +1,6 @@
 /*
  * JBoss, Home of Professional Open Source.
- * Copyright 2014 Red Hat, Inc., and individual contributors
+ * Copyright 2019 Red Hat, Inc., and individual contributors
  * as indicated by the @author tags.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
