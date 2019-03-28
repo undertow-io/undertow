20cacc96c0594f4f4f9bb1cc2b93a77b6be3f74c
Carter Kozak <ckozak@ckozak.net>
2019-03-28 08:09:12 -0400

    [UNDERTOW-1515] HttpServerExchange.toString does not include headers
    This reverts commit 51144633f22ba0e73ee1f435db72025720395797.

--- core/src/main/java/io/undertow/server/HttpServerExchange.java
+++ core/src/main/java/io/undertow/server/HttpServerExchange.java
@@ -2441,6 +2441,6 @@ public final class HttpServerExchange extends AbstractAttachable {
 
     @Override
     public String toString() {
-        return "HttpServerExchange{ " + getRequestMethod().toString() + " " + getRequestURI() + " request " + requestHeaders + " response " + responseHeaders + '}';
+        return "HttpServerExchange{ " + getRequestMethod().toString() + " " + getRequestURI() + '}';
     }
 }
