e7fefe6815026d161c37cee84577d55da728daa3
Flavia Rainone <frainone@redhat.com>
2019-05-23 17:53:09 -0300

    [UNDERTOW-1534] Remove unused import from AbstractFramedChannel

--- core/src/main/java/io/undertow/server/protocol/framed/AbstractFramedChannel.java
+++ core/src/main/java/io/undertow/server/protocol/framed/AbstractFramedChannel.java
@@ -34,7 +34,6 @@ import java.util.List;
 import java.util.ListIterator;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.concurrent.CopyOnWriteArraySet;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.RejectedExecutionException;
 import java.util.concurrent.TimeUnit;
