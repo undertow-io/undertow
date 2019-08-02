f78abf31e049fcccd275f7fd8d78292ed7019aa7
Masafumi Miura <mmiura@redhat.com>
2019-08-02 09:28:23 +0900

    UNDERTOW-1606 Output undertow version logging at INFO level on Undertow#start() and Undertow#stop()

--- core/src/main/java/io/undertow/Undertow.java
+++ core/src/main/java/io/undertow/Undertow.java
@@ -114,7 +114,7 @@ public final class Undertow {
     }
 
     public synchronized void start() {
-        UndertowLogger.ROOT_LOGGER.debugf("starting undertow server %s", this);
+        UndertowLogger.ROOT_LOGGER.infof("starting server: %s", Version.getFullVersionString());
         xnio = Xnio.getInstance(Undertow.class.getClassLoader());
         channels = new ArrayList<>();
         try {
@@ -249,7 +249,7 @@ public final class Undertow {
     }
 
     public synchronized void stop() {
-        UndertowLogger.ROOT_LOGGER.debugf("stopping undertow server %s", this);
+        UndertowLogger.ROOT_LOGGER.infof("stopping server: %s", Version.getFullVersionString());
         if (channels != null) {
             for (AcceptingChannel<? extends StreamConnection> channel : channels) {
                 IoUtils.safeClose(channel);
