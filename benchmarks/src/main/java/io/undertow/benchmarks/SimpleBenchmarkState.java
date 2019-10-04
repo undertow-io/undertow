328f15596682da503d7c7584f70982ccd19717e8
Carter Kozak <ckozak@ckozak.net>
2019-10-04 13:40:04 -0400

    Use IO-threads based on detected cores

--- benchmarks/src/main/java/io/undertow/benchmarks/SimpleBenchmarkState.java
+++ benchmarks/src/main/java/io/undertow/benchmarks/SimpleBenchmarkState.java
@@ -62,8 +62,8 @@ public class SimpleBenchmarkState {
     @Setup
     public final void before() {
         Undertow.Builder builder = Undertow.builder()
-                .setIoThreads(4)
-                .setWorkerThreads(64)
+                .setIoThreads(computeIoThreadCount())
+                .setWorkerThreads(256)
                 .setServerOption(UndertowOptions.SHUTDOWN_TIMEOUT, 10000)
                 .setSocketOption(Options.SSL_CLIENT_AUTH_MODE, SslClientAuthMode.NOT_REQUESTED)
                 .setHandler(Handlers.routing()
@@ -120,6 +120,29 @@ public class SimpleBenchmarkState {
                         request.addHeader("Connection", "close"))
                 .build();
         baseUri = (listenerType == ListenerType.HTTP ? "http" : "https") + "://localhost:" + PORT;
+    }
+
+    private static int computeIoThreadCount() {
+        int logicalCores = Runtime.getRuntime().availableProcessors();
+        if (logicalCores % 2 == 0) {
+            // https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=07f4c90062f8fc7c8c26f8f95324cbe8fa3145a5
+            // From the commit message:
+            // > In this patch, I changed the starting point in __inet_hash_connect()
+            // > so that we try to favor even [1] ports, leaving odd ports for bind()
+            // > users.
+            //
+            // Which conflicts with the xnio hashing function, which picks an
+            // IO thread using (hash(addr) * 23 + port) % IO_THREADS.
+            // reference:
+            // https://github.com/xnio/xnio/blob/6bf7f1e5b9e56f14db28deea7e38db3f624fbb35/nio-impl/src/main/java/org/xnio/nio/QueuedNioTcpServer.java#L495
+            //
+            // When the port component is always even, only half of our IO threads will
+            // be used.
+            //
+            // Tracking: https://issues.jboss.org/browse/XNIO-328
+            logicalCores--;
+        }
+        return Math.max(1, logicalCores);
     }
 
     @TearDown
