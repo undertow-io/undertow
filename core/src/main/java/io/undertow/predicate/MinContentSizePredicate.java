9428466e9f8eceefd9121bf4679756a9c1a88d32
Brad Wood <brad@bradwood.com>
2020-07-08 01:00:52 -0500

    Implemented new predicates

--- core/src/main/java/io/undertow/predicate/MinContentSizePredicate.java
+++ core/src/main/java/io/undertow/predicate/MinContentSizePredicate.java
@@ -29,8 +29,11 @@ import io.undertow.util.Headers;
  * Predicate that returns true if the Content-Size of a request is below a
  * given value.
  *
+ * Use {@link RequestSmallerThanPredicate} instead.
+ *
  * @author Stuart Douglas
  */
+@Deprecated
 public class MinContentSizePredicate implements Predicate {
 
     private final long minSize;
