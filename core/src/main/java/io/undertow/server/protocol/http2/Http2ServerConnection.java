f0c0d5c29ea49f7e6812bed93ba64702945f4c08
Björn Svensson <bjorn.a.svensson@est.tech>
2021-05-17 11:35:46 +0200

    Track HTTP/2 channels in GracefulShutdownHandler
    Signed-off-by: Björn Svensson <bjorn.a.svensson@est.tech>

--- core/src/main/java/io/undertow/server/protocol/http2/Http2ServerConnection.java
+++ core/src/main/java/io/undertow/server/protocol/http2/Http2ServerConnection.java
@@ -162,6 +162,10 @@ public class Http2ServerConnection extends ServerConnection {
         return channel.getSslSession();
     }
 
+    public Http2Channel getChannel() {
+        return channel;
+    }
+
     @Override
     public ByteBufferPool getByteBufferPool() {
         return channel.getBufferPool();
