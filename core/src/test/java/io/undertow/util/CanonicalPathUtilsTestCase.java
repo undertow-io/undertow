55c6ccf53591826ab2f39c2e69869e5ac4cf0894
Flavia Rainone <frainone@redhat.com>
2021-05-25 08:37:18 -0300

    [UNDERTOW-1886] Add back nullAllowed option to CanonicalPathUtils, while still keeping the system property out.
    This partially reverts commit 66c6252073075e7c37703545160a162c3b0a6fed.

--- core/src/test/java/io/undertow/util/CanonicalPathUtilsTestCase.java
+++ core/src/test/java/io/undertow/util/CanonicalPathUtilsTestCase.java
@@ -63,9 +63,11 @@ public class CanonicalPathUtilsTestCase {
         Assert.assertEquals("/b", CanonicalPathUtils.canonicalize("/a/c/../../b"));
 
         // out of servlet context
-        Assert.assertNull(CanonicalPathUtils.canonicalize("/a/../.."));
-        Assert.assertNull(CanonicalPathUtils.canonicalize("/a/../../foo"));
-        Assert.assertNull(CanonicalPathUtils.canonicalize("/../../a/b/bar"));
+        Assert.assertNull(CanonicalPathUtils.canonicalize("/a/../..", true));
+        Assert.assertNull(CanonicalPathUtils.canonicalize("/a/../../foo", true));
+        Assert.assertNull(CanonicalPathUtils.canonicalize("/../../a/b/bar", true));
+        Assert.assertEquals("/", CanonicalPathUtils.canonicalize("/a/../.."));
+        Assert.assertEquals("/foo", CanonicalPathUtils.canonicalize("/a/../../foo"));
 
         //preserve (single) trailing /
         Assert.assertEquals("/a/", CanonicalPathUtils.canonicalize("/a/"));
@@ -106,9 +108,11 @@ public class CanonicalPathUtilsTestCase {
         Assert.assertEquals("\\b", CanonicalPathUtils.canonicalize("\\a\\c\\..\\..\\b"));
 
          // out of servlet context
-        Assert.assertNull(CanonicalPathUtils.canonicalize("\\a\\..\\.."));
-        Assert.assertNull(CanonicalPathUtils.canonicalize("\\a\\..\\..\\foo"));
-        Assert.assertNull(CanonicalPathUtils.canonicalize("\\..\\..\\a\\b\\bar"));
+        Assert.assertNull(CanonicalPathUtils.canonicalize("\\a\\..\\..", true));
+        Assert.assertNull(CanonicalPathUtils.canonicalize("\\a\\..\\..\\foo", true));
+        Assert.assertNull(CanonicalPathUtils.canonicalize("\\..\\..\\a\\b\\bar", true));
+        Assert.assertEquals("/", CanonicalPathUtils.canonicalize("\\a\\..\\.."));
+        Assert.assertEquals("\\foo", CanonicalPathUtils.canonicalize("\\a\\..\\..\\foo"));
 
         //preserve (single) trailing \
         Assert.assertEquals("\\a\\", CanonicalPathUtils.canonicalize("\\a\\"));
