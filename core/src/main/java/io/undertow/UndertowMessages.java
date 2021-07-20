4d3a8a3032cc429efb26d8859df092c57756a570
Flavia Rainone <frainone@redhat.com>
2021-07-20 02:32:50 -0300

    [UNDERTOW-1858] Fix ReadTimeoutTestCase (#1050)
    [UNDERTOW-1858] Fix ReadTimeoutTestCase

--- core/src/main/java/io/undertow/UndertowMessages.java
+++ core/src/main/java/io/undertow/UndertowMessages.java
@@ -618,4 +618,7 @@ public interface UndertowMessages {
 
     @Message(id = 198, value = "Blocking write timed out after %s nanoseconds.")
     WriteTimeoutException blockingWriteTimedOut(long timeoutNanoseconds);
+
+    @Message(id = 199, value = "Read timed out after %s milliseconds.")
+    ReadTimeoutException readTimedOut(long timeoutMilliseconds);
 }
--- core/src/main/java/io/undertow/conduits/ReadTimeoutStreamSourceConduit.java
+++ core/src/main/java/io/undertow/conduits/ReadTimeoutStreamSourceConduit.java
@@ -18,26 +18,28 @@
 
 package io.undertow.conduits;
 
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.concurrent.TimeUnit;
+
 import io.undertow.UndertowLogger;
+import io.undertow.UndertowMessages;
 import io.undertow.UndertowOptions;
 import io.undertow.server.OpenListener;
 import io.undertow.util.WorkerUtils;
-
+import org.xnio.ChannelListener;
 import org.xnio.ChannelListeners;
 import org.xnio.IoUtils;
 import org.xnio.Options;
 import org.xnio.StreamConnection;
 import org.xnio.XnioExecutor;
+import org.xnio.channels.ReadTimeoutException;
 import org.xnio.channels.StreamSinkChannel;
 import org.xnio.conduits.AbstractStreamSourceConduit;
+import org.xnio.conduits.ConduitStreamSourceChannel;
 import org.xnio.conduits.ReadReadyHandler;
 import org.xnio.conduits.StreamSourceConduit;
-
-import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.channels.ClosedChannelException;
-import java.nio.channels.FileChannel;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Wrapper for read timeout. This should always be the first wrapper applied to the underlying channel.
@@ -53,6 +55,7 @@ public final class ReadTimeoutStreamSourceConduit extends AbstractStreamSourceCo
     private final OpenListener openListener;
 
     private static final int FUZZ_FACTOR = 50; //we add 50ms to the timeout to make sure the underlying channel has actually timed out
+    private volatile boolean expired;
 
     private final Runnable timeoutCommand = new Runnable() {
         @Override
@@ -68,13 +71,20 @@ public final class ReadTimeoutStreamSourceConduit extends AbstractStreamSourceCo
                 return;
             }
             UndertowLogger.REQUEST_LOGGER.tracef("Timing out channel %s due to inactivity", connection.getSourceChannel());
-            IoUtils.safeClose(connection);
-            if (connection.getSourceChannel().isReadResumed()) {
-                ChannelListeners.invokeChannelListener(connection.getSourceChannel(), connection.getSourceChannel().getReadListener());
+            synchronized (ReadTimeoutStreamSourceConduit.this) {
+                expired = true;
+            }
+            boolean readResumed = connection.getSourceChannel().isReadResumed();
+            ChannelListener<? super ConduitStreamSourceChannel> readListener = connection.getSourceChannel().getReadListener();
+
+            if (readResumed) {
+                ChannelListeners.invokeChannelListener(connection.getSourceChannel(), readListener);
             }
             if (connection.getSinkChannel().isWriteResumed()) {
                 ChannelListeners.invokeChannelListener(connection.getSinkChannel(), connection.getSinkChannel().getWriteListener());
             }
+            // close only after invoking listeners, to allow space for listener getting ReadTimeoutException
+            IoUtils.safeClose(connection);
         }
     };
 
@@ -108,28 +118,31 @@ public final class ReadTimeoutStreamSourceConduit extends AbstractStreamSourceCo
             cleanup();
             return;
         }
-        if(ret == -1) {
+        if (ret == -1) {
             cleanup();
-            return;
-        }
-        if (ret == 0 && handle != null) {
             return;
         }
         Integer timeout = getTimeout();
         if (timeout == null || timeout <= 0) {
             return;
         }
-        long currentTime = System.currentTimeMillis();
-        long expireTimeVar = expireTime;
-        if (expireTimeVar != -1 && currentTime > expireTimeVar) {
-            IoUtils.safeClose(connection);
-            throw new ClosedChannelException();
+        final long currentTime = System.currentTimeMillis();
+        if (ret == 0) {
+            final long expireTimeVar = expireTime;
+            if (expireTimeVar != -1 && currentTime > expireTimeVar) {
+                IoUtils.safeClose(connection);
+                throw UndertowMessages.MESSAGES.readTimedOut(this.getTimeout());
+            }
         }
         expireTime = currentTime + timeout;
+        if (handle == null) {
+            handle = connection.getIoThread().executeAfter(timeoutCommand, timeout, TimeUnit.MILLISECONDS);
+        }
     }
 
     @Override
     public long transferTo(final long position, final long count, final FileChannel target) throws IOException {
+        checkExpired();
         long ret = super.transferTo(position, count, target);
         handleReadTimeout(ret);
         return ret;
@@ -137,6 +150,7 @@ public final class ReadTimeoutStreamSourceConduit extends AbstractStreamSourceCo
 
     @Override
     public long transferTo(final long count, final ByteBuffer throughBuffer, final StreamSinkChannel target) throws IOException {
+        checkExpired();
         long ret = super.transferTo(count, throughBuffer, target);
         handleReadTimeout(ret);
         return ret;
@@ -144,6 +158,7 @@ public final class ReadTimeoutStreamSourceConduit extends AbstractStreamSourceCo
 
     @Override
     public long read(final ByteBuffer[] dsts, final int offset, final int length) throws IOException {
+        checkExpired();
         long ret = super.read(dsts, offset, length);
         handleReadTimeout(ret);
         return ret;
@@ -151,6 +166,7 @@ public final class ReadTimeoutStreamSourceConduit extends AbstractStreamSourceCo
 
     @Override
     public int read(final ByteBuffer dst) throws IOException {
+        checkExpired();
         int ret = super.read(dst);
         handleReadTimeout(ret);
         return ret;
@@ -158,6 +174,7 @@ public final class ReadTimeoutStreamSourceConduit extends AbstractStreamSourceCo
 
     @Override
     public void awaitReadable() throws IOException {
+        checkExpired();
         Integer timeout = getTimeout();
         if (timeout != null && timeout > 0) {
             super.awaitReadable(timeout + FUZZ_FACTOR, TimeUnit.MILLISECONDS);
@@ -168,6 +185,7 @@ public final class ReadTimeoutStreamSourceConduit extends AbstractStreamSourceCo
 
     @Override
     public void awaitReadable(long time, TimeUnit timeUnit) throws IOException {
+        checkExpired();
         Integer timeout = getTimeout();
         if (timeout != null && timeout > 0) {
             long millis = timeUnit.toMillis(time);
@@ -195,49 +213,34 @@ public final class ReadTimeoutStreamSourceConduit extends AbstractStreamSourceCo
 
     @Override
     public void terminateReads() throws IOException {
+        checkExpired();
         super.terminateReads();
         cleanup();
     }
 
     private void cleanup() {
-        if(handle != null) {
+        if (handle != null) {
             handle.remove();
             handle = null;
+            expireTime = -1;
         }
-    }
-
-    @Override
-    public void resumeReads() {
-        super.resumeReads();
-        handleResumeTimeout();
     }
 
     @Override
     public void suspendReads() {
         super.suspendReads();
-        XnioExecutor.Key handle = this.handle;
-        if(handle != null) {
-            handle.remove();
-            this.handle = null;
+        cleanup();
+    }
+
+    private void checkExpired() throws ReadTimeoutException {
+        synchronized (this) {
+            if (expired) {
+                throw UndertowMessages.MESSAGES.readTimedOut(System.currentTimeMillis());
+            }
         }
     }
 
-    @Override
-    public void wakeupReads() {
-        super.wakeupReads();
-        handleResumeTimeout();
-    }
-
-    private void handleResumeTimeout() {
-        Integer timeout = getTimeout();
-        if (timeout == null || timeout <= 0) {
-            return;
-        }
-        long currentTime = System.currentTimeMillis();
-        expireTime = currentTime + timeout;
-        XnioExecutor.Key key = handle;
-        if (key == null) {
-            handle = connection.getIoThread().executeAfter(timeoutCommand, timeout, TimeUnit.MILLISECONDS);
-        }
+    public String toString() {
+        return super.toString() + " (next: " + next + ")";
     }
 }
