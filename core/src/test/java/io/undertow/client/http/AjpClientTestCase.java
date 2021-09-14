7fb00a99086e00ded7e0f3b5128073071651e29e
Flavia Rainone <frainone@redhat.com>
2021-09-13 22:56:25 -0300

    [UNDERTOW-1963] Tidy up AjpClientTestCase and add lambdas

--- core/src/test/java/io/undertow/client/http/AjpClientTestCase.java
+++ core/src/test/java/io/undertow/client/http/AjpClientTestCase.java
@@ -33,9 +33,7 @@ import io.undertow.client.ClientExchange;
 import io.undertow.client.ClientRequest;
 import io.undertow.client.ClientResponse;
 import io.undertow.client.UndertowClient;
-import io.undertow.io.Receiver;
 import io.undertow.io.Sender;
-import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.server.handlers.PathHandler;
 import io.undertow.testutils.DefaultServer;
@@ -61,6 +59,7 @@ import org.xnio.XnioWorker;
 import org.xnio.channels.StreamSinkChannel;
 
 import static io.undertow.testutils.StopServerWithExternalWorkerUtils.stopWorker;
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author Emanuel Muckenhuber
@@ -106,29 +105,13 @@ public class AjpClientTestCase {
     @BeforeClass
     public static void beforeClass() throws IOException {
         // Create xnio worker
-        final Xnio xnio = Xnio.getInstance();
-        final XnioWorker xnioWorker = xnio.createWorker(null, DEFAULT_OPTIONS);
-        worker = xnioWorker;
+        worker = Xnio.getInstance().createWorker(null, DEFAULT_OPTIONS);
         undertow = Undertow.builder().addListener(new Undertow.ListenerBuilder().setType(Undertow.ListenerType.AJP).setPort(AJP_PORT))
-        .setHandler(new PathHandler()
-        .addExactPath(MESSAGE, new HttpHandler() {
-            @Override
-            public void handleRequest(HttpServerExchange exchange) throws Exception {
-                sendMessage(exchange);
-            }
-        })
-        .addExactPath(POST, new HttpHandler() {
-            @Override
-            public void handleRequest(HttpServerExchange exchange) throws Exception {
-                exchange.getRequestReceiver().receiveFullString(new Receiver.FullStringCallback() {
-                    @Override
-                    public void handle(HttpServerExchange exchange, String message) {
-                        exchange.getResponseSender().send(message);
-                    }
-                });
-            }
-        }))
-        .build();
+            .setHandler(new PathHandler()
+            .addExactPath(MESSAGE, AjpClientTestCase::sendMessage)
+            .addExactPath(POST, exchange -> exchange.getRequestReceiver().receiveFullString(
+                (exchange1, message) -> exchange1.getResponseSender().send(message))))
+            .build();
         undertow.start();
     }
 
@@ -139,10 +122,6 @@ public class AjpClientTestCase {
     }
 
     static UndertowClient createClient() {
-        return createClient(OptionMap.EMPTY);
-    }
-
-    static UndertowClient createClient(final OptionMap options) {
         return UndertowClient.getInstance();
     }
 
@@ -155,19 +134,15 @@ public class AjpClientTestCase {
         final CountDownLatch latch = new CountDownLatch(10);
         final ClientConnection connection = client.connect(ADDRESS, worker, DefaultServer.getBufferPool(), OptionMap.EMPTY).get();
         try {
-            connection.getIoThread().execute(new Runnable() {
-                @Override
-                public void run() {
-                    for (int i = 0; i < 10; i++) {
-                        final ClientRequest request = new ClientRequest().setMethod(Methods.GET).setPath(MESSAGE);
-                        request.getRequestHeaders().put(Headers.HOST, DefaultServer.getHostAddress());
-                        connection.sendRequest(request, createClientCallback(responses, latch));
-                    }
+            connection.getIoThread().execute(() -> {
+                for (int i = 0; i < 10; i++) {
+                    final ClientRequest request = new ClientRequest().setMethod(Methods.GET).setPath(MESSAGE);
+                    request.getRequestHeaders().put(Headers.HOST, DefaultServer.getHostAddress());
+                    connection.sendRequest(request, createClientCallback(responses, latch));
                 }
-
             });
 
-            latch.await(10, TimeUnit.SECONDS);
+            assertTrue(latch.await(10, TimeUnit.SECONDS));
 
             Assert.assertEquals(10, responses.size());
             for (final ClientResponse response : responses) {
@@ -188,35 +163,32 @@ public class AjpClientTestCase {
         final FutureResult<Boolean> result = new FutureResult<>();
         final CountDownLatch latch = new CountDownLatch(3);
         final ClientConnection connection = client.connect(ADDRESS, worker, DefaultServer.getBufferPool(), OptionMap.EMPTY).get();
-        Assert.assertTrue(connection.isPingSupported());
+        assertTrue(connection.isPingSupported());
         try {
-            connection.getIoThread().execute(new Runnable() {
-                @Override
-                public void run() {
-                        final ClientRequest request = new ClientRequest().setMethod(Methods.GET).setPath(MESSAGE);
-                        request.getRequestHeaders().put(Headers.HOST, DefaultServer.getHostAddress());
-                        connection.sendRequest(request, createClientCallback(responses, latch));
-                        connection.sendPing(new ClientConnection.PingListener() {
-                            @Override
-                            public void acknowledged() {
-                                result.setResult(true);
-                                latch.countDown();
-                            }
+            connection.getIoThread().execute(() -> {
+                    final ClientRequest request = new ClientRequest().setMethod(Methods.GET).setPath(MESSAGE);
+                    request.getRequestHeaders().put(Headers.HOST, DefaultServer.getHostAddress());
+                    connection.sendRequest(request, createClientCallback(responses, latch));
+                    connection.sendPing(new ClientConnection.PingListener() {
+                        @Override
+                        public void acknowledged() {
+                            result.setResult(true);
+                            latch.countDown();
+                        }
 
-                            @Override
-                            public void failed(IOException e) {
-                                result.setException(e);
-                                latch.countDown();
-                            }
-                        }, 5, TimeUnit.SECONDS);
-                        connection.sendRequest(request, createClientCallback(responses, latch));
-                    }
+                        @Override
+                        public void failed(IOException e) {
+                            result.setException(e);
+                            latch.countDown();
+                        }
+                    }, 5, TimeUnit.SECONDS);
+                    connection.sendRequest(request, createClientCallback(responses, latch));
                 });
 
-            latch.await(10, TimeUnit.SECONDS);
+            assertTrue(latch.await(10, TimeUnit.SECONDS));
 
             Assert.assertEquals(2, responses.size());
-            Assert.assertTrue(result.getIoFuture().get());
+            assertTrue(result.getIoFuture().get());
             for (final ClientResponse response : responses) {
                 Assert.assertEquals(message, response.getAttachment(RESPONSE_BODY));
             }
@@ -226,23 +198,18 @@ public class AjpClientTestCase {
                 undertow.stop();
 
                 final FutureResult<Boolean> failResult = new FutureResult<>();
-                connection.getIoThread().execute(new Runnable() {
+                connection.getIoThread().execute(() -> connection.sendPing(new ClientConnection.PingListener() {
                     @Override
-                    public void run() {
-                        connection.sendPing(new ClientConnection.PingListener() {
-                            @Override
-                            public void acknowledged() {
-                                failResult.setResult(true);
-                            }
-
-                            @Override
-                            public void failed(IOException e) {
-                                failResult.setException(e);
-
-                            }
-                        }, 4, TimeUnit.SECONDS);
+                    public void acknowledged() {
+                        failResult.setResult(true);
                     }
-                });
+
+                    @Override
+                    public void failed(IOException e) {
+                        failResult.setException(e);
+
+                    }
+                }, 4, TimeUnit.SECONDS));
                 try {
                     failResult.getIoFuture().get();
                     Assert.fail("ping should have failed");
@@ -270,56 +237,52 @@ public class AjpClientTestCase {
         final CountDownLatch latch = new CountDownLatch(10);
         final ClientConnection connection = client.connect(ADDRESS, worker, DefaultServer.getBufferPool(), OptionMap.EMPTY).get();
         try {
-            connection.getIoThread().execute(new Runnable() {
-                @Override
-                public void run() {
-                    for (int i = 0; i < 10; i++) {
-                        final ClientRequest request = new ClientRequest().setMethod(Methods.POST).setPath(POST);
-                        request.getRequestHeaders().put(Headers.HOST, DefaultServer.getHostAddress());
-                        request.getRequestHeaders().put(Headers.TRANSFER_ENCODING, "chunked");
-                        connection.sendRequest(request, new ClientCallback<ClientExchange>() {
-                            @Override
-                            public void completed(ClientExchange result) {
-                                new StringWriteChannelListener(postMessage).setup(result.getRequestChannel());
-                                result.setResponseListener(new ClientCallback<ClientExchange>() {
-                                    @Override
-                                    public void completed(ClientExchange result) {
-                                        new StringReadChannelListener(DefaultServer.getBufferPool()) {
+            connection.getIoThread().execute(() -> {
+                for (int i = 0; i < 10; i++) {
+                    final ClientRequest request = new ClientRequest().setMethod(Methods.POST).setPath(POST);
+                    request.getRequestHeaders().put(Headers.HOST, DefaultServer.getHostAddress());
+                    request.getRequestHeaders().put(Headers.TRANSFER_ENCODING, "chunked");
+                    connection.sendRequest(request, new ClientCallback<ClientExchange>() {
+                        @Override
+                        public void completed(ClientExchange result) {
+                            new StringWriteChannelListener(postMessage).setup(result.getRequestChannel());
+                            result.setResponseListener(new ClientCallback<ClientExchange>() {
+                                @Override
+                                public void completed(ClientExchange result) {
+                                    new StringReadChannelListener(DefaultServer.getBufferPool()) {
 
-                                            @Override
-                                            protected void stringDone(String string) {
-                                                responses.add(string);
-                                                latch.countDown();
-                                            }
+                                        @Override
+                                        protected void stringDone(String string) {
+                                            responses.add(string);
+                                            latch.countDown();
+                                        }
 
-                                            @Override
-                                            protected void error(IOException e) {
-                                                e.printStackTrace();
-                                                latch.countDown();
-                                            }
-                                        }.setup(result.getResponseChannel());
-                                    }
+                                        @Override
+                                        protected void error(IOException e) {
+                                            e.printStackTrace();
+                                            latch.countDown();
+                                        }
+                                    }.setup(result.getResponseChannel());
+                                }
 
-                                    @Override
-                                    public void failed(IOException e) {
-                                        e.printStackTrace();
-                                        latch.countDown();
-                                    }
-                                });
-                            }
+                                @Override
+                                public void failed(IOException e) {
+                                    e.printStackTrace();
+                                    latch.countDown();
+                                }
+                            });
+                        }
 
-                            @Override
-                            public void failed(IOException e) {
-                                e.printStackTrace();
-                                latch.countDown();
-                            }
-                        });
-                    }
+                        @Override
+                        public void failed(IOException e) {
+                            e.printStackTrace();
+                            latch.countDown();
+                        }
+                    });
                 }
-
             });
 
-            latch.await(10, TimeUnit.SECONDS);
+            assertTrue(latch.await(10, TimeUnit.SECONDS));
 
             Assert.assertEquals(10, responses.size());
             for (final String response : responses) {
@@ -348,7 +311,7 @@ public class AjpClientTestCase {
             latch.await();
             final ClientResponse response = responses.iterator().next();
             Assert.assertEquals(message, response.getAttachment(RESPONSE_BODY));
-            Assert.assertEquals(false, connection.isOpen());
+            Assert.assertFalse(connection.isOpen());
         } finally {
             IoUtils.safeClose(connection);
         }
