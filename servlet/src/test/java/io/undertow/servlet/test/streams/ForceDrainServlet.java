b67e821df341e2449314b4b8c8abd0ff2ad805a1
Krystof Stekovic <kstekovi@redhat.com>
2022-07-20 17:05:07 +0200

    [UNDERTOW-2130] Classes witch annotation RunWith are triggered as TestCase.
    Signed-off-by: Flavia Rainone <frainone@redhat.com>

--- servlet/src/test/java/io/undertow/servlet/test/streams/ForceDrainServlet.java
+++ servlet/src/test/java/io/undertow/servlet/test/streams/ForceDrainServlet.java
@@ -23,14 +23,10 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.junit.runner.RunWith;
-
-import io.undertow.testutils.DefaultServer;
 
 /**
  * @author Stuart Douglas
  */
-@RunWith(DefaultServer.class)
 public class ForceDrainServlet extends HttpServlet {
 
     @Override
