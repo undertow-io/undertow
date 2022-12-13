e0825dfad9e3d60a443145c5be57fbd34813edfe
Flavia Rainone <frainone@redhat.com>
2022-12-13 11:42:21 -0300

    [UNDERTOW-2209] Style and cleanup changes (none of the changes will affect the algorithm workflow)
    Signed-off-by: Flavia Rainone <frainone@redhat.com>

--- servlet/src/main/java/io/undertow/servlet/handlers/security/SecurityPathMatches.java
+++ servlet/src/main/java/io/undertow/servlet/handlers/security/SecurityPathMatches.java
@@ -190,16 +190,15 @@ public class SecurityPathMatches {
                 transport(currentMatch, role.transportGuaranteeType);
                 currentMatch.constraints.add(new SingleConstraintMatch(role.emptyRoleSemantic, role.roles));
             }
-        } else if(denyUncoveredHttpMethods) {
-            if(exact.perMethodRequiredRoles.size() == 0) {
+        } else if (denyUncoveredHttpMethods) {
+            if (exact.perMethodRequiredRoles.size() == 0) {
                 // 13.8.4. When HTTP methods are not enumerated within a security-constraint, the protections defined by the
                 // constraint apply to the complete set of HTTP (extension) methods.
                 currentMatch.uncovered = false;
                 currentMatch.constraints.add(new SingleConstraintMatch(SecurityInfo.EmptyRoleSemantic.PERMIT, new HashSet<>()));
-            } else if(exact.perMethodRequiredRoles.size() > 0) {
-                //at this point method is null, but there is match, above if will be triggered for default path, we need to flip it?
-                currentMatch.uncovered = true;
-                //NOTE: ?
+            } else {
+                //at this point method info is null, but there is match, above if will be triggered for default path, we need to flip it?
+                // keep currentMatch.uncovered value as true (this is the value that is initially set)
                 currentMatch.constraints.clear();
                 currentMatch.constraints.add(new SingleConstraintMatch(SecurityInfo.EmptyRoleSemantic.DENY, new HashSet<>()));
             }
