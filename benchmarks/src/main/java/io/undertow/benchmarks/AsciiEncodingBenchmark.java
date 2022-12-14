5ecbd9c4c83027f13b9ba8466bc7b60c7ae5df24
franz1981 <nigro.fra@gmail.com>
2022-12-14 21:42:33 +0100

    single class import on JMH bench

--- benchmarks/src/main/java/io/undertow/benchmarks/AsciiEncodingBenchmark.java
+++ benchmarks/src/main/java/io/undertow/benchmarks/AsciiEncodingBenchmark.java
@@ -18,7 +18,18 @@
 
 package io.undertow.benchmarks;
 
-import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.CompilerControl;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
