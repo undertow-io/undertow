caabe65a1bf0b09315c152fc87ddb786af2fbb63
Flavia Rainone <frainone@redhat.com>
2023-01-10 08:09:37 -0300

    [UNDERTOW-2425] At ServletOutputStreamImpl synchronized workflow (listener = null), prevent the buffer.flip() from not being cleared after an error during attempts to write. Also, at ServletPrintWriter, verify if no progress is being made when attempting to encode returns overflow after flushing, and mark error even if there are remaining bytes in the buffer.
    Signed-off-by: Flavia Rainone <frainone@redhat.com>

--- servlet/src/main/java/io/undertow/servlet/spec/ServletPrintWriter.java
+++ servlet/src/main/java/io/undertow/servlet/spec/ServletPrintWriter.java
@@ -95,7 +95,9 @@ public class ServletPrintWriter {
                 underflow = null;
             }
             if (charsetEncoder != null) {
+                int remaining = 0;
                 do {
+                    // before we get the underlying buffer, we need to flush outputStream
                     ByteBuffer out = outputStream.underlyingBuffer();
                     if (out == null) {
                         //servlet output stream has already been closed
@@ -105,11 +107,13 @@ public class ServletPrintWriter {
                     CoderResult result = charsetEncoder.encode(buffer, out, true);
                     if (result.isOverflow()) {
                         outputStream.flushInternal();
-                        if (out.remaining() == 0) {
+                        if (out.remaining() == remaining) {
+                            // no progress in flush
                             outputStream.close();
                             error = true;
                             return;
-                        }
+                        } else
+                            remaining = out.remaining();
                     } else {
                         done = true;
                     }
@@ -169,7 +173,7 @@ public class ServletPrintWriter {
                 outputStream.updateWritten(writtenLength);
                 if (result.isOverflow() || !buffer.hasRemaining()) {
                     outputStream.flushInternal();
-                    if (!buffer.hasRemaining()) {
+                    if (buffer.remaining() == remaining) {
                         error = true;
                         return;
                     }
