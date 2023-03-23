e259cd80735e3bd23b6fff87c36ff0b6e9094946
Jesper Hammarbäck <jesper@hammarback.eu>
2023-03-23 17:54:26 +0100

    [UNDERTOW-2254] Included the HttpServerExchange in the HostSelector.
     This allows for advanced routing based on eg. header values or attributes.
     Jira: https://issues.redhat.com/browse/UNDERTOW-2254

--- core/src/main/java/io/undertow/server/handlers/proxy/LoadBalancingProxyClient.java
+++ core/src/main/java/io/undertow/server/handlers/proxy/LoadBalancingProxyClient.java
@@ -473,7 +473,11 @@ public class LoadBalancingProxyClient implements ProxyClient {
 
     public interface HostSelector {
 
+        @Deprecated(forRemoval = true)
+        int selectHost(Host[] availableHosts);
+
         int selectHost(Host[] availableHosts, HttpServerExchange exchange);
+
     }
 
     static class RoundRobinHostSelector implements HostSelector {
@@ -481,8 +485,15 @@ public class LoadBalancingProxyClient implements ProxyClient {
         private final AtomicInteger currentHost = new AtomicInteger(0);
 
         @Override
-        public int selectHost(Host[] availableHosts, HttpServerExchange exchange) {
+        @Deprecated(forRemoval = true)
+        public int selectHost(Host[] availableHosts) {
             return currentHost.incrementAndGet() % availableHosts.length;
         }
+
+        @Override
+        public int selectHost(Host[] availableHosts, HttpServerExchange exchange) {
+            return selectHost(availableHosts);
+        }
+
     }
 }
