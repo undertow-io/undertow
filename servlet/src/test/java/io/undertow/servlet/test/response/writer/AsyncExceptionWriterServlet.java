undertow-io/undertow-ee

[UNDERTOW-2293] Add tests for asynchronous writing response, and extra tests for writing response on post before reading input
Signed-off-by: Flavia Rainone <frainone@redhat.com>

servlet/src/test/java/io/undertow/servlet/test/response/writer/AsyncExceptionWriterServlet.java
@@ -1,0 +1,51 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2023 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package io.undertow.servlet.test.response.writer;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Asynchronous version of {@link ExceptionWriterServlet}.
+ *
+ * @author rmartinc
+ */
+public class AsyncExceptionWriterServlet extends jakarta.servlet.http.HttpServlet {
+
+    @Override
+    protected void doGet(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
+        final var asyncContext = req.startAsync();
+        new Thread(()->{
+            try {
+                resp.setContentType("text/plain;charset=UTF-8");
+                try (PrintWriter writer = resp.getWriter()) {
+                    new Exception("TestException").printStackTrace(writer);
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } finally {
+                asyncContext.complete();
+            }
+        }).start();
+    }
+}

servlet/src/test/java/io/undertow/servlet/test/response/writer/AsyncLargeResponseWriterServlet.java
@@ -1,0 +1,50 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2023 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package io.undertow.servlet.test.response.writer;
+
+import jakarta.servlet.AsyncContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+/**
+ * Asynchronous version of {@link LargeResponseWriterServlet}.
+ *
+ * @author Flavia Rainone
+ */
+public class AsyncLargeResponseWriterServlet extends LargeResponseWriterServlet {
+
+    @Override
+    protected void doGet(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
+        final AsyncContext asyncContext = req.startAsync();
+        new Thread(()->{
+            try {
+                String msg = getMessage();
+                resp.setContentLength(msg.length());
+                resp.getWriter().write(msg);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } finally {
+                asyncContext.complete();
+            }
+        }).start();
+    }
+}

servlet/src/test/java/io/undertow/servlet/test/response/writer/AsyncResponseWriterOnPostServlet.java
@@ -1,0 +1,64 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.undertow.servlet.test.response.writer;
+
+import jakarta.servlet.AsyncContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+/**
+ * Asynchronous version of {@link ResponseWriterOnPostServlet}.
+ *
+ * @author Flavia Rainone
+ */
+public class AsyncResponseWriterOnPostServlet extends ResponseWriterOnPostServlet {
+
+    @Override
+    protected void doPost(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
+        String test = req.getParameter("test");
+        if (!test.equals(CONTENT_LENGTH_FLUSH)) {
+            throw new IllegalArgumentException("not a test " + test);
+        }
+        final AsyncContext asyncContext = req.startAsync();
+        new Thread(()->{
+            try {
+                HttpServletResponse response = (HttpServletResponse) asyncContext.getResponse();
+                contentLengthFlush(response);
+                // read after writing the response (UNDERTOW-2243)
+                while (true) {
+                    if (req.getInputStream().readLine(new byte[100], 0, 100) == -1) {
+                        req.getInputStream().close();
+                        break;
+                    }
+                }
+            } catch (RuntimeException e) {
+                exception = e;
+                throw e;
+            } catch (Throwable t) {
+                exception = t;
+                throw new RuntimeException(t);
+            } finally {
+                asyncContext.complete();
+            }
+        }).start();
+    }
+}

servlet/src/test/java/io/undertow/servlet/test/response/writer/AsyncResponseWriterServlet.java
@@ -1,6 +1,6 @@
 /*
  * JBoss, Home of Professional Open Source.
- * Copyright 2012 Red Hat, Inc., and individual contributors
+ * Copyright 2023 Red Hat, Inc., and individual contributors
  * as indicated by the @author tags.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -18,46 +18,36 @@
 
 package io.undertow.servlet.test.response.writer;
 
+import jakarta.servlet.AsyncContext;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
-import java.io.PrintWriter;
 
 /**
- * @author Stuart Douglas
+ * Asynchronous version of {@link ResponseWriterServlet}.
+ *
+ * @author Flavia Rainone
  */
-public class ResponseWriterServlet extends HttpServlet {
-
-    public static final String CONTENT_LENGTH_FLUSH = "content-length-flush";
+public class AsyncResponseWriterServlet extends ResponseWriterServlet {
 
     @Override
     protected void doGet(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
 
         String test = req.getParameter("test");
-        if (test.equals(CONTENT_LENGTH_FLUSH)) {
-            contentLengthFlush(resp);
-        } else {
+        if (!test.equals(CONTENT_LENGTH_FLUSH)) {
             throw new IllegalArgumentException("not a test " + test);
         }
-    }
-
-    protected void contentLengthFlush(HttpServletResponse resp) throws IOException {
-        int size = 10;
-
-        PrintWriter pw = resp.getWriter();
-        StringBuffer tmp = new StringBuffer(2 * size);
-        int i = 0;
-
-        pw.write("first-");
-        resp.setContentLength(size);
-        //write more data than the content length
-        while (i < 20) {
-            tmp.append("a");
-            i = i + 1;
-        }
-        pw.println(tmp);
-        resp.addHeader("not-header", "not");
+        final AsyncContext asyncContext = req.startAsync();
+        new Thread(()->{
+            try {
+                contentLengthFlush((HttpServletResponse) asyncContext.getResponse());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } finally {
+                asyncContext.complete();
+            }
+        }).start();
     }
 }

undertow-io/undertow

servlet/src/test/java/io/undertow/servlet/test/response/writer/AsyncExceptionWriterServlet.java
@@ -1,0 +1,52 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2023 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package io.undertow.servlet.test.response.writer;
+
+import javax.servlet.AsyncContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Asynchronous version of {@link ExceptionWriterServlet}.
+ *
+ * @author rmartinc
+ */
+public class AsyncExceptionWriterServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
+        final AsyncContext asyncContext = req.startAsync();
+        new Thread(()->{
+            try {
+                resp.setContentType("text/plain;charset=UTF-8");
+                try (PrintWriter writer = resp.getWriter()) {
+                    new Exception("TestException").printStackTrace(writer);
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } finally {
+                asyncContext.complete();
+            }
+        }).start();
+    }
+}

servlet/src/test/java/io/undertow/servlet/test/response/writer/AsyncLargeResponseWriterServlet.java
@@ -1,0 +1,49 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2023 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package io.undertow.servlet.test.response.writer;
+
+import javax.servlet.AsyncContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Asynchronous version of {@link LargeResponseWriterServlet}.
+ *
+ * @author Flavia Rainone
+ */
+public class AsyncLargeResponseWriterServlet extends LargeResponseWriterServlet {
+
+    @Override
+    protected void doGet(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
+        final AsyncContext asyncContext = req.startAsync();
+        new Thread(()->{
+            try {
+                String msg = getMessage();
+                resp.setContentLength(msg.length());
+                resp.getWriter().write(msg);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } finally {
+                asyncContext.complete();
+            }
+        }).start();
+    }
+}

servlet/src/test/java/io/undertow/servlet/test/response/writer/AsyncResponseWriterOnPostServlet.java
@@ -1,0 +1,63 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.undertow.servlet.test.response.writer;
+
+import javax.servlet.AsyncContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Asynchronous version of {@link ResponseWriterOnPostServlet}.
+ *
+ * @author Flavia Rainone
+ */
+public class AsyncResponseWriterOnPostServlet extends ResponseWriterOnPostServlet {
+
+    @Override
+    protected void doPost(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
+        String test = req.getParameter("test");
+        if (!test.equals(CONTENT_LENGTH_FLUSH)) {
+            throw new IllegalArgumentException("not a test " + test);
+        }
+        final AsyncContext asyncContext = req.startAsync();
+        new Thread(()->{
+            try {
+                HttpServletResponse response = (HttpServletResponse) asyncContext.getResponse();
+                contentLengthFlush(response);
+                // read after writing the response (UNDERTOW-2243)
+                while (true) {
+                    if (req.getInputStream().readLine(new byte[100], 0, 100) == -1) {
+                        req.getInputStream().close();
+                        break;
+                    }
+                }
+            } catch (RuntimeException e) {
+                exception = e;
+                throw e;
+            } catch (Throwable t) {
+                exception = t;
+                throw new RuntimeException(t);
+            } finally {
+                asyncContext.complete();
+            }
+        }).start();
+    }
+}

servlet/src/test/java/io/undertow/servlet/test/response/writer/AsyncResponseWriterServlet.java
@@ -1,6 +1,6 @@
 /*
  * JBoss, Home of Professional Open Source.
- * Copyright 2012 Red Hat, Inc., and individual contributors
+ * Copyright 2023 Red Hat, Inc., and individual contributors
  * as indicated by the @author tags.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -18,46 +18,35 @@
 
 package io.undertow.servlet.test.response.writer;
 
+import javax.servlet.AsyncContext;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 /**
- * @author Stuart Douglas
+ * Asynchronous version of {@link ResponseWriterServlet}.
+ *
+ * @author Flavia Rainone
  */
-public class ResponseWriterServlet extends HttpServlet {
-
-    public static final String CONTENT_LENGTH_FLUSH = "content-length-flush";
+public class AsyncResponseWriterServlet extends ResponseWriterServlet {
 
     @Override
     protected void doGet(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
 
         String test = req.getParameter("test");
-        if (test.equals(CONTENT_LENGTH_FLUSH)) {
-            contentLengthFlush(resp);
-        } else {
+        if (!test.equals(CONTENT_LENGTH_FLUSH)) {
             throw new IllegalArgumentException("not a test " + test);
         }
-    }
-
-    protected void contentLengthFlush(HttpServletResponse resp) throws IOException {
-        int size = 10;
-
-        PrintWriter pw = resp.getWriter();
-        StringBuffer tmp = new StringBuffer(2 * size);
-        int i = 0;
-
-        pw.write("first-");
-        resp.setContentLength(size);
-        //write more data than the content length
-        while (i < 20) {
-            tmp.append("a");
-            i = i + 1;
-        }
-        pw.println(tmp);
-        resp.addHeader("not-header", "not");
+        final AsyncContext asyncContext = req.startAsync();
+        new Thread(()->{
+            try {
+                contentLengthFlush((HttpServletResponse) asyncContext.getResponse());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } finally {
+                asyncContext.complete();
+            }
+        }).start();
     }
 }