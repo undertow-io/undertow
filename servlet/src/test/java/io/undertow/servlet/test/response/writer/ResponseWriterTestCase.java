undertow-io/undertow-ee

[UNDERTOW-2293] Add tests for asynchronous writing response, and extra tests for writing response on post before reading input
Signed-off-by: Flavia Rainone <frainone@redhat.com>

servlet/src/test/java/io/undertow/servlet/test/response/writer/ResponseWriterTestCase.java
@@ -18,17 +18,6 @@
 
 package io.undertow.servlet.test.response.writer;
 
-import jakarta.servlet.ServletException;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.hamcrest.CoreMatchers;
-import org.hamcrest.MatcherAssert;
-import org.junit.Assert;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
 import io.undertow.server.handlers.PathHandler;
 import io.undertow.servlet.Servlets;
 import io.undertow.servlet.api.DeploymentInfo;
@@ -39,9 +28,43 @@ import io.undertow.testutils.DefaultServer;
 import io.undertow.testutils.TestHttpClient;
 import io.undertow.util.FileUtils;
 import io.undertow.util.StatusCodes;
+import jakarta.servlet.ServletException;
+import org.apache.http.Header;
+import org.apache.http.HttpHeaders;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.InputStreamEntity;
+import org.apache.http.impl.NoConnectionReuseStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicHeader;
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
+import static org.wildfly.common.Assert.assertTrue;
 
 /**
+ * Tests for response writer servlets.
+ *
  * @author Tomaz Cerar
+ * @author Flavia Rainone
  */
 @RunWith(DefaultServer.class)
 public class ResponseWriterTestCase {
@@ -58,10 +81,24 @@ public class ResponseWriterTestCase {
                 .setDeploymentName("servletContext.war")
                 .addServlet(Servlets.servlet("resp", ResponseWriterServlet.class)
                         .addMapping("/resp"))
-                .addServlet(Servlets.servlet("respLArget", LargeResponseWriterServlet.class)
+                .addServlet(Servlets.servlet("respLarge", LargeResponseWriterServlet.class)
                         .addMapping("/large"))
                 .addServlet(Servlets.servlet("exception", ExceptionWriterServlet.class)
-                        .addMapping("/exception"));
+                        .addMapping("/exception"))
+                .addServlet(Servlets.servlet("respBeforeRead", ResponseWriterOnPostServlet.class)
+                        .addMapping("/resp-before-read"))
+                .addServlet(Servlets.servlet("asyncResp", AsyncResponseWriterServlet.class)
+                        .setAsyncSupported(true)
+                        .addMapping("/async-resp"))
+                .addServlet(Servlets.servlet("asyncRespLarge", AsyncLargeResponseWriterServlet.class)
+                        .setAsyncSupported(true)
+                        .addMapping("/async-large"))
+                .addServlet(Servlets.servlet("asyncException", AsyncExceptionWriterServlet.class)
+                        .setAsyncSupported(true)
+                        .addMapping("/async-exception"))
+                .addServlet(Servlets.servlet("asyncRespBeforeRead", AsyncResponseWriterOnPostServlet.class)
+                        .setAsyncSupported(true)
+                        .addMapping("/async-resp-before-read"));
 
         DeploymentManager manager = container.addDeployment(builder);
         manager.deploy();
@@ -71,30 +108,47 @@ public class ResponseWriterTestCase {
 
     @Test
     public void testContentLengthBasedFlush() throws Exception {
+        assertContentLengthBasedFlush("resp");
+    }
+
+    @Test
+    public void testAsyncContentLengthBasedFlush() throws Exception {
+        assertContentLengthBasedFlush("async-resp");
+    }
+
+    private void assertContentLengthBasedFlush(String path) throws Exception {
         TestHttpClient client = new TestHttpClient();
         try {
-            HttpGet get = new HttpGet(DefaultServer.getDefaultServerURL() + "/servletContext/resp?test=" + ResponseWriterServlet.CONTENT_LENGTH_FLUSH);
+            HttpGet get = new HttpGet(DefaultServer.getDefaultServerURL() + "/servletContext/" + path + "?test=" + ResponseWriterServlet.CONTENT_LENGTH_FLUSH);
             HttpResponse result = client.execute(get);
-            Assert.assertEquals(StatusCodes.OK, result.getStatusLine().getStatusCode());
+            assertEquals(StatusCodes.OK, result.getStatusLine().getStatusCode());
             String data = FileUtils.readFile(result.getEntity().getContent());
-            Assert.assertEquals("first-aaaa", data);
-            Assert.assertEquals(0, result.getHeaders("not-header").length);
+            assertEquals("first-aaaa", data);
+            assertEquals(0, result.getHeaders("not-header").length);
 
         } finally {
             client.getConnectionManager().shutdown();
         }
     }
 
-
     @Test
     public void testWriterLargeResponse() throws Exception {
+        assertWriterLargeResponse("large");
+    }
+
+    @Test
+    public void testAsyncWriterLargeResponse() throws Exception {
+        assertWriterLargeResponse("async-large");
+    }
+
+    private void assertWriterLargeResponse(String path) throws Exception {
         TestHttpClient client = new TestHttpClient();
         try {
-            HttpGet get = new HttpGet(DefaultServer.getDefaultServerURL() + "/servletContext/large");
+            HttpGet get = new HttpGet(DefaultServer.getDefaultServerURL() + "/servletContext/" + path);
             HttpResponse result = client.execute(get);
-            Assert.assertEquals(StatusCodes.OK, result.getStatusLine().getStatusCode());
+            assertEquals(StatusCodes.OK, result.getStatusLine().getStatusCode());
             String data = FileUtils.readFile(result.getEntity().getContent());
-            Assert.assertEquals(LargeResponseWriterServlet.getMessage(), data);
+            assertEquals(LargeResponseWriterServlet.getMessage(), data);
 
         } finally {
             client.getConnectionManager().shutdown();
@@ -103,15 +157,92 @@ public class ResponseWriterTestCase {
 
     @Test
     public void testExceptionResponse() throws Exception {
+        assertExceptionResponse("exception");
+    }
+
+    @Test
+    public void testAsyncExceptionResponse() throws Exception {
+        assertExceptionResponse("async-exception");
+    }
+
+    private void assertExceptionResponse(String path) throws Exception {
         TestHttpClient client = new TestHttpClient();
         try {
-            HttpGet get = new HttpGet(DefaultServer.getDefaultServerURL() + "/servletContext/exception");
+            HttpGet get = new HttpGet(DefaultServer.getDefaultServerURL() + "/servletContext/" + path);
             HttpResponse result = client.execute(get);
-            Assert.assertEquals(StatusCodes.OK, result.getStatusLine().getStatusCode());
+            assertEquals(StatusCodes.OK, result.getStatusLine().getStatusCode());
             String response = FileUtils.readFile(result.getEntity().getContent());
             MatcherAssert.assertThat(response, CoreMatchers.startsWith("java.lang.Exception: TestException"));
         } finally {
             client.getConnectionManager().shutdown();
+        }
+    }
+
+    @Test
+    public void testRespondBeforeRead() throws Throwable {
+        assertRespondBeforeRead("resp-before-read");
+    }
+
+    @Test
+    public void testAsyncRespondBeforeRead() throws Throwable {
+        assertRespondBeforeRead("async-resp-before-read");
+    }
+
+    private void assertRespondBeforeRead(String path) throws Throwable {
+        final List<Header> headers = new ArrayList<>();
+        headers.add(new BasicHeader(HttpHeaders.CONNECTION, "close"));
+        final HttpClientBuilder builder = HttpClients.custom().setDefaultHeaders(headers)
+                .setConnectionReuseStrategy(new NoConnectionReuseStrategy());
+        try (CloseableHttpClient client = builder.build()) {
+            final HttpPost post = new HttpPost(DefaultServer.getDefaultServerURL() + "/servletContext/" + path + "?test=" + ResponseWriterServlet.CONTENT_LENGTH_FLUSH);
+            // anything will do, send the bytecodes of this class just for testing purposes
+            final Path rootPath = Paths.get(getClass().getResource(getClass().getSimpleName() + ".class").toURI());
+            final SlowInputStream inputStream = new SlowInputStream(new BufferedInputStream(new FileInputStream(rootPath.toFile())));
+            post.setEntity(new InputStreamEntity(inputStream));
+            final HttpResponse result = client.execute(post);
+            // wait til it is fully read
+            boolean fullyRead = inputStream.waitTillIsFullyRead();
+            // check if servlet ran without any exceptions
+            final Throwable exception = ResponseWriterOnPostServlet.getExceptionIfAny();
+            if (exception != null) {
+                throw exception;
+            }
+            assertTrue(fullyRead);
+            assertEquals(StatusCodes.OK, result.getStatusLine().getStatusCode());
+            String data = FileUtils.readFile(result.getEntity().getContent());
+            assertEquals("first-aaaa", data);
+            assertEquals(0, result.getHeaders("not-header").length);
+        }
+    }
+
+    private static class SlowInputStream extends InputStream {
+        private final InputStream innerInputStream;
+        final CountDownLatch latch = new CountDownLatch(1);
+
+        SlowInputStream(InputStream innerInputStream) {
+            this.innerInputStream = innerInputStream;
+        }
+
+        @Override // enforce that reading will take place after the response is written with some extra delay
+        public int read() throws IOException {
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            int readByte = innerInputStream.read();
+            if (readByte == -1) {
+                latch.countDown();
+            }
+            return readByte;
+        }
+
+        boolean waitTillIsFullyRead() throws InterruptedException {
+            boolean fullyRead = latch.await(60, TimeUnit.SECONDS);
+            // I tested this and the extra sleep time is necessary to be able to view any exception caught
+            // by the servlet, since the exception might happen after read above has returned -1
+            Thread.sleep(50);
+            return fullyRead;
         }
     }
 }

undertow-io/undertow

servlet/src/test/java/io/undertow/servlet/test/response/writer/ResponseWriterTestCase.java
@@ -18,16 +18,6 @@
 
 package io.undertow.servlet.test.response.writer;
 
-import javax.servlet.ServletException;
-
-import io.undertow.util.StatusCodes;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.junit.Assert;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
 import io.undertow.server.handlers.PathHandler;
 import io.undertow.servlet.Servlets;
 import io.undertow.servlet.api.DeploymentInfo;
@@ -37,9 +27,44 @@ import io.undertow.servlet.test.util.TestClassIntrospector;
 import io.undertow.testutils.DefaultServer;
 import io.undertow.testutils.TestHttpClient;
 import io.undertow.util.FileUtils;
+import io.undertow.util.StatusCodes;
+import javax.servlet.ServletException;
+import org.apache.http.Header;
+import org.apache.http.HttpHeaders;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.InputStreamEntity;
+import org.apache.http.impl.NoConnectionReuseStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicHeader;
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
+import static org.wildfly.common.Assert.assertTrue;
 
 /**
+ * Tests for response writer servlets.
+ *
  * @author Tomaz Cerar
+ * @author Flavia Rainone
  */
 @RunWith(DefaultServer.class)
 public class ResponseWriterTestCase {
@@ -56,8 +81,24 @@ public class ResponseWriterTestCase {
                 .setDeploymentName("servletContext.war")
                 .addServlet(Servlets.servlet("resp", ResponseWriterServlet.class)
                         .addMapping("/resp"))
-                .addServlet(Servlets.servlet("respLArget", LargeResponseWriterServlet.class)
-                        .addMapping("/large"));
+                .addServlet(Servlets.servlet("respLarge", LargeResponseWriterServlet.class)
+                        .addMapping("/large"))
+                .addServlet(Servlets.servlet("exception", ExceptionWriterServlet.class)
+                        .addMapping("/exception"))
+                .addServlet(Servlets.servlet("respBeforeRead", ResponseWriterOnPostServlet.class)
+                        .addMapping("/resp-before-read"))
+                .addServlet(Servlets.servlet("asyncResp", AsyncResponseWriterServlet.class)
+                        .setAsyncSupported(true)
+                        .addMapping("/async-resp"))
+                .addServlet(Servlets.servlet("asyncRespLarge", AsyncLargeResponseWriterServlet.class)
+                        .setAsyncSupported(true)
+                        .addMapping("/async-large"))
+                .addServlet(Servlets.servlet("asyncException", AsyncExceptionWriterServlet.class)
+                        .setAsyncSupported(true)
+                        .addMapping("/async-exception"))
+                .addServlet(Servlets.servlet("asyncRespBeforeRead", AsyncResponseWriterOnPostServlet.class)
+                        .setAsyncSupported(true)
+                        .addMapping("/async-resp-before-read"));
 
         DeploymentManager manager = container.addDeployment(builder);
         manager.deploy();
@@ -67,33 +108,141 @@ public class ResponseWriterTestCase {
 
     @Test
     public void testContentLengthBasedFlush() throws Exception {
+        assertContentLengthBasedFlush("resp");
+    }
+
+    @Test
+    public void testAsyncContentLengthBasedFlush() throws Exception {
+        assertContentLengthBasedFlush("async-resp");
+    }
+
+    private void assertContentLengthBasedFlush(String path) throws Exception {
         TestHttpClient client = new TestHttpClient();
         try {
-            HttpGet get = new HttpGet(DefaultServer.getDefaultServerURL() + "/servletContext/resp?test=" + ResponseWriterServlet.CONTENT_LENGTH_FLUSH);
+            HttpGet get = new HttpGet(DefaultServer.getDefaultServerURL() + "/servletContext/" + path + "?test=" + ResponseWriterServlet.CONTENT_LENGTH_FLUSH);
             HttpResponse result = client.execute(get);
-            Assert.assertEquals(StatusCodes.OK, result.getStatusLine().getStatusCode());
+            assertEquals(StatusCodes.OK, result.getStatusLine().getStatusCode());
             String data = FileUtils.readFile(result.getEntity().getContent());
-            Assert.assertEquals("first-aaaa", data);
-            Assert.assertEquals(0, result.getHeaders("not-header").length);
+            assertEquals("first-aaaa", data);
+            assertEquals(0, result.getHeaders("not-header").length);
 
         } finally {
             client.getConnectionManager().shutdown();
         }
     }
 
-
     @Test
     public void testWriterLargeResponse() throws Exception {
+        assertWriterLargeResponse("large");
+    }
+
+    @Test
+    public void testAsyncWriterLargeResponse() throws Exception {
+        assertWriterLargeResponse("async-large");
+    }
+
+    private void assertWriterLargeResponse(String path) throws Exception {
         TestHttpClient client = new TestHttpClient();
         try {
-            HttpGet get = new HttpGet(DefaultServer.getDefaultServerURL() + "/servletContext/large");
+            HttpGet get = new HttpGet(DefaultServer.getDefaultServerURL() + "/servletContext/" + path);
             HttpResponse result = client.execute(get);
-            Assert.assertEquals(StatusCodes.OK, result.getStatusLine().getStatusCode());
+            assertEquals(StatusCodes.OK, result.getStatusLine().getStatusCode());
             String data = FileUtils.readFile(result.getEntity().getContent());
-            Assert.assertEquals(LargeResponseWriterServlet.getMessage(), data);
+            assertEquals(LargeResponseWriterServlet.getMessage(), data);
 
         } finally {
             client.getConnectionManager().shutdown();
+        }
+    }
+
+    @Test
+    public void testExceptionResponse() throws Exception {
+        assertExceptionResponse("exception");
+    }
+
+    @Test
+    public void testAsyncExceptionResponse() throws Exception {
+        assertExceptionResponse("async-exception");
+    }
+
+    private void assertExceptionResponse(String path) throws Exception {
+        TestHttpClient client = new TestHttpClient();
+        try {
+            HttpGet get = new HttpGet(DefaultServer.getDefaultServerURL() + "/servletContext/" + path);
+            HttpResponse result = client.execute(get);
+            assertEquals(StatusCodes.OK, result.getStatusLine().getStatusCode());
+            String response = FileUtils.readFile(result.getEntity().getContent());
+            MatcherAssert.assertThat(response, CoreMatchers.startsWith("java.lang.Exception: TestException"));
+        } finally {
+            client.getConnectionManager().shutdown();
+        }
+    }
+
+    @Test
+    public void testRespondBeforeRead() throws Throwable {
+        assertRespondBeforeRead("resp-before-read");
+    }
+
+    @Test
+    public void testAsyncRespondBeforeRead() throws Throwable {
+        assertRespondBeforeRead("async-resp-before-read");
+    }
+
+    private void assertRespondBeforeRead(String path) throws Throwable {
+        final List<Header> headers = new ArrayList<>();
+        headers.add(new BasicHeader(HttpHeaders.CONNECTION, "close"));
+        final HttpClientBuilder builder = HttpClients.custom().setDefaultHeaders(headers)
+                .setConnectionReuseStrategy(new NoConnectionReuseStrategy());
+        try (CloseableHttpClient client = builder.build()) {
+            final HttpPost post = new HttpPost(DefaultServer.getDefaultServerURL() + "/servletContext/" + path + "?test=" + ResponseWriterServlet.CONTENT_LENGTH_FLUSH);
+            // anything will do, send the bytecodes of this class just for testing purposes
+            final Path rootPath = Paths.get(getClass().getResource(getClass().getSimpleName() + ".class").toURI());
+            final SlowInputStream inputStream = new SlowInputStream(new BufferedInputStream(new FileInputStream(rootPath.toFile())));
+            post.setEntity(new InputStreamEntity(inputStream));
+            final HttpResponse result = client.execute(post);
+            // wait til it is fully read
+            boolean fullyRead = inputStream.waitTillIsFullyRead();
+            // check if servlet ran without any exceptions
+            final Throwable exception = ResponseWriterOnPostServlet.getExceptionIfAny();
+            if (exception != null) {
+                throw exception;
+            }
+            assertTrue(fullyRead);
+            assertEquals(StatusCodes.OK, result.getStatusLine().getStatusCode());
+            String data = FileUtils.readFile(result.getEntity().getContent());
+            assertEquals("first-aaaa", data);
+            assertEquals(0, result.getHeaders("not-header").length);
+        }
+    }
+
+    private static class SlowInputStream extends InputStream {
+        private final InputStream innerInputStream;
+        final CountDownLatch latch = new CountDownLatch(1);
+
+        SlowInputStream(InputStream innerInputStream) {
+            this.innerInputStream = innerInputStream;
+        }
+
+        @Override // enforce that reading will take place after the response is written with some extra delay
+        public int read() throws IOException {
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            int readByte = innerInputStream.read();
+            if (readByte == -1) {
+                latch.countDown();
+            }
+            return readByte;
+        }
+
+        boolean waitTillIsFullyRead() throws InterruptedException {
+            boolean fullyRead = latch.await(60, TimeUnit.SECONDS);
+            // I tested this and the extra sleep time is necessary to be able to view any exception caught
+            // by the servlet, since the exception might happen after read above has returned -1
+            Thread.sleep(50);
+            return fullyRead;
         }
     }
 }