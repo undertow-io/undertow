0ef8d7ce5678a0ed27611cc08c30b638571902f7
Flavia Rainone <frainone@redhat.com>
2024-10-11 08:50:17 -0300

    [UNDERTOW-2449] Disable PushResourceRSTTestCase. We are still seeing failures in CI.
    Signed-off-by: Flavia Rainone <frainone@redhat.com>

--- core/src/test/java/io/undertow/protocols/http2/PushResourceRSTTestCase.java
+++ core/src/test/java/io/undertow/protocols/http2/PushResourceRSTTestCase.java
@@ -17,42 +17,6 @@
  */
 package io.undertow.protocols.http2;
 
-import static io.undertow.server.protocol.http2.Http2OpenListener.HTTP2;
-import static io.undertow.testutils.StopServerWithExternalWorkerUtils.stopWorker;
-import static java.security.AccessController.doPrivileged;
-
-import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.net.URI;
-import java.security.PrivilegedAction;
-import java.util.List;
-import java.util.ServiceLoader;
-import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
-
-import org.jboss.logging.Logger;
-import org.junit.AfterClass;
-import org.junit.Assert;
-import org.junit.Assume;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.junit.experimental.categories.Category;
-import org.junit.runner.RunWith;
-import org.xnio.ChannelListener;
-import org.xnio.ChannelListeners;
-import org.xnio.FutureResult;
-import org.xnio.IoFuture;
-import org.xnio.IoUtils;
-import org.xnio.OptionMap;
-import org.xnio.Options;
-import org.xnio.StreamConnection;
-import org.xnio.Xnio;
-import org.xnio.XnioWorker;
-import org.xnio.channels.StreamSinkChannel;
-import org.xnio.ssl.SslConnection;
-
 import io.undertow.Undertow;
 import io.undertow.UndertowLogger;
 import io.undertow.UndertowOptions;
@@ -71,15 +35,49 @@ import io.undertow.io.Sender;
 import io.undertow.protocols.ssl.UndertowXnioSsl;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.server.handlers.PathHandler;
-import io.undertow.testutils.AjpIgnore;
 import io.undertow.testutils.DefaultServer;
-import io.undertow.testutils.ProxyIgnore;
 import io.undertow.testutils.category.UnitTest;
 import io.undertow.util.AttachmentKey;
 import io.undertow.util.Headers;
 import io.undertow.util.Methods;
 import io.undertow.util.StatusCodes;
 import io.undertow.util.StringReadChannelListener;
+import org.jboss.logging.Logger;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Assume;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.xnio.ChannelListener;
+import org.xnio.ChannelListeners;
+import org.xnio.FutureResult;
+import org.xnio.IoFuture;
+import org.xnio.IoUtils;
+import org.xnio.OptionMap;
+import org.xnio.Options;
+import org.xnio.StreamConnection;
+import org.xnio.Xnio;
+import org.xnio.XnioWorker;
+import org.xnio.channels.StreamSinkChannel;
+import org.xnio.ssl.SslConnection;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.URI;
+import java.security.PrivilegedAction;
+import java.util.List;
+import java.util.ServiceLoader;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static io.undertow.server.protocol.http2.Http2OpenListener.HTTP2;
+import static io.undertow.testutils.StopServerWithExternalWorkerUtils.stopWorker;
+import static java.security.AccessController.doPrivileged;
 
 /**
  * Test RST frames handling on push. This test mimics rapid refresh on client side, which will result in push requests from
@@ -88,8 +86,7 @@ import io.undertow.util.StringReadChannelListener;
  */
 @Category(UnitTest.class)
 @RunWith(DefaultServer.class)
-@ProxyIgnore
-@AjpIgnore
+@Ignore
 public class PushResourceRSTTestCase {
     private static final Logger log = Logger.getLogger(PushResourceRSTTestCase.class);
     private static final String PUSHER = "/pusher";
