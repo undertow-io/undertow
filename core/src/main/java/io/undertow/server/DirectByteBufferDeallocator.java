a6d7286758fa1859aa2609ea2570f8e46dccc8e2
Flavia Rainone <frainone@redhat.com>
2025-10-10 01:10:15 -0300

    [UNDERTOW-2598] CVE-2025-9784 Add a delay in the actual direct byte buffer deallocation via Unsafe.
    This is done in a way that does not impact the server's throughput or the server's ByteBuffer pool APIs.
    
    Signed-off-by: Flavia Rainone <frainone@redhat.com>

--- core/src/main/java/io/undertow/server/DirectByteBufferDeallocator.java
+++ core/src/main/java/io/undertow/server/DirectByteBufferDeallocator.java
@@ -1,13 +1,16 @@
 package io.undertow.server;
 
+import io.undertow.UndertowLogger;
+import sun.misc.Unsafe;
+
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.nio.ByteBuffer;
 import java.security.AccessController;
 import java.security.PrivilegedAction;
-
-import io.undertow.UndertowLogger;
-import sun.misc.Unsafe;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 /**
  * {@link DirectByteBufferDeallocator} Utility class used to free direct buffer memory.
@@ -16,11 +19,13 @@ public final class DirectByteBufferDeallocator {
     private static final boolean SUPPORTED;
     private static final Method cleaner;
     private static final Method cleanerClean;
+    private static final Queue<ByteBuffer> bufferQueue;
 
     private static final Unsafe UNSAFE;
 
 
     static {
+        bufferQueue = new ConcurrentLinkedQueue<>();
         String versionString = System.getProperty("java.specification.version");
         if(versionString.equals("0.9")) {
             //android hardcoded
@@ -73,15 +78,48 @@ public final class DirectByteBufferDeallocator {
     public static void free(ByteBuffer buffer) {
         if (SUPPORTED && buffer != null && buffer.isDirect()) {
             try {
-                if (UNSAFE != null) {
-                    //use the JDK9 method
-                    cleanerClean.invoke(UNSAFE, buffer);
+                // free the whole buffer queue if we reached a size bigger than
+                // or equal to 5
+                // this size is just a small enough arbitrarily chosen number
+                // to make sure that we are not too strict, such as choosing 2 as
+                // the limit, but not too permissive, we don't want to let the
+                // queue grow too big
+                if (bufferQueue.size() >= 5) {
+                    ByteBuffer queuedBuffer = bufferQueue.poll();
+                    while (queuedBuffer != null) {
+                        cleanBuffer(queuedBuffer);
+                        queuedBuffer = bufferQueue.poll();
+                    }
                 } else {
-                    Object cleaner = DirectByteBufferDeallocator.cleaner.invoke(buffer);
-                    cleanerClean.invoke(cleaner);
+                    // this is the standard operation here, pick one buffer from
+                    // the queue and clean it
+                    final ByteBuffer queuedBuffer = bufferQueue.poll();
+                    if (queuedBuffer != null) {
+                        cleanBuffer(queuedBuffer);
+                    }
                 }
+                // put the buffer to be cleaned in the queue
+                // the goal here is to create a delay to make sure
+                // that the buffer is not immediately deallocated
+                // as there is a small window of time in which the
+                // buffer is still accessible via local variables;
+                // if a direct buffer is cleaned and then written to
+                // or read from, the behavior of the sdk is unpredictable
+                bufferQueue.add(buffer);
             } catch (Throwable t) {
                 UndertowLogger.ROOT_LOGGER.directBufferDeallocationFailed(t);
+            }
+        }
+    }
+
+    private static void cleanBuffer(ByteBuffer buffer) throws InvocationTargetException, IllegalAccessException {
+        if (buffer != null) {
+            if (UNSAFE != null) {
+                //use the JDK9 method
+                cleanerClean.invoke(UNSAFE, buffer);
+            } else {
+                Object cleaner = DirectByteBufferDeallocator.cleaner.invoke(buffer);
+                cleanerClean.invoke(cleaner);
             }
         }
     }
