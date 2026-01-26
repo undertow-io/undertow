7d6f52063273db2436ebb8f7e6d2a11334134751
baranowb <baranowb@gmail.com>
2026-01-26 14:29:07 +0100

    [UNDERTOW-2667] Add annotation to ignore long running tests with proper switch

---
+++ core/src/test/java/io/undertow/testutils/IgnoreLongRunning.java
@@ -0,0 +1,33 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2026 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package io.undertow.testutils;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+
+/**
+* Annotation to use on long running test in order to shorten non CI TS run. This  can be used only with DefaultServer runner.
+* @author baranowb
+*/
+@Retention(RUNTIME)
+@Inherited
+public @interface IgnoreLongRunning {
+    String value() default "IGNORE";
+}
